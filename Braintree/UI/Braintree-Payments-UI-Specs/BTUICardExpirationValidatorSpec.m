456761fae844360ad813ffb754076e06c1c5de49
Mickey Reiss <mickey@venmo.com>
2014-07-28 12:06:49 -0700

    sdk DD2Td5Ma - Refactor BTUICardExpirationValidatorSpec

--- Braintree/UI/Braintree-Payments-UI-Specs/BTUICardExpirationValidatorSpec.m
+++ Braintree/UI/Braintree-Payments-UI-Specs/BTUICardExpirationValidatorSpec.m
@@ -2,13 +2,11 @@
 
 SpecBegin(BTUICardExpirationValidator)
 
-describe(@"BTUICardExpirationValidator", ^{
-
-    describe(@"monthYearValidForDate:", ^{
+describe(@"month:year:validForDate:", ^{
+    context(@"validating month and year relative to given validation date", ^{
         __block NSDate *today;
-        __block NSDate *endOfYearToday;
 
-        before(^{
+        beforeEach(^{
             NSDateComponents *components = [[NSDateComponents alloc] init];
             components.calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
             components.day = 2;
@@ -17,58 +15,71 @@ describe(@"BTUICardExpirationValidator", ^{
             today = components.date;
         });
 
-        it(@"should return false when the month year are before the provided date ", ^{
-            BOOL monthYearValid = [BTUICardExpirationValidator month:4 year: 14 validForDate:today];
-            expect(monthYearValid).to.beFalsy();
+        it(@"returns false when the month year are before the provided date ", ^{
+            BOOL monthYearBeforeTodayValid = [BTUICardExpirationValidator month:4 year:14 validForDate:today];
+            expect(monthYearBeforeTodayValid).to.beFalsy();
         });
-        it(@"should return true when the month year are the same as the provided date ", ^{
-            BOOL monthYearValid = [BTUICardExpirationValidator month:5 year: 14 validForDate:today];
-            expect(monthYearValid).to.beTruthy();
+
+        it(@"returns true when the month year are the same as the provided date ", ^{
+            BOOL monthYearSameAsTodayValid = [BTUICardExpirationValidator month:5 year:14 validForDate:today];
+            expect(monthYearSameAsTodayValid).to.beTruthy();
         });
-        it(@"should return true when the month year are after the provided date ", ^{
-            BOOL monthYearValid = [BTUICardExpirationValidator month:8 year: 14 validForDate:today];
-            expect(monthYearValid).to.beTruthy();
+
+        it(@"returns true when the month year are after the provided date ", ^{
+            BOOL monthYearAfterTodayValid = [BTUICardExpirationValidator month:8 year:14 validForDate:today];
+            expect(monthYearAfterTodayValid).to.beTruthy();
         });
 
         describe(@"Year in YYYY", ^{
-            it(@"should return true when the month year are after the provided date", ^{
-                BOOL monthYearValid = [BTUICardExpirationValidator month:8 year: 2014 validForDate:today];
+            it(@"returns true when the month year are after the provided date", ^{
+                BOOL monthYearValid = [BTUICardExpirationValidator month:8 year:2014 validForDate:today];
                 expect(monthYearValid).to.beTruthy();
             });
-            it(@"should return false when the month year are before the provided date", ^{
-                BOOL monthYearValid = [BTUICardExpirationValidator month:4 year: 2014 validForDate:today];
+
+            it(@"returns false when the month year are before the provided date", ^{
+                BOOL monthYearValid = [BTUICardExpirationValidator month:4 year:2014 validForDate:today];
                 expect(monthYearValid).to.beFalsy();
             });
         });
+    });
 
-
-        describe(@"Date is at the end of the year", ^{
-            before(^{
-                NSDateComponents *components = [[NSDateComponents alloc] init];
-                components.calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
-                components.day = 1;
-                components.month = 12;
-                components.year = 2014;
-                endOfYearToday = components.date;
-            });
-
-            it(@"should return true when the month/year are the same as the provided date", ^{
-                BOOL monthYearValid = [BTUICardExpirationValidator month:12 year: 2014 validForDate:endOfYearToday];
-                expect(monthYearValid).to.beTruthy();
-            });
+    context(@"validating dates at the end of the year", ^{
+        __block NSDate *endOfYearToday;
+        beforeEach(^{
+            NSDateComponents *components = [[NSDateComponents alloc] init];
+            components.calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+            components.day = 1;
+            components.month = 12;
+            components.year = 2014;
+            endOfYearToday = components.date;
         });
 
-        describe(@"Date is far in the future", ^{
+        it(@"returns true when the month/year are the same as the provided date", ^{
+            BOOL monthYearValid = [BTUICardExpirationValidator month:12 year:2014 validForDate:endOfYearToday];
+            expect(monthYearValid).to.beTruthy();
+        });
+    });
 
-            it(@"should return true when the month year are before but near the far future date", ^{
-                BOOL monthYearValid = [BTUICardExpirationValidator month:4 year:14 + kBTUICardExpirationValidatorFarFutureYears validForDate:today];
-                expect(monthYearValid).to.beTruthy();
-            });
+    context(@"validating dates far in the future", ^{
+        __block NSDate *today;
 
-            it(@"should return false when the month year are not before the far future date", ^{
-                BOOL monthYearValid = [BTUICardExpirationValidator month:5 year:14 + kBTUICardExpirationValidatorFarFutureYears validForDate:today];
-                expect(monthYearValid).to.beFalsy();
-            });
+        beforeEach(^{
+            NSDateComponents *components = [[NSDateComponents alloc] init];
+            components.calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+            components.day = 2;
+            components.month = 5;
+            components.year = 2014;
+            today = components.date;
+        });
+
+        it(@"returns true when the month year are before but near the far future date", ^{
+            BOOL monthYearValid = [BTUICardExpirationValidator month:4 year:14 + kBTUICardExpirationValidatorFarFutureYears validForDate:today];
+            expect(monthYearValid).to.beTruthy();
+        });
+
+        it(@"returns false when the month year are not before the far future date", ^{
+            BOOL monthYearValid = [BTUICardExpirationValidator month:5 year:14 + kBTUICardExpirationValidatorFarFutureYears validForDate:today];
+            expect(monthYearValid).to.beFalsy();
         });
     });
 
