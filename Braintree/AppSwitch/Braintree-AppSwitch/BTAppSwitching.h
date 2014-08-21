dc228112752ecad2ab76b660633076385bbc2772
Brent Fitzgerald <brent.fitzgerald@getbraintree.com>
2014-08-21 12:59:45 -0700

    Add header docs to BTAppSwitching

--- Braintree/AppSwitch/Braintree-AppSwitch/BTAppSwitching.h
+++ Braintree/AppSwitch/Braintree-AppSwitch/BTAppSwitching.h
@@ -12,12 +12,31 @@
 /// with your Bundle ID, e.g. com.yourcompany.Your-App.payment
 @property (nonatomic, copy) NSString *returnURLScheme;
 
+/// A delegate that receives messages throughout the app switch cycle
 @property (nonatomic, weak) id<BTAppSwitchingDelegate>delegate;
 
+///  Perform app switch
+///
+///  @param client   A BTClient needed for obtaining app switch configuration,
+///                  and performing post-switch gateway operations.
+///  @param delegate A delegate that will receive throughout the app switch cycle
+///
+///  @return whether app switch is occurring.
 - (BOOL)initiateAppSwitchWithClient:(BTClient *)client delegate:(id<BTAppSwitchingDelegate>)delegate;
 
+///  Whether this instance can be used to handle this response URL.
+///
+///  @param url
+///  @param sourceApplication
+///
+///  @return Whether this instance can handle the given callback URL from
+///  the given source application.
 - (BOOL)canHandleReturnURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication;
 
+///  Handle the actual response URL that contains payment authorization,
+///  indication of cancellation, or error information.
+///
+///  @param url The callback response URL.
 - (void)handleReturnURL:(NSURL *)url;
 
 @end
