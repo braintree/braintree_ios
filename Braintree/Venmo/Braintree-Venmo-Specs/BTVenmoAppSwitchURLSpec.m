4f0a0abcc3d98839457f790fb600ccb25d8d0f2b
Brent Fitzgerald <brent.fitzgerald@getbraintree.com>
2014-08-25 12:57:24 -0700

    Fix NSApplication -> UIApplication typo in Venmo tests

--- Braintree/Venmo/Braintree-Venmo-Specs/BTVenmoAppSwitchURLSpec.m
+++ Braintree/Venmo/Braintree-Venmo-Specs/BTVenmoAppSwitchURLSpec.m
@@ -1,17 +1,18 @@
 #import "BTVenmoAppSwitchURL.h"
+#import <UIKit/UIKit.h>
 
 SpecBegin(BTVenmoAppSwitchURL)
 
 describe(@"isAppSwitchAvailable", ^{
     it(@"returns YES if application says the Venmo app is available", ^{
-        id application = [OCMockObject mockForClass:[NSApplication class]];
+        id application = [OCMockObject mockForClass:[UIApplication class]];
         [[[application expect] andReturnValue:@YES] canOpenURL:OCMOCK_ANY];
         expect([BTVenmoAppSwitchURL isAppSwitchAvailable]).to.beTruthy();
         [application verify];
         [application stopMocking];
     });
     it(@"returns YES if application says the Venmo app is not available", ^{
-        id application = [OCMockObject mockForClass:[NSApplication class]];
+        id application = [OCMockObject mockForClass:[UIApplication class]];
         [[[application expect] andReturnValue:@NO] canOpenURL:OCMOCK_ANY];
         expect([BTVenmoAppSwitchURL isAppSwitchAvailable]).to.beFalsy();
         [application verify];
