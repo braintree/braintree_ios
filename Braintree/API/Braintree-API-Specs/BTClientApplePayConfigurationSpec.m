c70e6aeb1f7b3a2d62aa5ad4300fb24c7f98c799
Brent Fitzgerald <brent.fitzgerald@getbraintree.com>
2014-09-26 15:17:10 -0700

    Log warnings for Apple Pay configuration that is deprecated

--- Braintree/API/Braintree-API-Specs/BTClientApplePayConfigurationSpec.m
+++ Braintree/API/Braintree-API-Specs/BTClientApplePayConfigurationSpec.m
@@ -1,32 +1,32 @@
 #import "BTClientApplePayConfiguration.h"
-#import "BTLogger.h"
+#import "BTLogger_Internal.h"
 
 SpecBegin(BTClientApplePayConfiguration)
 
 describe(@"initWithConfigurationObject:", ^{
 
     context(@"when argument is a dictionary", ^{
-        it(@"is initialized with a dictionary", ^{
+        it(@"is initialized", ^{
             BTClientApplePayConfiguration *configuration = [[BTClientApplePayConfiguration alloc] initWithConfigurationObject:@{}];
             expect(configuration.status).to.equal(BTClientApplePayStatusOff);
         });
 
-        it(@"is initialized and has status off when status is 'off'", ^{
+        it(@"has status off when status is 'off'", ^{
             BTClientApplePayConfiguration *configuration = [[BTClientApplePayConfiguration alloc] initWithConfigurationObject:@{@"status": @"off"}];
             expect(configuration.status).to.equal(BTClientApplePayStatusOff);
         });
 
-        it(@"is initialized and has status off when status is 'I AM ON'", ^{
+        it(@"has status off when status is 'I AM ON'", ^{
             BTClientApplePayConfiguration *configuration = [[BTClientApplePayConfiguration alloc] initWithConfigurationObject:@{@"status": @"i am off"}];
             expect(configuration.status).to.equal(BTClientApplePayStatusOff);
         });
 
-        it(@"is initialized and has status mock when status is 'mock'", ^{
+        it(@"has status mock when status is 'mock'", ^{
             BTClientApplePayConfiguration *configuration = [[BTClientApplePayConfiguration alloc] initWithConfigurationObject:@{@"status": @"mock"}];
             expect(configuration.status).to.equal(BTClientApplePayStatusMock);
         });
 
-        it(@"is initialized and has status production when status is 'production'", ^{
+        it(@"has status production when status is 'production'", ^{
             BTClientApplePayConfiguration *configuration = [[BTClientApplePayConfiguration alloc] initWithConfigurationObject:@{@"status": @"production"}];
             expect(configuration.status).to.equal(BTClientApplePayStatusProduction);
         });
@@ -40,6 +40,17 @@ describe(@"initWithConfigurationObject:", ^{
             BTClientApplePayConfiguration *configuration = [[BTClientApplePayConfiguration alloc] initWithConfigurationObject:@{@"merchantId": @"1234"}];
             expect(configuration.merchantId).to.equal(@"1234");
         });
+
+        it(@"logs a warning", ^{
+            id mockLogger = [OCMockObject mockForClass:[BTLogger class]];
+            [[[mockLogger stub] andReturn:mockLogger] sharedLogger];
+            [[mockLogger stub] info:OCMOCK_ANY];
+            [[mockLogger expect] warning:OCMOCK_ANY];
+            __unused id _ = [[BTClientApplePayConfiguration alloc] initWithConfigurationObject:@{}];
+            [mockLogger verify];
+            [mockLogger stopMocking];
+        });
+
     });
 
     context(@"when argument is a string", ^{
@@ -73,6 +84,15 @@ describe(@"initWithConfigurationObject:", ^{
             expect(configuration.merchantId).to.beNil();
         });
 
+        it(@"logs a warning", ^{
+            id mockLogger = [OCMockObject mockForClass:[BTLogger class]];
+            [[[mockLogger stub] andReturn:mockLogger] sharedLogger];
+            [[mockLogger stub] info:OCMOCK_ANY];
+            [[mockLogger expect] warning:OCMOCK_ANY];
+            __unused id _ = [[BTClientApplePayConfiguration alloc] initWithConfigurationObject:@"mock"];
+            [mockLogger verify];
+            [mockLogger stopMocking];
+        });
     });
 
     context(@"when argument is nil", ^{
--- Braintree/API/Braintree-API/Models/BTClientApplePayConfiguration.m
+++ Braintree/API/Braintree-API/Models/BTClientApplePayConfiguration.m
@@ -1,29 +1,34 @@
 #import "BTClientApplePayConfiguration.h"
+#import "BTLogger_Internal.h"
 
 @implementation BTClientApplePayConfiguration
 
 - (instancetype)initWithConfigurationObject:(id)configurationObject {
     self = [super init];
     if (self) {
-
+        NSString *statusString;
         if ([configurationObject respondsToSelector:@selector(objectForKeyedSubscript:)]) {
-            // merchant ID
+            [[BTLogger sharedLogger] warning:@"This Apple Pay configuration is deprecated. Upcoming service changes will disable Apple Pay for this SDK version."];
             if ([configurationObject[@"merchantId"] isKindOfClass:[NSString class]]) {
                 _merchantId = [configurationObject[@"merchantId"] copy];
             }
-
-            // status
-            NSString *statusString = configurationObject[@"status"];
+            statusString = configurationObject[@"status"];
             _status = [[self class] statusTypeForString:statusString];
-
         } else if ([configurationObject isKindOfClass:[NSString class]]) {
-            _status = [[self class] statusTypeForString:configurationObject];
+            [[BTLogger sharedLogger] warning:@"This Apple Pay configuration is deprecated. Upcoming service changes will disable Apple Pay for this SDK version."];
+            statusString = configurationObject;
+            _status = [[self class] statusTypeForString:statusString];
             if (_status == BTClientApplePayStatusMock) {
                 _merchantId = @"mock-merchant-id";
             }
         } else {
+            if (configurationObject != nil) {
+                [[BTLogger sharedLogger] error:@"Encountered an unexpected Apple Pay configuration format."];
+            }
+            statusString = @"off";
             _status = BTClientApplePayStatusOff;
         }
+        [[BTLogger sharedLogger] info:@"Using '%@' Apple Pay for merchant '%@'", statusString, _merchantId];
     }
     return self;
 }
@@ -36,7 +41,6 @@
     } else {
         return BTClientApplePayStatusOff;
     }
-
 }
 
 @end
