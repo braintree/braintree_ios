f5388796f43ad6e5936da60b3fb87bd03247b514
Mickey Reiss <mickey@venmo.com>
2014-10-03 10:08:25 -0700

    Clean up cruft in BTTestClientTokenFactory

--- Braintree/API/Braintree-API-Specs/BTTestClientTokenFactory.h
+++ Braintree/API/Braintree-API-Specs/BTTestClientTokenFactory.h
@@ -8,24 +8,4 @@
 
 + (NSMutableDictionary *)configuration;
 
-#pragma mark Deprecated
-
-//+ (NSString *)token;
-//+ (NSString *)tokenWithAnalyticsUrl:(NSString *)analyticsUrl;
-//+ (NSString *)base64EncodedToken;
-//+ (NSString *)base64EncodedTokenWithMerchantId:(NSString *)merchantId;
-//+ (NSString *)base64EncodedTokenFromDictionary:(NSDictionary *)dictionary;
-//+ (NSString *)tokenWithPayPalClientId;
-//+ (NSString *)invalidToken;
-//+ (NSString *)tokenWithoutCustomerIdentifier;
-//+ (NSString *)tokenWithoutAuthorizationUrl;
-//+ (NSString *)tokenWithoutAuthorizationFingerprint;
-//+ (NSString *)tokenWithoutClientApiUrl;
-//+ (NSString *)tokenWithBlankAuthorizationUrl;
-//+ (NSString *)tokenWithBlankAuthorizationFingerprint;
-//+ (NSString *)tokenWithBlankClientApiUrl;
-//
-//+ (NSString *)tokenWithTestURLs;
-//+ (NSString *)testURLScheme;
-
 @end
