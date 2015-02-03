a456ce3355dafff0da0b20469ecf8f3a99036e08
Mickey Reiss <mickey@venmo.com>
2015-02-03 10:33:27 -0800

    Fix bug in BTClientPaymentMethodValueTransformer
    When BTClientPaymentMethodValueTransformer was merged, there was a
    conflict with the recent changes to make Apple Pay optional. This commit
    makes the payment method value transformer compatible with excluding
    Apple Pay.

--- Braintree/API/Client/BTClientPaymentMethodValueTransformer.m
+++ Braintree/API/Client/BTClientPaymentMethodValueTransformer.m
@@ -20,57 +20,59 @@
         return nil;
     }
 
-    BTAPIResponseParser *r = [BTAPIResponseParser parserWithDictionary:value];
+    BTAPIResponseParser *responseParser = [BTAPIResponseParser parserWithDictionary:value];
 
-    BTPaymentMethod *p;
+    BTPaymentMethod *paymentMethod;
 
-    NSString *type = [r stringForKey:@"type"];
+    NSString *type = [responseParser stringForKey:@"type"];
     if ([type isEqualToString:@"CreditCard"]) {
         BTMutableCardPaymentMethod *card = [[BTMutableCardPaymentMethod alloc] init];
 
-        card.description = [r stringForKey:@"description"];
-        card.typeString = [r[@"details"] stringForKey:@"cardType"];
-        card.lastTwo = [r[@"details"] stringForKey:@"lastTwo"];
-        card.challengeQuestions = [r setForKey:@"securityQuestions"];
-        card.nonce = [r stringForKey:@"nonce"];
+        card.description = [responseParser stringForKey:@"description"];
+        card.typeString = [responseParser[@"details"] stringForKey:@"cardType"];
+        card.lastTwo = [responseParser[@"details"] stringForKey:@"lastTwo"];
+        card.challengeQuestions = [responseParser setForKey:@"securityQuestions"];
+        card.nonce = [responseParser stringForKey:@"nonce"];
 
-        p = card;
+        paymentMethod = card;
     } else if ([type isEqualToString:@"PayPalAccount"]) {
         BTMutablePayPalPaymentMethod *payPal = [[BTMutablePayPalPaymentMethod alloc] init];
 
-        payPal.nonce = [r stringForKey:@"nonce"];
-        payPal.email = [r[@"details"] stringForKey:@"email"];
+        payPal.nonce = [responseParser stringForKey:@"nonce"];
+        payPal.email = [responseParser[@"details"] stringForKey:@"email"];
 
         // Braintree gateway has some inconsistent behavior depending on
         // the type of nonce, and sometimes returns "PayPal" for description,
         // and sometimes returns a real identifying string. The former is not
         // desirable for display. The latter is.
         // As a workaround, we ignore descriptions that look like "PayPal".
-        id description = [r stringForKey:@"description"];
+        id description = [responseParser stringForKey:@"description"];
         if (![[description lowercaseString] isEqualToString:@"paypal"]) {
             payPal.description = description;
         }
 
-        p = payPal;
+        paymentMethod = payPal;
+#ifdef BT_ENABLE_APPLE_PAY
     } else if ([type isEqualToString:@"ApplePayPayment"]) {
         BTMutableApplePayPaymentMethod *card = [[BTMutableApplePayPaymentMethod alloc] init];
 
-        card.description = [r stringForKey:@"description"];
-        card.challengeQuestions = [r setForKey:@"securityQuestions"];
-        card.nonce = [r stringForKey:@"nonce"];
+        card.nonce = [responseParser stringForKey:@"nonce"];
+        card.description = [responseParser stringForKey:@"description"];
+        card.challengeQuestions = [responseParser setForKey:@"securityQuestions"];
 
-        p = card;
+        paymentMethod = card;
+#endif
     } else {
-        BTMutablePaymentMethod *paymentMethod = [[BTMutablePaymentMethod alloc] init];
+        BTMutablePaymentMethod *genericPaymentMethod = [[BTMutablePaymentMethod alloc] init];
 
-        paymentMethod.nonce = [r stringForKey:@"nonce"];
-        paymentMethod.description = [r stringForKey:@"nonce"];
-        paymentMethod.challengeQuestions = [r setForKey:@"securityQuestions"];
+        genericPaymentMethod.nonce = [responseParser stringForKey:@"nonce"];
+        genericPaymentMethod.description = [responseParser stringForKey:@"description"];
+        genericPaymentMethod.challengeQuestions = [responseParser setForKey:@"securityQuestions"];
 
-        p = paymentMethod;
+        paymentMethod = genericPaymentMethod;
     }
     
-    return p;
+    return paymentMethod;
 }
 
 @end
