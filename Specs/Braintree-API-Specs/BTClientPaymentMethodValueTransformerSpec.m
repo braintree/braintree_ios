f7140d5fd433f96c5caae7ecbb088a1a3b4888f7
dmerino and intelliot <david.merino@getbraintree.com , elliot.lee@getbraintree.com>
2015-03-17 16:50:23 -0700

    Add tests of BTClientPaymentMethodValueTransformer's interpretation of CoinbaseAccount's description

--- Specs/Braintree-API-Specs/BTClientPaymentMethodValueTransformerSpec.m
+++ Specs/Braintree-API-Specs/BTClientPaymentMethodValueTransformerSpec.m
@@ -1,5 +1,6 @@
 #import "BTClientPaymentMethodValueTransformer.h"
 #import "BTPayPalPaymentMethod.h"
+#import "BTCoinbasePaymentMethod.h"
 
 SpecBegin(BTClientPaymentMethodValueTransformer)
 
@@ -7,35 +8,86 @@ __block NSMutableDictionary *responseDictionary;
 __block NSValueTransformer *valueTransformer;
 
 beforeEach(^{
-    responseDictionary = [NSMutableDictionary dictionaryWithDictionary:@{ @"type": @"PayPalAccount",
-                                                                          @"nonce": @"a-nonce-value",
-                                                                          @"details": @{@"email": @"email@foo.bar"}}];
+    responseDictionary = [NSMutableDictionary dictionaryWithDictionary:@{
+                                                                         @"nonce": @"a-nonce-value",
+                                                                         @"details": @{@"email": @"email@foo.bar"}}];
     valueTransformer = [NSValueTransformer valueTransformerForName:NSStringFromClass([BTClientPaymentMethodValueTransformer class])];
 });
 
-it(@"returns a PayPal payment method with nil description if description is null", ^{
-    responseDictionary[@"description"] = [NSNull null];
-    BTPayPalPaymentMethod *paymentMethod = [valueTransformer transformedValue:responseDictionary];
-    expect(paymentMethod).to.beKindOf([BTPayPalPaymentMethod class]);
-    expect(paymentMethod.nonce).to.equal(@"a-nonce-value");
+context(@"PayPal", ^{
+
+    beforeEach(^{
+        responseDictionary[@"type"] = @"PayPalAccount";
+    });
+
+    it(@"returns a PayPal payment method with a nonce even if description is null", ^{
+        responseDictionary[@"description"] = [NSNull null];
+        //[responseDictionary removeObjectForKey:@"description"];
+        BTPayPalPaymentMethod *paymentMethod = [valueTransformer transformedValue:responseDictionary];
+        expect(paymentMethod).to.beKindOf([BTPayPalPaymentMethod class]);
+        expect(paymentMethod.nonce).to.equal(@"a-nonce-value");
+    });
+
+    it(@"returns a PayPal payment method with nil description if description is null", ^{
+        responseDictionary[@"description"] = [NSNull null];
+        BTPayPalPaymentMethod *paymentMethod = [valueTransformer transformedValue:responseDictionary];
+        expect(paymentMethod.description).to.beNil();
+    });
+
+    it(@"returns a PayPal payment method with nil description if description is 'PayPal'", ^{
+        responseDictionary[@"description"] = @"PayPal";
+        BTPayPalPaymentMethod *paymentMethod = [valueTransformer transformedValue:responseDictionary];
+        expect(paymentMethod.description).to.beNil();
+    });
+
+    it(@"returns a PayPal payment method with the description if description is not 'PayPal' and non-nil", ^{
+        responseDictionary[@"description"] = @"foo";
+        BTPayPalPaymentMethod *paymentMethod = [valueTransformer transformedValue:responseDictionary];
+        expect(paymentMethod.description).equal(@"foo");
+    });
 });
 
-it(@"returns a PayPal payment method with nil description if description is null", ^{
-    responseDictionary[@"description"] = [NSNull null];
-    BTPayPalPaymentMethod *paymentMethod = [valueTransformer transformedValue:responseDictionary];
-    expect(paymentMethod.description).to.beNil();
-});
+context(@"Coinbase", ^{
 
-it(@"returns a PayPal payment method with nil description if description is 'PayPal'", ^{
-    responseDictionary[@"description"] = @"PayPal";
-    BTPayPalPaymentMethod *paymentMethod = [valueTransformer transformedValue:responseDictionary];
-    expect(paymentMethod.description).to.beNil();
-});
+    beforeEach(^{
+        responseDictionary[@"type"] = @"CoinbaseAccount";
+    });
 
-it(@"returns a PayPal payment method with the description if description is not 'PayPal' and non-nil", ^{
-    responseDictionary[@"description"] = @"foo";
-    BTPayPalPaymentMethod *paymentMethod = [valueTransformer transformedValue:responseDictionary];
-    expect(paymentMethod.description).equal(@"foo");
+    it(@"returns a payment method with a nonce even if description is null", ^{
+        responseDictionary[@"description"] = [NSNull null];
+        BTCoinbasePaymentMethod *paymentMethod = [valueTransformer transformedValue:responseDictionary];
+        expect(paymentMethod).to.beKindOf([BTCoinbasePaymentMethod class]);
+        expect(paymentMethod.nonce).to.equal(@"a-nonce-value");
+    });
+
+    it(@"returns a payment method with nil description if description is null", ^{
+        responseDictionary[@"description"] = [NSNull null];
+        BTCoinbasePaymentMethod *paymentMethod = [valueTransformer transformedValue:responseDictionary];
+        expect(paymentMethod.description).to.beNil();
+    });
+
+    it(@"returns a payment method with nil description if description is 'Coinbase' and email is null", ^{
+        responseDictionary[@"description"] = @"Coinbase";
+        responseDictionary[@"details"] = @{@"email": [NSNull null]};
+        BTCoinbasePaymentMethod *paymentMethod = [valueTransformer transformedValue:responseDictionary];
+        expect(paymentMethod.description).to.beNil();
+        expect(paymentMethod.userIdentifier).to.beNil();
+    });
+
+    it(@"returns a payment method with email as description if description is 'Coinbase'", ^{
+        responseDictionary[@"description"] = @"Coinbase";
+        BTCoinbasePaymentMethod *paymentMethod = [valueTransformer transformedValue:responseDictionary];
+        expect(paymentMethod.description).to.equal(@"email@foo.bar");
+        expect(paymentMethod.userIdentifier).to.equal(@"email@foo.bar");
+    });
+
+    // (Not currently used by the gateway, which returns "Coinbase" for all CoinbaseAccounts)
+    it(@"returns a payment method with description as description if description is not 'Coinbase' and non-nil", ^{
+        responseDictionary[@"description"] = @"Satoshi Nakamoto";
+        BTCoinbasePaymentMethod *paymentMethod = [valueTransformer transformedValue:responseDictionary];
+        expect(paymentMethod.description).to.equal(@"Satoshi Nakamoto");
+        expect(paymentMethod.userIdentifier).to.equal(@"email@foo.bar");
+    });
 });
 
 SpecEnd
