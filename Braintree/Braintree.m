4839a56f03142342b96cd26a54f82ab0a42c04c5
Mickey Reiss <mickey.reiss@getbraintree.com>
2015-04-14 15:40:13 -0700

    Improve error message or exception for improper manual Apple Pay integrations
    If a developer does a manual integration with a dynamic framework
    target, and neglects to set `BT_ENABLE_APPLE_PAY=1` in your framework
    Build Settings, but do so in the app target, they'd receive a
    run-time exception for "tokenizeApplePayPayment:completion: is not
    defined".
    
    This change should improve the error messaging in this integration
    error case.

--- Braintree/Braintree.m
+++ Braintree/Braintree.m
@@ -124,7 +124,18 @@
                                  }
                              }];
 }
+#else
+- (void)tokenizeApplePayPayment:(id)payment
+                     completion:(void (^)(NSString *, NSError *))completionBlock {
+    NSString *message = @"Apple Pay is not compiled into this integration of Braintree. Please ensure that BT_ENABLE_APPLE_PAY=1 in your framework and app targets.";
+    [[BTLogger sharedLogger] warning:message];
+#if DEBUG
+    @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                   reason:message
+                                 userInfo:nil];
+    return;
 #endif
+}
 
 - (BTPaymentProvider *)paymentProviderWithDelegate:(id<BTPaymentMethodCreationDelegate>)delegate {
     BTPaymentProvider *paymentProvider = [[BTPaymentProvider alloc] initWithClient:self.client];
