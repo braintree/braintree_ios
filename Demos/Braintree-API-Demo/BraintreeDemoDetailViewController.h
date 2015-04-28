be17919534a22d70b9cd4f5d6d9a0e846f449969
Mickey Reiss <mickey.reiss@getbraintree.com>
2015-04-27 20:27:28 -0700

    Remove Braintree-API-Demo
    🔥

--- Demos/Braintree-API-Demo/BraintreeDemoDetailViewController.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-#import "BraintreeDemoOperationManager.h"
-
-@interface BraintreeDemoDetailViewController : UITableViewController
-
-@property (strong, nonatomic) BraintreeDemoClientOperation *selectedOperation;
-
-@end
