b548d4f02469e775febbaf9ca07c13982dd0ccae
richardshin <richard.shin@getbraintree.com>
2015-06-26 11:53:25 -0700

    Fix BTAppSwitch unit tests

--- Specs/Braintree-API-Specs/BTAppSwitchSpec.m
+++ Specs/Braintree-API-Specs/BTAppSwitchSpec.m
@@ -30,7 +30,7 @@ describe(@"handleReturnURL:sourceApplication:", ^{
         it(@"should return YES", ^{
             BTAppSwitch *appSwitch;
             appSwitch = [[BTAppSwitch alloc] init];
-            [appSwitch addAppSwitching:happyAppSwitcher forPaymentType:0];
+            [appSwitch addAppSwitching:happyAppSwitcher forApp:0];
             BOOL handled = [appSwitch handleReturnURL:[NSURL new] sourceApplication:@""];
             expect(handled).to.beTruthy();
         });
@@ -52,7 +52,7 @@ describe(@"handleReturnURL:sourceApplication:", ^{
         it(@"should return NO", ^{
             BTAppSwitch *appSwitch;
             appSwitch = [[BTAppSwitch alloc] init];
-            [appSwitch addAppSwitching:sadAppSwitcher forPaymentProvider:0];
+            [appSwitch addAppSwitching:sadAppSwitcher forApp:0];
             BOOL handled = [appSwitch handleReturnURL:[NSURL new] sourceApplication:@""];
             expect(handled).to.beFalsy();
         });
