7fe6614f8f749928db9c39ed59c414f1ac4b415c
Elliot Lee <elliot.lee@getbraintree.com>
2015-10-27 16:30:05 -0700

    Remove Coinbase Demos
    - BraintreeDemoBTUICoinbaseButtonViewController
    - BraintreeDemoCustomCoinbaseButtonViewController

--- Demo/Features/Coinbase - BTUICoinbaseButton/BraintreeDemoBTUICoinbaseButtonViewController.h
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-
-#import "BraintreeDemoPaymentButtonBaseViewController.h"
-
-@interface BraintreeDemoBTUICoinbaseButtonViewController : BraintreeDemoPaymentButtonBaseViewController
-@end
