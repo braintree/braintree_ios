064522397994ba765dc671b09b9b29ba28119042
David Merino <david.merino@getbraintree.com>
2015-12-18 10:04:23 -0800

    Add sleep(2) after moving to webview. Add tests for Billing Agreements.

--- UITests/BraintreePayPal_UITests.swift
+++ UITests/BraintreePayPal_UITests.swift
@@ -10,6 +10,7 @@ class BraintreePayPal_FuturePayment_UITests: BTUITest {
         app.launchArguments.append("-Integration:BraintreeDemoCustomPayPalButtonViewController")
         app.launch()
         app.buttons["PayPal (custom button)"].forceTapElement()
+        sleep(2)
     }
     
     override func tearDown() {
@@ -21,7 +22,6 @@ class BraintreePayPal_FuturePayment_UITests: BTUITest {
         let app = XCUIApplication()
 
         self.waitForElementToAppear(app.webViews.textFields["Email"])
-        sleep(1)
 
         app.webViews.textFields["Email"].forceTapElement()
         sleep(1)
@@ -44,9 +44,8 @@ class BraintreePayPal_FuturePayment_UITests: BTUITest {
     
     func testPayPal_futurePayment_cancelsSuccessfully() {
         let app = XCUIApplication()
-
+        
         self.waitForElementToAppear(app.webViews.textFields["Email"])
-        sleep(1)
 
         // Close button has no accessibility helper
         app.webViews.buttons.elementBoundByIndex(0).forceTapElement()
@@ -67,6 +66,7 @@ class BraintreePayPal_SinglePayment_UITests: BTUITest {
         app.launchArguments.append("-Integration:BraintreeDemoPayPalOneTimePaymentViewController")
         app.launch()
         app.buttons["PayPal one-time payment"].forceTapElement()
+        sleep(2)
     }
     
     override func tearDown() {
@@ -93,6 +93,48 @@ class BraintreePayPal_SinglePayment_UITests: BTUITest {
         app.webViews.links["Cancel Sandbox Purchase"].forceTapElement()
         
         self.waitForElementToAppear(app.buttons["PayPal one-time payment"])
+        
+        XCTAssertTrue(app.buttons["Cancelled"].exists);
+    }
+}
+
+class BraintreePayPal_BillingAgreement_UITests: BTUITest {
+    
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        let app = XCUIApplication()
+        app.launchArguments.append("-EnvironmentSandbox")
+        app.launchArguments.append("-Integration:BraintreeDemoPayPalBillingAgreementViewController")
+        app.launch()
+        app.buttons["Billing Agreement with PayPal"].forceTapElement()
+        sleep(2)
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testPayPal_billingAgreement_receivesNonce() {
+        let app = XCUIApplication()
+        
+        self.waitForElementToAppear(app.webViews.links["Proceed with Sandbox Purchase"])
+        
+        app.webViews.links["Proceed with Sandbox Purchase"].forceTapElement()
+        
+        self.waitForElementToAppear(app.buttons["Got a nonce. Tap to make a transaction."])
+        
+        XCTAssertTrue(app.buttons["Got a nonce. Tap to make a transaction."].exists);
+    }
+    
+    func testPayPal_billingAgreement_cancelsSuccessfully() {
+        let app = XCUIApplication()
+        
+        self.waitForElementToAppear(app.webViews.links["Cancel Sandbox Purchase"])
+        
+        app.webViews.links["Cancel Sandbox Purchase"].forceTapElement()
+        
+        self.waitForElementToAppear(app.buttons["Billing Agreement with PayPal"])
         
         XCTAssertTrue(app.buttons["Cancelled"].exists);
     }
