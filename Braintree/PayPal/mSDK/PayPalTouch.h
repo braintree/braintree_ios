b86841eafffccc4831331e203f650fa0d51d3eba
Bryant Luk <bryant.luk@getbraintree.com>
2016-03-02 14:41:37 -0600

    Update PayPal mSDK to 2.14.0-bt1

--- Braintree/PayPal/mSDK/PayPalTouch.h
+++ Braintree/PayPal/mSDK/PayPalTouch.h
@@ -1,9 +1,9 @@
 //
 //  PayPalTouch.h
 //
-//  Version 2.12.1-bt1
+//  Version 2.14.0-bt1
 //
-//  Copyright (c) 2014, PayPal
+//  Copyright (c) 2014-2016 PayPal, Inc. All rights reserved.
 //  All rights reserved.
 //
 
@@ -22,12 +22,12 @@ typedef NS_ENUM(NSUInteger, PayPalTouchResultType) {
 @property (nonatomic, readonly, assign) PayPalTouchResultType resultType;
 /// When App Switch is successful the authorization dictionary
 /// containing information that your server will need to process the payment.
-@property (nonatomic, readonly, copy) NSDictionary *authorization;
+@property (nullable, nonatomic, readonly, copy) NSDictionary *authorization;
 /// This dictionary is always present when resultType is PayPalTouchResultTypeError.
 /// This dictionary is sometimes present when resultType is PayPalTouchResultTypeCancel.
 /// The "message" and "debug_id" are for developer integaration to help analyze a problem,
 /// and never for user display.
-@property (nonatomic, readonly, copy) NSDictionary *error;
+@property (nullable, nonatomic, readonly, copy) NSDictionary *error;
 
 @end
 
@@ -47,7 +47,7 @@ typedef NS_ENUM(NSUInteger, PayPalTouchResultType) {
 /// and the current app is set up to receive the callback.
 ///
 /// @return YES if the authorization method is available
-+ (BOOL)canAppSwitchForUrlScheme:(NSString*) scheme;
++ (BOOL)canAppSwitchForUrlScheme:(nonnull NSString*) scheme;
 
 /// Obtain future payments authorization via app switch.
 ///
@@ -56,13 +56,13 @@ typedef NS_ENUM(NSUInteger, PayPalTouchResultType) {
 ///
 /// @param configuration The configuration to be used for the app switch
 /// @return YES if app switch was successful
-+ (BOOL)authorizeFuturePayments:(PayPalConfiguration *)configuration __attribute__((deprecated("Please use authorizeScopeValues:configuration: instead")));
++ (BOOL)authorizeFuturePayments:(nonnull PayPalConfiguration *)configuration __attribute__((deprecated("Please use authorizeScopeValues:configuration: instead")));
 
 /// @param scopeValues Set of requested scope-values. Each scope-value is defined in PayPalOAuthScopes.h.
 /// @param configuration The configuration to be used for the app switch
 /// @return YES if app switch was successful
 /// @note Given limitations in the Wallet app, the only scopes allowed are: https://uri.../futurepayments, email, address, phone, openid
-+ (BOOL)authorizeScopeValues:(NSSet *)scopeValues configuration:(PayPalConfiguration *)configuration;
++ (BOOL)authorizeScopeValues:(nonnull NSSet *)scopeValues configuration:(nonnull PayPalConfiguration *)configuration;
 
 /// Process a URL request.
 ///
@@ -72,7 +72,7 @@ typedef NS_ENUM(NSUInteger, PayPalTouchResultType) {
 /// @param  url   The url to handle
 /// @return PayPalTouchResult containing result of the App Switch.
 ///
-+ (PayPalTouchResult *)parseAppSwitchURL:(NSURL *)url;
++ (nonnull PayPalTouchResult *)parseAppSwitchURL:(nonnull NSURL *)url;
 
 #pragma mark - Custom URL handling
 
@@ -87,6 +87,6 @@ typedef NS_ENUM(NSUInteger, PayPalTouchResultType) {
 /// @param  sourceApplication The bundle ID of the source application
 ///
 /// @return Whether the application that made the request is valid.
-+ (BOOL)canHandleURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication;
++ (BOOL)canHandleURL:(nonnull NSURL *)url sourceApplication:(nonnull NSString *)sourceApplication;
 
 @end
