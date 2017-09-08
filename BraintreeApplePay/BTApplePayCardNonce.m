a11226cc1fffa815ffe22f79b5a8abfb0d158655
rksaraf <rksaraf@gmail.com>
2017-09-08 10:20:29 -0700

    adding the original init method back and deprecating it

--- BraintreeApplePay/BTApplePayCardNonce.m
+++ BraintreeApplePay/BTApplePayCardNonce.m
@@ -14,4 +14,14 @@
     return self;
 }
 
+#pragma mark - Deprecated methods
+
+- (instancetype)initWithNonce:(NSString *)nonce localizedDescription:(NSString *)description type:(NSString *)type json:(BTJSON *)json {
+    self = [super initWithNonce:nonce localizedDescription:description type:type];
+    if (self) {
+        _binData = [[BTBinData alloc] initWithJSON:json[@"binData"]];
+    }
+    return self;
+}
+
 @end
