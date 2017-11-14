2ee1434450cb4eb08637e07826c0c669ce8db408
Unknown <rksaraf@gmail.com>
2017-11-14 15:12:27 +0700

    bringing back the old default method for ApplePayCardNonce

--- BraintreeApplePay/BTApplePayCardNonce.m
+++ BraintreeApplePay/BTApplePayCardNonce.m
@@ -2,22 +2,8 @@
 
 @implementation BTApplePayCardNonce
 
-- (instancetype)initWithNonce:(NSString *)nonce
-         localizedDescription:(NSString *)description
-                         type:(NSString *)type
-                    isDefault:(BOOL)isDefault
-                         json:(BTJSON *)json {
-    self = [super initWithNonce:nonce localizedDescription:description type:type isDefault:isDefault];
-    if (self) {
-        _binData = [[BTBinData alloc] initWithJSON:json[@"binData"]];
-    }
-    return self;
-}
-
-#pragma mark - Deprecated methods
-
 - (instancetype)initWithNonce:(NSString *)nonce localizedDescription:(NSString *)description type:(NSString *)type json:(BTJSON *)json {
-    self = [super initWithNonce:nonce localizedDescription:description type:type];
+    self = [super initWithNonce:nonce localizedDescription:description type:type isDefault:[json[@"default"] isTrue]];
     if (self) {
         _binData = [[BTBinData alloc] initWithJSON:json[@"binData"]];
     }
