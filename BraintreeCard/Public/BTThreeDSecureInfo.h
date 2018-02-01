3488dbfaef6e352958f8e663eb7f26bf9a8e38f7
dmerino <code@getbraintree.com>
2018-02-01 14:31:22 -0800

    Fix import in BTThreeDSecureInfo

--- BraintreeCard/Public/BTThreeDSecureInfo.h
+++ BraintreeCard/Public/BTThreeDSecureInfo.h
@@ -1,5 +1,9 @@
 #import <Foundation/Foundation.h>
-#import "BTJSON.h"
+#if __has_include("BraintreeCore.h")
+#import "BraintreeCore.h"
+#else
+#import <BraintreeCore/BraintreeCore.h>
+#endif
 
 NS_ASSUME_NONNULL_BEGIN
 
