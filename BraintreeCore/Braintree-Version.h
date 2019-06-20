d9fa34cd3a92b8275e78c5c10c3bff7605827ab4
dmerino <code@getbraintree.com>
2019-06-20 10:25:12 -0700

    Bump pod version to 4.23.2

--- BraintreeCore/Braintree-Version.h
+++ BraintreeCore/Braintree-Version.h
@@ -1,1 +1,1 @@
-#define BRAINTREE_VERSION (@"4.23.1")
+#define BRAINTREE_VERSION (@"4.23.2")
--- BraintreePayPal/PayPalUtils/Public/PPOTVersion.h
+++ BraintreePayPal/PayPalUtils/Public/PPOTVersion.h
@@ -6,6 +6,6 @@
 #ifndef PPOTVersion_h
 #define PPOTVersion_h
 
-#define PayPalOTVersion() @"4.23.1"
+#define PayPalOTVersion() @"4.23.2"
 
 #endif
