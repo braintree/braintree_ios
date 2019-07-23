61b3797397f0f6194604f140d83719527ecc7b30
Susan Stevens <susan.stevens@getbraintree.com>
2019-07-23 15:31:38 -0500

    Add defensive logic for deployment targets that don't specify a minor version
    Co-authored-by: sestevens <susan.stevens@getbraintree.com>
    Co-authored-by: scannillo <samantha.cannillo@getbraintree.com>

--- UnitTests/BTAnalyticsService_Tests.m
+++ UnitTests/BTAnalyticsService_Tests.m
@@ -263,23 +263,20 @@
 }
 
 - (void)validateMetaParameters:(NSDictionary *)metaParameters {
-    NSString *unitTestDeploymentTargetVersion = [@(__IPHONE_OS_VERSION_MIN_REQUIRED) stringValue];
-    NSString *unitTestBaseSDKVersion = [@(__IPHONE_OS_VERSION_MAX_ALLOWED) stringValue];
-
     XCTAssertEqualObjects(metaParameters[@"deviceManufacturer"], @"Apple");
     XCTAssertEqualObjects(metaParameters[@"deviceModel"], [self deviceModel]);
     XCTAssertEqualObjects(metaParameters[@"deviceAppGeneratedPersistentUuid"], [self deviceAppGeneratedPersistentUuid]);
     XCTAssertEqualObjects(metaParameters[@"deviceScreenOrientation"], @"Portrait");
     XCTAssertEqualObjects(metaParameters[@"integrationType"], @"custom");
-    XCTAssertEqualObjects(metaParameters[@"iosBaseSDK"], unitTestBaseSDKVersion);
-    XCTAssertEqualObjects(metaParameters[@"iosDeploymentTarget"], unitTestDeploymentTargetVersion);
-    XCTAssertEqualObjects(metaParameters[@"iosDeviceName"], [[UIDevice currentDevice] name]);
+    XCTAssertEqualObjects(metaParameters[@"iosBaseSDK"], @(__IPHONE_OS_VERSION_MAX_ALLOWED).stringValue);
+    XCTAssertEqualObjects(metaParameters[@"iosDeploymentTarget"], @"90000");
+    XCTAssertEqualObjects(metaParameters[@"iosDeviceName"], UIDevice.currentDevice.name);
     XCTAssertTrue((BOOL)metaParameters[@"isSimulator"] == TARGET_IPHONE_SIMULATOR);
     XCTAssertEqualObjects(metaParameters[@"merchantAppId"], @"com.braintreepayments.Demo");
     XCTAssertEqualObjects(metaParameters[@"merchantAppName"], @"Braintree iOS SDK Demo");
     XCTAssertEqualObjects(metaParameters[@"sdkVersion"], BRAINTREE_VERSION);
     XCTAssertEqualObjects(metaParameters[@"platform"], @"iOS");
-    XCTAssertEqualObjects(metaParameters[@"platformVersion"], [[UIDevice currentDevice] systemVersion]);
+    XCTAssertEqualObjects(metaParameters[@"platformVersion"], UIDevice.currentDevice.systemVersion);
     XCTAssertNotNil(metaParameters[@"sessionId"]);
     XCTAssertEqualObjects(metaParameters[@"source"], @"unknown");
     XCTAssertTrue([metaParameters[@"venmoInstalled"] isKindOfClass:[NSNumber class]]);
