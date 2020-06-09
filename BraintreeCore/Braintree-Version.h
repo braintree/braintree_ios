b0af85dd88e157e2b33baab374fb1d4a6827badf
Susan Stevens <susan.stevens@getbraintree.com>
2020-06-09 15:37:39 -0500

    Bump pod version to 4.34.0

--- BraintreeCore/Braintree-Version.h
+++ BraintreeCore/Braintree-Version.h
@@ -1,1 +1,1 @@
-#define BRAINTREE_VERSION (@"4.33.0")
+#define BRAINTREE_VERSION (@"4.34.0")
--- BraintreePayPal/PayPalUtils/Public/PPOTVersion.h
+++ BraintreePayPal/PayPalUtils/Public/PPOTVersion.h
@@ -6,6 +6,6 @@
 #ifndef PPOTVersion_h
 #define PPOTVersion_h
 
-#define PayPalOTVersion() @"4.33.0"
+#define PayPalOTVersion() @"4.34.0"
 
 #endif
