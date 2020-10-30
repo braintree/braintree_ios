d98f7f5753d05d81713e5042fdc299773bbc9b46
Susan Stevens <susan.stevens@getbraintree.com>
2020-10-30 09:58:52 -0500

    Remove BraintreeThreeDSecure library and target from Package.swift since we can't support them yet

--- Package.swift
+++ Package.swift
@@ -40,12 +40,6 @@ let package = Package(
             targets: ["BraintreePaymentFlow"]
         ),
         .library(
-            name: "BraintreeThreeDSecure",
-            targets: ["BraintreeThreeDSecure"]
-            //  TODO: uncomment once we have CardinalMobile.xcframework
-            //, "CardinalMobile"]
-        ),
-        .library(
             name: "BraintreePayPal",
             targets: ["BraintreePayPal"]
         )
@@ -110,24 +104,10 @@ let package = Package(
             path: "Frameworks/PPRiskMagnes.xcframework"
         ),
         .target(
-            name: "BraintreeThreeDSecure",
-            dependencies: ["BraintreePaymentFlow"],
-            exclude: ["Info.plist"],
-            publicHeadersPath: "Public"
-        ),
-        .target(
             name: "BraintreePayPal",
             dependencies: ["BraintreeCore", "PayPalDataCollector"],
             exclude: ["Info.plist"],
             publicHeadersPath: "Public"
-        ),
-//        TODO: Get xcframework version of CardinalMobile
-//        .binaryTarget(
-//            name: "CardinalMobile",
-//            path: "Frameworks/CardinalMobile.xcframework"
-//        ),
-
-//        TODO: Add target & library for BraintreeDataCollector (w/ Kount)
-//        Get Kount as .xcframework
+        )
     ]
 )
