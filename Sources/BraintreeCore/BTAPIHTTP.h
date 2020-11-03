33f7a774f9c843ad4cdcc85e9f780b8ab6928234
Susan Stevens <susan.stevens@getbraintree.com>
2020-11-03 15:44:00 -0600

    BraintreeCore: Disable errors for using -style imports for BTHTTP.h since this is the only way SPM can find the file

--- Sources/BraintreeCore/BTAPIHTTP.h
+++ Sources/BraintreeCore/BTAPIHTTP.h
@@ -1,9 +1,17 @@
-#if __has_include(<Braintree/BraintreeCore.h>)
+#if __has_include(<Braintree/BraintreeCore.h>) // CocoaPods
 #import <Braintree/BTHTTP.h>
-#elif SWIFT_PACKAGE
-#import <BTHTTP.h>
-#else
+
+#elif SWIFT_PACKAGE // SPM
+// The only way SPM can find BTHTTP.h is using quoted includes,
+// so we need to silence the warning.
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wquoted-include-in-framework-header"
+#import "BTHTTP.h"
+#pragma clang diagnostic pop
+
+#else // Carthage
 #import <BraintreeCore/BTHTTP.h>
+
 #endif
 
 NS_ASSUME_NONNULL_BEGIN
