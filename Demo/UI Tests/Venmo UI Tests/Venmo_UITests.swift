69ab699b78d92058fc4beb8f706cca32fe96e6bd
Samantha Cannillo <samantha.cannillo@getbraintree.com>
2021-01-22 14:56:51 -0600

    Remove usage of BTTokenizationService from Demo app and UI tests
    * Remove BraintreeDemoCustomMultiPayViewController
    
    * Update Venmo UI test to use BraintreeDemoCustomVenmoButtonViewController
    
    * Always show custom Venmo button in
    BraintreeDemoCustomVenmoButtonViewController for UI test
    
    Signed-off-by: Susan Stevens <susan.stevens@getbraintree.com>

--- Demo/UI Tests/Venmo UI Tests/Venmo_UITests.swift
+++ Demo/UI Tests/Venmo UI Tests/Venmo_UITests.swift
@@ -10,14 +10,14 @@ class Venmo_UITests: XCTestCase {
         app = XCUIApplication()
         app.launchArguments.append("-EnvironmentSandbox")
         app.launchArguments.append("-ClientToken")
-        app.launchArguments.append("-Integration:BraintreeDemoCustomMultiPayViewController")
+        app.launchArguments.append("-Integration:BraintreeDemoCustomVenmoButtonViewController")
         app.launch()
 
-        waitForElementToAppear(app.buttons["Venmo"])
+        waitForElementToAppear(app.buttons["Venmo (custom button)"])
     }
 
-    func testTokenizeVenmo_withBTTokenizationService_returnsErrorWhenVenmoAppNotInstalled() {
-        app.buttons["Venmo"].tap()
+    func testTokenizeVenmo_returnsErrorWhenVenmoAppNotInstalled() {
+        app.buttons["Venmo (custom button)"].tap()
         sleep(2)
 
         XCTAssertTrue(app.buttons["The Venmo app is not installed on this device, or it is not configured or available for app switch."].exists);
