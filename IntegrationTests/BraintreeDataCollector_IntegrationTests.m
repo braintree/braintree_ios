766cca306c213d93f43d7b9d9090dbb01e19cdf5
Samantha Cannillo <samantha.cannillo@getbraintree.com>
2021-02-08 17:17:36 -0600

    Remove unneeded integration tests

--- IntegrationTests/BraintreeDataCollector_IntegrationTests.m
+++ IntegrationTests/BraintreeDataCollector_IntegrationTests.m
@@ -34,29 +34,4 @@
     [self waitForExpectationsWithTimeout:10 handler:nil];
 }
 
-- (void)testCollectCardFraudData_alsoReturnsPayPalFraudData {
-    XCTestExpectation *expectation = [self expectationWithDescription:@"Callback invoked"];
-    [self.dataCollector collectCardFraudData:^(NSString * _Nonnull deviceData) {
-        XCTAssertTrue([deviceData containsString:@"correlation_id"]);
-        XCTAssertTrue([deviceData containsString:@"device_session_id"]);
-        XCTAssertTrue([deviceData containsString:@"fraud_merchant_id"]);
-        [expectation fulfill];
-    }];
-    [self waitForExpectationsWithTimeout:10 handler:nil];
-}
-
-// Test is failing because Kount is no longer async and doesn't return errors
-- (void)pendCollectCardFraudData_whenMerchantIDIsInvalid_invokesErrorCallback {
-    [self.dataCollector setFraudMerchantID:@"-1"];
-    XCTestExpectation *expectation = [self expectationWithDescription:@"Error callback invoked"];
-    
-    [self.dataCollector collectCardFraudData:^(NSString * _Nonnull deviceData) {
-        NSLog(@"%@", deviceData);
-        //XCTAssertEqualObjects(error.localizedDescription, @"Merchant ID formatted incorrectly.");
-        //XCTAssertEqual(error.code, (NSInteger)KDataCollectorErrorCodeBadParameter);
-        [expectation fulfill];
-    }];
-    [self waitForExpectationsWithTimeout:10 handler:nil];
-}
-
 @end
