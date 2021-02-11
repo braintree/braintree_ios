5fe706d3e3cb23bc203b566c2337061bbde8f2b4
Samantha Cannillo <samantha.cannillo@getbraintree.com>
2021-02-11 16:21:36 -0600

    Revert "Trial removing Kount public SPM library"
    This reverts commit 68a48c8013f4e235e0ea761abceb1d87e88ef873.

--- Package.swift
+++ Package.swift
@@ -48,6 +48,10 @@ let package = Package(
             targets: ["BraintreeVenmo"]
         ),
         .library(
+            name: "KountDataCollector",
+            targets: ["KountDataCollector"]
+        ),
+        .library(
             name: "PayPalDataCollector",
             targets: ["PayPalDataCollector"]
         ),
