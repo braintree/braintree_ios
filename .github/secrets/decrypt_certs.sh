4c4bebdb006a7d75e2bb5a86d3fb55b33dfcb0cb
Samantha Cannillo <samantha.cannillo@getbraintree.com>
2021-03-11 14:11:09 -0600

    Add pod install; re-add signing steps

--- .github/secrets/decrypt_certs.sh
+++ .github/secrets/decrypt_certs.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -eo pipefail
 
-echo $TESTFLIGHT_CERTS_DECRYPT_PASSPHRASE
-echo "$TESTFLIGHT_CERTS_DECRYPT_PASSPHRASE"
+# echo $TESTFLIGHT_CERTS_DECRYPT_PASSPHRASE
+# echo "$TESTFLIGHT_CERTS_DECRYPT_PASSPHRASE"
 
-gpg --quiet --batch --yes --decrypt --passphrase="$TESTFLIGHT_CERTS_DECRYPT_PASSPHRASE" --output ./.github/secrets/BTDemo_Base_SDK__App_Store.mobileprovision ./.github/secrets/BTDemo_Base_SDK__App_Store.mobileprovision.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$TESTFLIGHT_CERTS_DECRYPT_PASSPHRASE" --output ./.github/secrets/apple_distribution_key.p12 ./.github/secrets/apple_distribution_key.p12.gpg
+# gpg --quiet --batch --yes --decrypt --passphrase="$TESTFLIGHT_CERTS_DECRYPT_PASSPHRASE" --output ./.github/secrets/BTDemo_Base_SDK__App_Store.mobileprovision ./.github/secrets/BTDemo_Base_SDK__App_Store.mobileprovision.gpg
+# gpg --quiet --batch --yes --decrypt --passphrase="$TESTFLIGHT_CERTS_DECRYPT_PASSPHRASE" --output ./.github/secrets/apple_distribution_key.p12 ./.github/secrets/apple_distribution_key.p12.gpg
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
