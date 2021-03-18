5bfbd20ba0157f6e309facb165dc8f029a0f8dd0
Susan Stevens <susan.stevens@getbraintree.com>
2021-03-18 10:49:30 -0500

    Update Kount to 4.1.4

---
+++ Sources/BraintreeDataCollector/Kount/TouchControls/UIControl+CustomControl.h
@@ -0,0 +1,38 @@
+//
+//  UIControl+CustomControl.h
+//  TouchDemo
+//
+//  Created by Astha Ameta on 19/08/20.
+//  Copyright © 2020 Kount Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (CustomButton)
+
+@end
+
+@interface UISlider (CustomSlider)
+
+@end
+
+@interface UIStepper (CustomStepper)
+
+@end
+
+@interface UIPageControl (CustomPageControl)
+
+@end
+
+@interface UIColorWell (CustomColorWell)
+
+@end
+
+@interface UIScrollView (CustomScrollView)
+
+@end
+
+@interface UITableView (CustomTableView)
+
+@end
+
