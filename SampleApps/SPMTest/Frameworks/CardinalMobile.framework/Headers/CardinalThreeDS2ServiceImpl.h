32ad7f17f5f8d2a826262c961ef330b3760b8b8a
braintreeps <code@getbraintree.com>
2021-05-10 18:50:42 +0000

    Revert "Remove SPMTest app to avoid Carthage timeout"
    This reverts commit 61cbdaff7a733285e006bc8bc4c23bbbfc07fea8.

---
+++ SampleApps/SPMTest/Frameworks/CardinalMobile.framework/Headers/CardinalThreeDS2ServiceImpl.h
@@ -0,0 +1,21 @@
+//
+//  CardinalThreeDS2ServiceImpl.h
+//  CardinalEMVCoSDK
+//
+//  Copyright © 2018 Cardinal Commerce. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ThreeDS2Service.h"
+
+@class ConfigParameters;
+@class UiCustomization;
+
+/**
+ * The CEThreeDS2ServiceImpl interface confronts to ThreeDS2ServiceImpl protocol and is the main 3DS SDK interface.
+ * It shall provide methods to process transactions.
+ */
+
+@interface CardinalThreeDS2ServiceImpl : NSObject<ThreeDS2Service>
+
+@end
