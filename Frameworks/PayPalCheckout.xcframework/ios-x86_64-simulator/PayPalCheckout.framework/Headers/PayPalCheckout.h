9be54e630a806b1e4163e1c13052d23601f6cd2a
sestevens <51723734+sestevens@users.noreply.github.com>
2021-05-20 09:42:41 -0500

    Initial setup for BraintreePayPalNative module (#681)
    * Initial setup for BraintreePayPalNative module
        * Add PayPalCheckout v0.51.0 as a dependency
        * Add BraintreePayPal as a dependency

---
+++ Frameworks/PayPalCheckout.xcframework/ios-x86_64-simulator/PayPalCheckout.framework/Headers/PayPalCheckout.h
@@ -0,0 +1,17 @@
+//
+//  PayPalCheckout.h
+//  PayPalCheckout
+//
+//  Created by Winton, Cody on 8/28/19.
+//  Copyright © 2019 PayPal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PayPalCheckout.
+FOUNDATION_EXPORT double PayPalCheckoutVersionNumber;
+
+//! Project version string for PayPalCheckout.
+FOUNDATION_EXPORT const unsigned char PayPalCheckoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PayPalCheckout/PublicHeader.h>
