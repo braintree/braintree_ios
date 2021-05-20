5ffea382128a2fe219447ec05e75170e858eb5ad
Samantha Cannillo <samantha.cannillo@getbraintree.com>
2021-05-20 09:51:38 -0500

    Re-order import statements

--- Sources/BraintreePaymentFlow/BTLocalPaymentRequest.m
+++ Sources/BraintreePaymentFlow/BTLocalPaymentRequest.m
@@ -30,17 +30,17 @@
 #if __has_include(<Braintree/Braintree-Swift.h>)      // CocoaPods
 #import <Braintree/Braintree-Swift.h>
 
-#elif __has_include("Braintree-Swift.h")              // CocoaPods for ReactNative
-/* Use quoted style when importing Swift headers for ReactNative support
- * See https://github.com/braintree/braintree_ios/issues/671
- */
-#import "Braintree-Swift.h"
-
 #elif SWIFT_PACKAGE                                   // SPM
 /* Use @import for SPM support
  * See https://forums.swift.org/t/using-a-swift-package-in-a-mixed-swift-and-objective-c-project/27348
  */
 @import PayPalDataCollector;
+
+#elif __has_include("Braintree-Swift.h")              // CocoaPods for ReactNative
+/* Use quoted style when importing Swift headers for ReactNative support
+ * See https://github.com/braintree/braintree_ios/issues/671
+ */
+#import "Braintree-Swift.h"
 
 #else                                                 // Carthage
 #import <PayPalDataCollector/PayPalDataCollector-Swift.h>
