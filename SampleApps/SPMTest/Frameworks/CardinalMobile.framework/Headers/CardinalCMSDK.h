a1a708be70e04560b3261e7907187001ccbf1eb7
braintreeps <code@getbraintree.com>
2021-06-22 20:16:24 +0000

    Revert "Remove SPMTest app to avoid Carthage timeout"
    This reverts commit 8993b5e1e22a6e8ac04c3821faea4ca8550a0ea0.

---
+++ SampleApps/SPMTest/Frameworks/CardinalMobile.framework/Headers/CardinalCMSDK.h
@@ -0,0 +1,15 @@
+//
+//  CardinalEMVCoSDK.h
+//  CardinalEMVCoSDK
+//
+//  Copyright © 2018 Cardinal Commerce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CardinalMobile/CardinalResponse.h>
+#import <CardinalMobile/CardinalSession.h>
+#import <CardinalMobile/CardinalSessionConfiguration.h>
+#import <CardinalMobile/CardinalSession.h>
+#import <CardinalMobile/CardinalStepUpDelegate.h>
+#import <CardinalMobile/DirectoryServerIDConst.h>
