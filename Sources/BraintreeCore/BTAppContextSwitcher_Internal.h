dd0e9803f0720c692616317c651e5f081c97964e
Samantha Cannillo <samantha.cannillo@getbraintree.com>
2021-08-09 16:26:30 -0500

    Update Demo app to rely on SceneDelegate only for App Context switching; update docsstrings in BTAppContextSwitch header

---
+++ Sources/BraintreeCore/BTAppContextSwitcher_Internal.h
@@ -0,0 +1,12 @@
+//
+//  BTAppContextSwitcher_Internal.h
+//  BTAppContextSwitcher_Internal
+//
+//  Created by Cannillo, Sammy on 8/9/21.
+//
+
+#ifndef BTAppContextSwitcher_Internal_h
+#define BTAppContextSwitcher_Internal_h
+
+
+#endif /* BTAppContextSwitcher_Internal_h */
