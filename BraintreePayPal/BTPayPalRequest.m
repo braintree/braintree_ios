948c086baf8eb8f43c1720b3c747db72c7eeeaaa
Samantha Cannillo <samantha.cannillo@getbraintree.com>
2021-08-18 11:45:14 -0500

    Add offerPayLater to BTPayPalRequest

--- BraintreePayPal/BTPayPalRequest.m
+++ BraintreePayPal/BTPayPalRequest.m
@@ -8,6 +8,7 @@
     if (self) {
         _shippingAddressRequired = NO;
         _offerCredit = NO;
+        _offerPayLater = NO;
         _shippingAddressEditable = NO;
         _intent = BTPayPalRequestIntentAuthorize;
         _userAction = BTPayPalRequestUserActionDefault;
