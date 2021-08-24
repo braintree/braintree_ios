ca0183ae5cefd5b5784dc0f505670760b30b6af2
Samantha Cannillo <samantha.cannillo@getbraintree.com>
2021-08-24 15:21:39 -0500

    Bump pod version to 4.38.0

--- BraintreeCore/Braintree-Version.h
+++ BraintreeCore/Braintree-Version.h
@@ -1,1 +1,1 @@
-#define BRAINTREE_VERSION (@"4.37.1")
+#define BRAINTREE_VERSION (@"4.38.0")
--- BraintreePayPal/PayPalUtils/Public/PPOTVersion.h
+++ BraintreePayPal/PayPalUtils/Public/PPOTVersion.h
@@ -6,6 +6,6 @@
 #ifndef PPOTVersion_h
 #define PPOTVersion_h
 
-#define PayPalOTVersion() @"4.37.1"
+#define PayPalOTVersion() @"4.38.0"
 
 #endif
