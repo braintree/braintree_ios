5475b4a1cf85fafc12b331b859fdd4a9884298d7
david.merino <david.merino@getbraintree.com>
2021-09-23 09:25:49 -0700

    Add enriched consumer data fields to Venmo nonce.

--- Sources/BraintreeVenmo/Public/BraintreeVenmo/BTVenmoAccountNonce.h
+++ Sources/BraintreeVenmo/Public/BraintreeVenmo/BTVenmoAccountNonce.h
@@ -10,6 +10,31 @@
 @interface BTVenmoAccountNonce : BTPaymentMethodNonce
 
 /**
+ The email associated with the Venmo account
+*/
+@property (nonatomic, nullable, readonly, copy) NSString *email;
+
+/**
+ The external ID associated with the Venmo account
+*/
+@property (nonatomic, nullable, readonly, copy) NSString *externalId;
+
+/**
+ The first name associated with the Venmo account
+*/
+@property (nonatomic, nullable, readonly, copy) NSString *firstName;
+
+/**
+ The last name associated with the Venmo account
+*/
+@property (nonatomic, nullable, readonly, copy) NSString *lastName;
+
+/**
+ The phone number associated with the Venmo account
+*/
+@property (nonatomic, nullable, readonly, copy) NSString *phoneNumber;
+
+/**
  The username associated with the Venmo account
 */
 @property (nonatomic, nullable, readonly, copy) NSString *username;
