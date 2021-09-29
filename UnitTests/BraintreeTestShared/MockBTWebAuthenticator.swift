c7347e8d961e6f645c000b5d214a14a14e3c40e7
Steven <sshropshire@paypal.com>
2021-09-29 16:00:02 -0500

    Rename MockASWebAuthenticationSession to MockBTWebAuthenticator.

---
+++ UnitTests/BraintreeTestShared/MockBTWebAuthenticator.swift
@@ -0,0 +1,13 @@
+//
+//  MockBTWebAuthenticator.swift
+//  BraintreeTestShared
+//
+//  Created by Shropshire, Steven on 9/29/21.
+//
+
+import Foundation
+import BraintreeCore
+
+class MockBTWebAuthenticator: BTWebAuthenticator {
+    
+}
