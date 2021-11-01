10e4ffc630f528a703eca019e0bd69cfe076480f
david.merino <david.merino@getbraintree.com>
2021-11-01 09:44:49 -0700

    Add nodoc for beta fields.

--- Sources/BraintreeVenmo/Public/BraintreeVenmo/BTVenmoAccountNonce.h
+++ Sources/BraintreeVenmo/Public/BraintreeVenmo/BTVenmoAccountNonce.h
@@ -10,26 +10,31 @@
 @interface BTVenmoAccountNonce : BTPaymentMethodNonce
 
 /**
+ :nodoc:
  The email associated with the Venmo account
 */
 @property (nonatomic, nullable, readonly, copy) NSString *email;
 
 /**
+ :nodoc:
  The external ID associated with the Venmo account
 */
 @property (nonatomic, nullable, readonly, copy) NSString *externalId;
 
 /**
+ :nodoc:
  The first name associated with the Venmo account
 */
 @property (nonatomic, nullable, readonly, copy) NSString *firstName;
 
 /**
+ :nodoc:
  The last name associated with the Venmo account
 */
 @property (nonatomic, nullable, readonly, copy) NSString *lastName;
 
 /**
+ :nodoc:
  The phone number associated with the Venmo account
 */
 @property (nonatomic, nullable, readonly, copy) NSString *phoneNumber;
