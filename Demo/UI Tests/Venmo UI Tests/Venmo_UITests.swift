7b330a605d36470eaa8977f89695c2d318e9cce0
scannillo <35243507+scannillo@users.noreply.github.com>
2021-12-16 10:40:24 -0600

    Fix Flakey Venmo UI Tests (#764)
    - Add 2 second timeout to Venmo cancel UI test
    - Simplify `XCTestCase` helper methods
    - Add `waitForElementToBeHittable` timeout to all button presses in the UI Venmo test flow
    
    Co-authored-by: Samantha Cannillo <samantha.cannillo@getbraintree.com>
    Co-authored-by: Jax DesMarais-Leder <jdesmarais@paypal.com>
    Co-authored-by: Sarah Koop <skoop@paypal.com>

--- Demo/UI Tests/Venmo UI Tests/Venmo_UITests.swift
+++ Demo/UI Tests/Venmo UI Tests/Venmo_UITests.swift
@@ -18,24 +18,28 @@ class Venmo_UITests: XCTestCase {
         demoApp.launchArguments.append("-Integration:BraintreeDemoCustomVenmoButtonViewController")
         demoApp.launch()
 
+        waitForElementToBeHittable(demoApp.buttons["Venmo (custom button)"])
         demoApp.buttons["Venmo (custom button)"].tap()
     }
 
     func testTokenizeVenmo_whenSignInSuccessful_returnsNonce() {
+        waitForElementToBeHittable(mockVenmo.buttons["SUCCESS"])
         mockVenmo.buttons["SUCCESS"].tap()
 
-        XCTAssertTrue(demoApp.buttons["Got a nonce. Tap to make a transaction."].waitForExistence(timeout: 2))
+        XCTAssertTrue(demoApp.buttons["Got a nonce. Tap to make a transaction."].waitForExistence(timeout: 5))
     }
 
     func testTokenizeVenmo_whenErrorOccurs_returnsError() {
+        waitForElementToBeHittable(mockVenmo.buttons["ERROR"])
         mockVenmo.buttons["ERROR"].tap()
 
-        XCTAssertTrue(demoApp.buttons["An error occurred during the Venmo flow"].waitForExistence(timeout: 2))
+        XCTAssertTrue(demoApp.buttons["An error occurred during the Venmo flow"].waitForExistence(timeout: 5))
     }
 
     func testTokenizeVenmo_whenUserCancels_returnsCancel() {
+        waitForElementToBeHittable(mockVenmo.buttons["Cancel"])
         mockVenmo.buttons["Cancel"].tap()
 
-        XCTAssertTrue(demoApp.buttons["Canceled 🔰"].exists)
+        XCTAssertTrue(demoApp.buttons["Canceled 🔰"].waitForExistence(timeout: 5))
     }
 }
