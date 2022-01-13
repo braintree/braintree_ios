b468e55e2e932e3af2dcf43c7ea8afde9764b400
Sarah Koop <skoop@paypal.com>
2022-01-13 11:45:03 -0600

    Fix helper method type

--- Sources/BraintreeCard/BTCardClient.m
+++ Sources/BraintreeCard/BTCardClient.m
@@ -244,7 +244,7 @@ NSString *const BTCardClientGraphQLTokenizeFeature = @"tokenize_credit_cards";
     return graphQLFeatures && [graphQLFeatures containsObject:BTCardClientGraphQLTokenizeFeature];
 }
 
-- (NSError *)constructCallbackErrorForErrorCode:(String *)errorCode error:(NSError *)error {
+- (NSError *)constructCallbackErrorForErrorCode:(BTJSON *)errorCode error:(NSError *)error {
     NSError *callbackError = error;
 
     // Gateway error code for card already exists
