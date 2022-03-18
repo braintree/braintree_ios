238d830f1f1ce8e10180cfa216cd1ba2e2235089
Jax DesMarais-Leder <jdesmarais@paypal.com>
2022-03-18 15:54:32 -0500

    Add SEPA Direct Debit to Demo App (#795)
    * Add SEPA Direct Debit to Demo App
    
    Co-authored-by: Sarah Koop <skoop@paypal.com>

---
+++ Demo/Application/Features/SEPA Direct Debit/BraintreeDemoSEPADirectDebitViewController.swift
@@ -0,0 +1,81 @@
+import UIKit
+import BraintreeSEPADirectDebit
+
+class BraintreeDemoSEPADirectDebitViewController: BraintreeDemoBaseViewController {
+    private let sepaDirectDebitClient: BTSEPADirectDebitClient
+    private let sepaDirectDebitButton = UIButton(type: .system)
+    
+    override init?(authorization: String!) {
+        guard let apiClient = BTAPIClient(authorization: authorization) else { return nil }
+        
+        sepaDirectDebitClient = BTSEPADirectDebitClient(apiClient: apiClient)
+
+        super.init(authorization: authorization)
+        
+        title = "SEPA Direct Debit"
+        view.backgroundColor = UIColor(red: 250.0 / 255.0, green: 253.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0)
+        
+        sepaDirectDebitButton.setTitle("SEPA Direct Debit", for: .normal)
+        sepaDirectDebitButton.translatesAutoresizingMaskIntoConstraints = false
+        sepaDirectDebitButton.addTarget(self, action: #selector(sepaDirectDebitButtonTapped), for: .touchUpInside)
+        view.addSubview(sepaDirectDebitButton)
+        
+        NSLayoutConstraint.activate(
+            [
+                sepaDirectDebitButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                sepaDirectDebitButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            ]
+        )
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func sepaDirectDebitButtonTapped() {
+        self.progressBlock("Tapped SEPA Debit")
+
+        let billingAddress = BTPostalAddress()
+        billingAddress.streetAddress = "Kantstraße 70"
+        billingAddress.extendedAddress = "#170"
+        billingAddress.locality = "Freistaat Sachsen"
+        billingAddress.region = "Annaberg-buchholz"
+        billingAddress.postalCode = "09456"
+        billingAddress.countryCodeAlpha2 = "FR"
+        
+        let sepaDirectDebitRequest = BTSEPADirectDebitRequest()
+        sepaDirectDebitRequest.accountHolderName = "John Doe"
+        sepaDirectDebitRequest.iban = generateRandomIBAN()
+        sepaDirectDebitRequest.customerID = generateRandomCustomerID()
+        sepaDirectDebitRequest.mandateType = .oneOff
+        sepaDirectDebitRequest.billingAddress = billingAddress
+        
+        sepaDirectDebitClient.tokenize(request: sepaDirectDebitRequest) { sepaDirectDebitNonce, error in
+            self.sepaDirectDebitButton.setTitle("Processing...", for: .disabled)
+            self.sepaDirectDebitButton.isEnabled = false
+
+            if let sepaDirectDebitNonce = sepaDirectDebitNonce {
+                self.nonceStringCompletionBlock(sepaDirectDebitNonce.nonce)
+            } else if let error = error {
+                self.progressBlock(error.localizedDescription)
+            } else {
+                self.progressBlock("Canceled")
+            }
+        }
+        self.sepaDirectDebitButton.isEnabled = true
+    }
+    
+    private func generateRandomCustomerID() -> String {
+        UUID().uuidString.replacingOccurrences(of: "-", with: "")
+    }
+    
+    private func generateRandomIBAN() -> String {
+        let length = 24
+        let characters = "0123456789"
+        let randomCharacters = (0..<length).map{ _ in characters.randomElement()! }
+        let randomString = String(randomCharacters)
+
+        return "FR" + randomString
+    }
+}
+
