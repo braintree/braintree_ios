adb48c0b5e03e72e4863468a84279877e42f7142
Jax DesMarais-Leder <jdesmarais@paypal.com>
2022-03-21 14:09:56 -0500

    update test formatting

--- UnitTests/BraintreeSEPADirectDebitTests/BTSEPADirectDebitRequest_Tests.swift
+++ UnitTests/BraintreeSEPADirectDebitTests/BTSEPADirectDebitRequest_Tests.swift
@@ -6,8 +6,29 @@ class BTSEPADirectDebitRequest_Tests: XCTestCase {
 
     func testEncoding() throws {
         let expectedJSON = """
-{\"sepa_debit\":{\"customer_id\":\"A0E243A0A200491D929D\",\"mandate_type\":\"ONE_OFF\",\"account_holder_name\":\"John Doe\",\"iban\":\"FR891751244434203564412313\",\"billing_address\":{\"admin_area_2\":\"Annaberg-buchholz\",\"country_code\":\"FR\",\"address_line_2\":\"#170\",\"address_line_1\":\"Kantstraße 70\",\"admin_area_1\":\"Freistaat Sachsen\",\"postal_code\":\"09456\"}},\"merchant_account_id\":\"eur_pwpp_multi_account_merchant_account\",\"cancel_url\":\"https:\\/\\/example.com\",\"return_url\":\"https:\\/\\/example.com\"}
-"""
+        {
+            "sepa_debit":{
+                "customer_id":"A0E243A0A200491D929D",
+                "mandate_type":"ONE_OFF",
+                "account_holder_name":"John Doe",
+                "iban":"FR891751244434203564412313",
+                "billing_address":{
+                    "admin_area_2":"Annaberg-buchholz",
+                    "country_code":"FR",
+                    "address_line_2":"#170",
+                    "address_line_1":"Kantstraße 70",
+                    "admin_area_1":"Freistaat Sachsen",
+                    "postal_code":"09456"
+                }
+            },
+            "merchant_account_id":"eur_pwpp_multi_account_merchant_account",
+            "cancel_url":"https://example.com",
+            "return_url":"https://example.com"
+        }
+        """
+            .components(separatedBy: .newlines)
+            .map { $0.trimmingCharacters(in: .whitespaces) }
+            .joined()
 
         let billingAddress = BTPostalAddress()
         billingAddress.streetAddress = "Kantstraße 70"
@@ -27,7 +48,7 @@ class BTSEPADirectDebitRequest_Tests: XCTestCase {
         )
         
         let encodedRequest = try XCTUnwrap(JSONEncoder().encode(sepaDirectDebitRequest))
-        let encodedRequestData = String(data: encodedRequest, encoding: .utf8)
+        let encodedRequestData = String(data: encodedRequest, encoding: .utf8)?.replacingOccurrences(of: "\\/\\/", with: "//")
 
         XCTAssertEqual(encodedRequestData, expectedJSON)
     }
