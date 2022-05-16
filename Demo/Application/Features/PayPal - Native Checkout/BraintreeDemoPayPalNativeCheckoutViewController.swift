c96610cf407c64cebe201489103e36a4377cbeeb
Matthew Wylder <mwylder@paypal.com>
2022-05-16 15:23:33 -0500

    Added Native Checkout Demo ViewController

---
+++ Demo/Application/Features/PayPal - Native Checkout/BraintreeDemoPayPalNativeCheckoutViewController.swift
@@ -0,0 +1,41 @@
+import Foundation
+import UIKit
+import BraintreePayPalNativeCheckout
+
+class BraintreeDemoPayPalNativeCheckoutViewController: BraintreeDemoPaymentButtonBaseViewController {
+    
+    override func createPaymentButton() -> UIView! {
+        lazy var payPalCheckoutButton: UIButton = {
+            let payPalCheckoutButton = UIButton(type: .system)
+            payPalCheckoutButton.setTitle("PayPal Native Checkout", for: .normal)
+            payPalCheckoutButton.setTitleColor(.blue, for: .normal)
+            payPalCheckoutButton.setTitleColor(.lightGray, for: .highlighted)
+            payPalCheckoutButton.setTitleColor(.lightGray, for: .disabled)
+            payPalCheckoutButton.addTarget(self, action: #selector(tappedPayPalCheckout), for: .touchUpInside)
+            return payPalCheckoutButton
+        }()
+        
+        return payPalCheckoutButton
+    }
+    
+    
+    @objc func tappedPayPalCheckout(_ sender: UIButton) {
+        progressBlock("Tapped PayPal - Native Checkout using BTPayPalDriver")
+        sender.setTitle("Processing...", for: .disabled)
+        sender.isEnabled = false
+
+//        let driver = BTPayPalDriver(apiClient: apiClient)
+//        let request = BTPayPalCheckoutRequest(amount: "4.30")
+//        request.activeWindow = self.view.window
+//
+//        driver.tokenizePayPalAccount(with: request) { nonce, error in
+//            sender.isEnabled = true
+//
+//            guard let nonce = nonce else {
+//                self.progressBlock(error?.localizedDescription)
+//                return
+//            }
+//            self.completionBlock(nonce)
+//        }
+    }
+}
