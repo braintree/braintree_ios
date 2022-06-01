27646d06ec78dae745c99effbab857d2533bcf79
Jax DesMarais-Leder <jdesmarais@paypal.com>
2022-06-01 13:19:05 -0500

    update sample app demo for SPM

--- Sources/KountDataCollector/BTKountDataCollector.m
+++ Sources/KountDataCollector/BTKountDataCollector.m
@@ -1,3 +1,7 @@
-#import <KountDataCollector.h>
+#if __has_include(<Braintree/KountDataCollector.h>)
+#import <Braintree/KountDataCollector.h>
+#else
+#import <KountDataCollector/KountDataCollector.h>
+#endif
 
 int _keep;
