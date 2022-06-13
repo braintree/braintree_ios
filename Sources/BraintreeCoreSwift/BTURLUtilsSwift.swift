05ece4a0d2cfb393e66e0cc53c1dcdf929046403
Matt Wylder <mwylder@paypal.com>
2022-06-13 15:39:46 -0500

    translated queryStringsWithDictionary from objective c
    Signed-off-by: Jax DesMarais-Leder <jdesmarais@paypal.com>

--- Sources/BraintreeCoreSwift/BTURLUtilsSwift.swift
+++ Sources/BraintreeCoreSwift/BTURLUtilsSwift.swift
@@ -3,7 +3,29 @@ import Foundation
 @objc public class BTURLUtilsSwift: NSObject {
   
     @objc public static func queryStringWithDictionary(dict: NSDictionary) -> String {
-        return ""
+        var queryString: String = ""
+        for (rawKey, value) in dict {
+            guard let key = rawKey as? String else {
+                continue
+            }
+            let encodedKey = encode(key)
+            
+            if let arrayValue = value as? [String] {
+                for item in arrayValue {
+                    queryString.append("\(encodedKey)%%5B%%5D=\(encode(item))&")
+                }
+            } else if let dictValue = value as? [String: String] {
+                for (subkey, subvalue) in dictValue {
+                    queryString.append("\(encodedKey)%%5B\(encode(subkey))%%5D=\(encode(subvalue))&")
+                }
+            } else if let _ = value as? NSNull {
+                queryString.append("\(encodedKey)=&")
+            } else {
+                queryString.append("\(encodedKey)=\(encode(String(describing: value)))&")
+            }
+        }
+        
+        return String(queryString.dropLast())
     }
     
     @objc public static func queryParametersForURL(url: URL) -> [String: String] {
@@ -21,9 +43,7 @@ import Foundation
         return parameters
     }
     
-    
-    
-    func stringByURLEncodingAllCharactersInString(string: String) -> String {
+    static func encode(_ string: String) -> String {
         string.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
     }
 }
