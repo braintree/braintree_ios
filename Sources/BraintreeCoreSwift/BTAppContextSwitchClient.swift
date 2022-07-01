2af946e9624c3027037e4453093ef7875ec27a7b
Jax DesMarais-Leder <jdesmarais@paypal.com>
2022-07-01 11:08:49 -0500

    update to pull in type and docs
    Co-authored-by: Matt Wylder <mwylder@paypal.com>

--- Sources/BraintreeCoreSwift/BTAppContextSwitchClient.swift
+++ Sources/BraintreeCoreSwift/BTAppContextSwitchClient.swift
@@ -3,11 +3,11 @@ import Foundation
 @objc public protocol BTAppContextSwitchClient {
     
     /// :nodoc: Determine whether the return URL can be handled.
-    /// - Parameter returnURL: the URL you receive in  `scene(_:openURLContexts:)` (or `application(_:open:options:)` if not using SceneDelegate) when returning to your app
+    /// - Parameter url: the URL you receive in  `scene(_:openURLContexts:)` (or `application(_:open:options:)` if not using SceneDelegate) when returning to your app
     /// - Returns: `true` when the SDK can process the return URL
-    func canHandleReturnURL(_ url: URL) -> Bool
+    static func canHandleReturnURL(_ url: URL) -> Bool
     
     ///  :nodoc: Complete payment flow after returning from app or browser switch.
     /// - Parameter url: The URL you receive in `scene(_:openURLContexts:)` (or `application(_:open:options:)` if not using SceneDelegate)
-    func handleReturnURL(_ url: URL)
+    static func handleReturnURL(_ url: URL)
 }
--- Sources/BraintreeCoreSwift/BTAppContextSwitcher.swift
+++ Sources/BraintreeCoreSwift/BTAppContextSwitcher.swift
@@ -1,17 +1,17 @@
 import UIKit
 
 /// Handles return URLs when returning from app context switch and routes the return URL to the correct app context switch client class.
-/// @note `returnURLScheme` must contain your app's registered URL Type that starts with the app's bundle ID.
-/// When your app returns from app switch, the app delegate should call  `handleOpenURLContext:` (or `handleOpenURL` if not using SceneDelegate)
+/// - Note: `returnURLScheme` must contain your app's registered URL Type that starts with the app's bundle ID.
+/// When your app returns from app switch, the app delegate should call  `handleOpenURL` (or `handleOpen` if not using SceneDelegate)
 @objcMembers public class BTAppContextSwitcher: NSObject {
     
+    /// Singleton for shared instance of `BTAppContextSwitcher`
     public static let sharedInstance = BTAppContextSwitcher()
     
     /// The URL scheme to return to this app after switching to another app or opening a SFSafariViewController.
     /// This URL scheme must be registered as a URL Type in the app's info.plist, and it must start with the app's bundle ID.
     public var returnURLScheme: String = ""
-    //TODO: Optimize this, it was a set in Objc
-    private var appContextSwitchClients = [BTAppContextSwitchClient]()
+    private var appContextSwitchClients = [BTAppContextSwitchClient.Type]()
     
     @objc(handleOpenURLContext:)
     public func handleOpenURL(context: UIOpenURLContext) -> Bool {
@@ -30,7 +30,7 @@ import UIKit
     }
     
     @objc(registerAppContextSwitchClient:)
-    public func register(client: BTAppContextSwitchClient) {
+    public func register(_ client: BTAppContextSwitchClient.Type) {
         appContextSwitchClients.append(client)
     }
 }
