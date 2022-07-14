3ee866d253485d1360dbe182521a93ed998ae97f
Jax DesMarais-Leder <jdesmarais@paypal.com>
2022-07-14 13:18:39 -0500

    update package dependancy format

--- Package.swift
+++ Package.swift
@@ -61,7 +61,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "PayPalCheckout", url: "https://github.com/paypal/paypalcheckout-ios", .exact("0.94.0"))
+        .package(url: "https://github.com/paypal/paypalcheckout-ios.git", from: "0.94.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
