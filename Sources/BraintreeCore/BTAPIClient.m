07f599bdc499caf14f360bea3b47da190a2790a1
Matt Wylder <mwylder@paypal.com>
2022-07-20 13:14:35 -0500

    Added swift imports header
    Signed-off-by: Jax DesMarais-Leder <jdesmarais@paypal.com>

--- Sources/BraintreeCore/BTAPIClient.m
+++ Sources/BraintreeCore/BTAPIClient.m
@@ -5,6 +5,7 @@
 #import "BTGraphQLHTTP.h"
 #import "BTHTTP.h"
 #import "BTPaymentMethodNonceParser.h"
+#import "BraintreeCoreSwiftImports.h"
 
 #if __has_include(<Braintree/BraintreeCore.h>)
 #import <Braintree/BTClientToken.h>
@@ -14,26 +15,6 @@
 #import <BraintreeCore/BTClientToken.h>
 #import <BraintreeCore/BTConfiguration.h>
 #import <BraintreeCore/BTPaymentMethodNonce.h>
-#endif
-
-// Swift Module Imports
-#if __has_include(<Braintree/Braintree-Swift.h>) // Cocoapods-generated Swift Header
-#import <Braintree/Braintree-Swift.h>
-
-#elif SWIFT_PACKAGE                              // SPM
-/* Use @import for SPM support
- * See https://forums.swift.org/t/using-a-swift-package-in-a-mixed-swift-and-objective-c-project/27348
- */
-@import BraintreeCoreSwift;
-
-#elif __has_include("Braintree-Swift.h")         // CocoaPods for ReactNative
-/* Use quoted style when importing Swift headers for ReactNative support
- * See https://github.com/braintree/braintree_ios/issues/671
- */
-#import "Braintree-Swift.h"
-
-#else // Carthage or Local Builds
-#import <BraintreeCoreSwift/BraintreeCoreSwift-Swift.h>
 #endif
 
 NSString *const BTAPIClientErrorDomain = @"com.braintreepayments.BTAPIClientErrorDomain";
--- Sources/BraintreeCore/BTConfiguration.m
+++ Sources/BraintreeCore/BTConfiguration.m
@@ -1,27 +1,9 @@
+#import "BraintreeCoreSwiftImports.h"
+
 #if __has_include(<Braintree/BraintreeCore.h>)
 #import <Braintree/BTConfiguration.h>
 #else
 #import <BraintreeCore/BTConfiguration.h>
-#endif
-
-// Swift Module Imports
-#if __has_include(<Braintree/Braintree-Swift.h>) // Cocoapods-generated Swift Header
-#import <Braintree/Braintree-Swift.h>
-
-#elif SWIFT_PACKAGE                              // SPM
-/* Use @import for SPM support
- * See https://forums.swift.org/t/using-a-swift-package-in-a-mixed-swift-and-objective-c-project/27348
- */
-@import BraintreeCoreSwift;
-
-#elif __has_include("Braintree-Swift.h")         // CocoaPods for ReactNative
-/* Use quoted style when importing Swift headers for ReactNative support
- * See https://github.com/braintree/braintree_ios/issues/671
- */
-#import "Braintree-Swift.h"
-
-#else // Carthage or Local Builds
-#import <BraintreeCoreSwift/BraintreeCoreSwift-Swift.h>
 #endif
 
 @implementation BTConfiguration
