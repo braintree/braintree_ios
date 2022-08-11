bebb2e50eaa996e503b0d2afbf55767c46504984
Jax DesMarais-Leder <jdesmarais@paypal.com>
2022-08-11 12:47:11 -0500

    convert btbindata to swift
    Co-authored-by: Matt Wylder <mwylder@paypal.com>

--- Sources/BraintreeCore/Public/BraintreeCore/BraintreeCore.h
+++ Sources/BraintreeCore/Public/BraintreeCore/BraintreeCore.h
@@ -8,7 +8,6 @@ FOUNDATION_EXPORT const unsigned char BraintreeCoreVersionString[];
 
 #if __has_include(<Braintree/BraintreeCore.h>)
 #import <Braintree/BTAPIClient.h>
-#import <Braintree/BTBinData.h>
 #import <Braintree/BTClientToken.h>
 #import <Braintree/BTEnums.h>
 #import <Braintree/BTViewControllerPresentingDelegate.h>
@@ -17,7 +16,6 @@ FOUNDATION_EXPORT const unsigned char BraintreeCoreVersionString[];
 #import <Braintree/Braintree.h>
 #else
 #import <BraintreeCore/BTAPIClient.h>
-#import <BraintreeCore/BTBinData.h>
 #import <BraintreeCore/BTClientToken.h>
 #import <BraintreeCore/BTEnums.h>
 #import <BraintreeCore/BTViewControllerPresentingDelegate.h>
