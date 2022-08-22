7a1defeb917a1f7338fc437a86f9100513ca9af5
Jax DesMarais-Leder <jdesmarais@paypal.com>
2022-08-22 09:05:54 -0500

    organize funcitons, add wip stubs, cleanup

--- Sources/BraintreeCoreSwift/BTAPIHTTP.swift
+++ Sources/BraintreeCoreSwift/BTAPIHTTP.swift
@@ -3,19 +3,26 @@ import Foundation
 @objcMembers public class BTAPIHTTPSwift: BTHTTPSwift {
     
     let accessToken: String
-    
+
+    @objc(initWithBaseURL:accessToken:)
     public init(url: URL, accessToken: String) {
         self.accessToken = accessToken
+
         super.init(url: url)
+
         let configuration = URLSessionConfiguration.ephemeral
         configuration.httpAdditionalHeaders = self.defaultHeaders()
+
         let delegateQueue = OperationQueue()
         delegateQueue.name = "com.braintreepayments.BTHTTP"
         delegateQueue.maxConcurrentOperationCount = .max
-        
-        self.session = URLSession(configuration: configuration,
-                                  delegate: self,
-                                  delegateQueue: delegateQueue)
+
+        self.session = URLSession(
+            configuration: configuration,
+            delegate: self,
+            delegateQueue: delegateQueue
+        )
+
         self.pinnedCertificates = BTAPIPinnedCertificates.trustedCertificates()
     }
     
@@ -24,7 +31,7 @@ import Foundation
             "User-Agent": self.userAgentString(),
             "Accept": self.acceptString(),
             "Accept-Language": self.acceptLanguageString(),
-            "Braintree-Version": "2022-08-19", //TODO: change this, it's a macro in objc
+            "Braintree-Version": \(BraintreeCoreConstants.apiVersion),
             "Authorization": "Bearer \(self.accessToken)"
         ]
     }
--- Sources/BraintreeCoreSwift/BraintreeCoreConstants.swift
+++ Sources/BraintreeCoreSwift/BraintreeCoreConstants.swift
@@ -2,4 +2,6 @@ import Foundation
 
 public class BraintreeCoreConstants {
     public static var braintreeVersion: String = "5.11.0"
+    public static var apiVersion: String = "2016-10-07"
+    public static var graphQLVersion: String = "2018-03-06"
 }
