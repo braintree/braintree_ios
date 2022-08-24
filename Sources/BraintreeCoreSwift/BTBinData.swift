e359dc0305596e9d50d97c722c784ff38a414609
Matt Wylder <mwylder@paypal.com>
2022-08-24 14:57:12 -0500

    removed unused optionals'
    Signed-off-by: Jax DesMarais-Leder <jdesmarais@paypal.com>

--- Sources/BraintreeCoreSwift/BTBinData.swift
+++ Sources/BraintreeCoreSwift/BTBinData.swift
@@ -4,31 +4,31 @@ import Foundation
 @objcMembers public class BTBinData: NSObject {
 
     /// Whether the card is a prepaid card. Possible values: Yes/No/Unknown
-    public let prepaid: String?
+    public let prepaid: String
 
     /// Whether the card is a healthcare card. Possible values: Yes/No/Unknown
-    public let healthcare: String?
+    public let healthcare: String
 
     /// Whether the card is a debit card. Possible values: Yes/No/Unknown
-    public let debit: String?
+    public let debit: String
 
     ///  A value indicating whether the issuing bank's card range is regulated by the Durbin Amendment due to the bank's assets. Possible values: Yes/No/Unknown
-    public let durbinRegulated: String?
+    public let durbinRegulated: String
 
     ///  Whether the card type is a commercial card and is capable of processing Level 2 transactions. Possible values: Yes/No/Unknown
-    public let commercial: String?
+    public let commercial: String
 
     /// Whether the card is a payroll card. Possible values: Yes/No/Unknown
-    public let payroll: String?
+    public let payroll: String
 
     /// The bank that issued the credit card, if available.
-    public let issuingBank: String?
+    public let issuingBank: String
 
     /// The country that issued the credit card, if available.
-    public let countryOfIssuance: String?
+    public let countryOfIssuance: String
 
     /// The code for the product type of the card (e.g. `D` (Visa Signature Preferred), `G` (Visa Business)), if available.
-    public let productID: String?
+    public let productID: String
 
     /// Create a `BTBinData` object from JSON.
     @objc(initWithJSON:)
