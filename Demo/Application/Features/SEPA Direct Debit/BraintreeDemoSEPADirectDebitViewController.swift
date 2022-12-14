3a651f8a36a410a39a2d9de5d4f99d7121a8b79d
Jax DesMarais-Leder <jdesmarais@paypal.com>
2022-12-14 12:01:32 -0600

    update docstrings and cleanup

--- Demo/Application/Features/SEPA Direct Debit/BraintreeDemoSEPADirectDebitViewController.swift
+++ Demo/Application/Features/SEPA Direct Debit/BraintreeDemoSEPADirectDebitViewController.swift
@@ -78,6 +78,12 @@ class BraintreeDemoSEPADirectDebitViewController: BraintreeDemoBaseViewControlle
         }
     }
 
+    private func generateRandomCustomerID() -> String {
+        String(UUID().uuidString.replacingOccurrences(of: "-", with: "").prefix(20))
+    }
+
+    // MARK: - Helper functions for IBAN generation
+
     private func generateRandomIBAN() -> String {
         let countryCode: String = "FR"
         let bankCode: String = "30006"
@@ -93,12 +99,13 @@ class BraintreeDemoSEPADirectDebitViewController: BraintreeDemoBaseViewControlle
     private func accountNumberWithChecksum(bankCode: Int, branchCode: Int, accountNumber: Int) -> String {
         let sum: Int = 89 * bankCode + 15 * branchCode + 3 * accountNumber
         let checksum = 97 - calculateMod97(from: sum)
-        print("\(accountNumber)\(checksum)")
+
         return "\(accountNumber)\(checksum)"
     }
 
     private func checksum(bankCode: String, branchCode: String, accountNumber: Int) -> String {
-        // TODO: docs for where the final numbers come from here
+        // 152700 is taken from the conversion table here: https://community.appway.com/screen/kb/article/generating-and-validating-an-iban-1683400256881#conversion-table
+        // and is representative of the characters "FR" with 00 being added to the end for all bban's to calculate the checksum
         let bbanString: String = bankCode + branchCode + "\(accountNumber)" + "152700"
         let modResult = (Decimal(string: bbanString) ?? 0) % 97
         let result = 98 - modResult
@@ -115,25 +122,24 @@ class BraintreeDemoSEPADirectDebitViewController: BraintreeDemoBaseViewControlle
 
         return mod
     }
-    
-†    private func generateRandomCustomerID() -> String {
-        String(UUID().uuidString.replacingOccurrences(of: "-", with: "").prefix(20))
-    }
 }
 
-// Allows us to divide by a large integer for use in calculating a checksum
-public func % (lhs: Decimal, rhs: Decimal) -> Decimal {
-    precondition(lhs > 0 && rhs > 0)
+// We need this to calculate the mod result on a large integer since Int cannot handle the calculation of an number of this size
+public extension Decimal {
+    // Allows us to divide by a large integer for use in calculating a checksum
+    static func % (lhs: Decimal, rhs: Decimal) -> Decimal {
+        precondition(lhs > 0 && rhs > 0)
 
-    if lhs < rhs {
-        return lhs
-    } else if lhs == rhs {
-        return 0
+        if lhs < rhs {
+            return lhs
+        } else if lhs == rhs {
+            return 0
+        }
+
+        var quotient = lhs / rhs
+        var rounded = Decimal()
+        NSDecimalRound(&rounded, &quotient, 0, .down)
+
+        return lhs - (rounded * rhs)
     }
-
-    var quotient = lhs / rhs
-    var rounded = Decimal()
-    NSDecimalRound(&rounded, &quotient, 0, .down)
-
-    return lhs - (rounded * rhs)
 }
