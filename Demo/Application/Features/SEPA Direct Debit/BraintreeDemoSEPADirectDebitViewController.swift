cb76b8b2a37e013a01656613b57e5d2d8def31d6
Jax DesMarais-Leder <jdesmarais@paypal.com>
2022-12-14 09:50:49 -0600

    WIP - random IBAN generator for the demo app

--- Demo/Application/Features/SEPA Direct Debit/BraintreeDemoSEPADirectDebitViewController.swift
+++ Demo/Application/Features/SEPA Direct Debit/BraintreeDemoSEPADirectDebitViewController.swift
@@ -61,7 +61,7 @@ class BraintreeDemoSEPADirectDebitViewController: BraintreeDemoBaseViewControlle
 
         let sepaDirectDebitRequest = BTSEPADirectDebitRequest()
         sepaDirectDebitRequest.accountHolderName = "John Doe"
-        sepaDirectDebitRequest.iban = "FR7630006000011401744283861"
+        sepaDirectDebitRequest.iban = generateRandomIBAN()
         sepaDirectDebitRequest.customerID = generateRandomCustomerID()
         sepaDirectDebitRequest.mandateType = .oneOff
         sepaDirectDebitRequest.billingAddress = billingAddress
@@ -78,8 +78,62 @@ class BraintreeDemoSEPADirectDebitViewController: BraintreeDemoBaseViewControlle
         }
     }
 
-    private func generateRandomCustomerID() -> String {
+    private func generateRandomIBAN() -> String {
+        let countryCode: String = "FR"
+        let bankCode: String = "30006"
+        let branchCode: String = "00001"
+        let accountNumber = Int.random(in: 10_000_000_000...100_000_000_000)
+
+        let accountNumberWithChecksum = accountNumberWithChecksum(bankCode: Int(bankCode) ?? 0, branchCode: Int(branchCode) ?? 0, accountNumber: accountNumber)
+        let checksum = checksum(bankCode: bankCode, branchCode: branchCode, accountNumber: Int(accountNumberWithChecksum) ?? 0)
+
+        return countryCode + "\(checksum)\(bankCode)\(branchCode)\(accountNumberWithChecksum)"
+    }
+
+    private func accountNumberWithChecksum(bankCode: Int, branchCode: Int, accountNumber: Int) -> String {
+        let sum: Int = 89 * bankCode + 15 * branchCode + 3 * accountNumber
+        let checksum = 97 - calculateMod97(from: sum)
+        print("\(accountNumber)\(checksum)")
+        return "\(accountNumber)\(checksum)"
+    }
+
+    private func checksum(bankCode: String, branchCode: String, accountNumber: Int) -> String {
+        // TODO: docs for where the final numbers come from here
+        let bbanString: String = bankCode + branchCode + "\(accountNumber)" + "152700"
+        let modResult = (Decimal(string: bbanString) ?? 0) % 97
+        let result = 98 - modResult
+        return "\(result)"
+    }
+
+    private func calculateMod97(from accountNumber: Int) -> Int {
+        var mod: Int = 0
+        let accountNumberArray = String(accountNumber).map { String($0) }
+
+        for digit in accountNumberArray {
+            mod = ((mod * 10) + (Int(digit) ?? 0)) % 97
+        }
+
+        return mod
+    }
+    
+†    private func generateRandomCustomerID() -> String {
         String(UUID().uuidString.replacingOccurrences(of: "-", with: "").prefix(20))
     }
 }
 
+// Allows us to divide by a large integer for use in calculating a checksum
+public func % (lhs: Decimal, rhs: Decimal) -> Decimal {
+    precondition(lhs > 0 && rhs > 0)
+
+    if lhs < rhs {
+        return lhs
+    } else if lhs == rhs {
+        return 0
+    }
+
+    var quotient = lhs / rhs
+    var rounded = Decimal()
+    NSDecimalRound(&rounded, &quotient, 0, .down)
+
+    return lhs - (rounded * rhs)
+}
