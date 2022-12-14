081fda590b8a209056ba17ac17f1ff43cfcfd3b9
Matt Wylder <mwylder@paypal.com>
2022-12-14 11:31:52 -0600

    whitespace
    Signed-off-by: Jax DesMarais-Leder <jdesmarais@paypal.com>

--- Sources/BraintreePayPal/BTPayPalCreditFinancingAmount.swift
+++ Sources/BraintreePayPal/BTPayPalCreditFinancingAmount.swift
@@ -6,7 +6,6 @@ import Foundation
     /// 3 letter currency code as defined by <a href="http://www.iso.org/iso/home/standards/currency_codes.htm">ISO 4217</a>.
     public let currency: String
     
-    
     /// An amount defined by <a href="http://www.iso.org/iso/home/standards/currency_codes.htm">ISO 4217</a> for the given currency.
     public let value: String
     
