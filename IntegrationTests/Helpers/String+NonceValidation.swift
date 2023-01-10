79c5c872290e096d708fb627866bdff0dd8058f7
Matt Wylder <mwylder@paypal.com>
2023-01-10 14:57:19 -0600

    Cleanup test formatting
    Signed-off-by: Jax DesMarais-Leder <jdesmarais@paypal.com>

--- IntegrationTests/Helpers/String+NonceValidation.swift
+++ IntegrationTests/Helpers/String+NonceValidation.swift
@@ -1,19 +1,20 @@
 import Foundation
 
 extension String {
-    var isANonce: Bool {
+    
+    var isValidNonce: Bool {
         do {
             let noncePattern = "\\A[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}\\Z"
             var regex = try NSRegularExpression(pattern: noncePattern)
             
-            if regex.numberOfMatches(in: self, range: NSMakeRange(0, self.count)) > 0 {
+            if regex.numberOfMatches(in: self, range: NSRange(location: 0, length: self.count)) > 0 {
                 return true
             }
             
             let tokenizerNoncePattern = "\\Atokencc_[0-9a-z_]+\\Z"
             regex = try NSRegularExpression(pattern: tokenizerNoncePattern)
             
-            return regex.numberOfMatches(in: self, range: NSMakeRange(0, self.count)) > 0
+            return regex.numberOfMatches(in: self, range: NSRange(location: 0, length: self.count)) > 0
             
         } catch {
             NSLog("Error parsing regex: \(error.localizedDescription)")
