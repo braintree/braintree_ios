ad240d238fe7a1ecbf399c12c2edb60797f5ddcc
Matthew Wylder <matt@wylder.us>
2023-02-07 09:23:05 -0600

    Update `BTAnalyticsMetadata_Tests` to use Regex (#941)
    * Added match regex assertions to BTAnalyticsMetadata_Tests
    * Remove unneeded analytics events
    
    Co-authored-by: Jax DesMarais-Leder <jdesmarais@paypal.com>

--- Sources/BraintreeCore/BTAnalyticsMetadata.swift
+++ Sources/BraintreeCore/BTAnalyticsMetadata.swift
@@ -11,11 +11,8 @@ struct BTAnalyticsMetadata {
     static let merchantAppVersion: String = Bundle.main.infoDictionary?[kCFBundleVersionKey as String] as? String ?? ""
     static let merchantAppName: String = Bundle.main.infoDictionary?[kCFBundleNameKey as String] as? String ?? ""
     static let deviceManufacturer: String = "Apple"
-    static let iOSIdentifierForVendor: String = UIDevice.current.identifierForVendor?.uuidString ?? ""
-    static let iOSBaseSDK: String = "\(__IPHONE_OS_VERSION_MAX_ALLOWED)"
     static let iOSDeviceName: String = UIDevice.current.name
     static let iOSSystemName: String = UIDevice.current.systemName
-    static let isAppExtension: Bool = Bundle.main.bundleURL.pathExtension == "appex"
 
     // MARK: Metadata Computed Properties
 
@@ -40,65 +37,12 @@ struct BTAnalyticsMetadata {
         #endif
     }
 
-    static var iOSDeploymentTarget: String {
-        let rawVersionString: String? = Bundle.main.infoDictionary?["MinimumOSVersion"] as? String
-        let rawVersionArray: [String] = rawVersionString?.components(separatedBy: ".") ?? []
-        let firstIndexAsInt: Int = Int(rawVersionArray.first ?? "0") ?? 0
-        var formattedVersionNumber: Int = firstIndexAsInt * 10000
-
-        if rawVersionArray.count > 1 {
-            let indexAsInt: Int = Int(rawVersionArray[1]) ?? 0
-            formattedVersionNumber += indexAsInt * 100
-        }
-
-        return String(formattedVersionNumber)
-    }
-
-    static var deviceAppGeneratedPersistentUUID: String {
-        let deviceAppGeneratedPersistentUuidKeychainKey: String = "deviceAppGeneratedPersistentUuid"
-        var savedIdentifier: String = BTKeychain.stringForKey(deviceAppGeneratedPersistentUuidKeychainKey)
-
-        if savedIdentifier.count == 0 {
-            savedIdentifier = UUID().uuidString
-            let setDidSucceed = BTKeychain.setString(savedIdentifier, forKey: deviceAppGeneratedPersistentUuidKeychainKey)
-
-            if !setDidSucceed {
-                return ""
-            }
-        }
-
-        return savedIdentifier
-    }
-
     static var isSimulator: Bool {
         #if targetEnvironment(simulator)
             return true
         #else
             return false
         #endif
-    }
-
-    static var deviceScreenOrientation: String {
-        if isAppExtension {
-            return "AppExtension"
-        }
-
-        switch UIDevice.current.orientation {
-        case .faceUp:
-            return "FaceUp"
-        case .faceDown:
-            return "FaceDown"
-        case .portrait:
-            return "Portrait"
-        case .portraitUpsideDown:
-            return "PortraitUpsideDown"
-        case .landscapeLeft:
-            return "LandscapeLeft"
-        case .landscapeRight:
-            return "LandscapeRight"
-        default:
-            return "Unknown"
-        }
     }
 
     static var isVenmoInstalled: Bool {
@@ -134,13 +78,8 @@ struct BTAnalyticsMetadata {
             "deviceModel": deviceModel,
             "iosDeviceName": iOSDeviceName,
             "iosSystemName": iOSSystemName,
-            "iosBaseSDK": iOSBaseSDK,
-            "iosDeploymentTarget": iOSDeploymentTarget,
-            "iosIdentifierForVendor": iOSIdentifierForVendor,
             "iosPackageManager": iOSPackageManager,
-            "deviceAppGeneratedPersistentUuid": deviceAppGeneratedPersistentUUID,
             "isSimulator": isSimulator,
-            "deviceScreenOrientation": deviceScreenOrientation,
             "venmoInstalled": isVenmoInstalled,
             "dropinVersion": dropInVersion
         ]
--- UnitTests/BraintreeCoreTests/BTAnalyticsMetadata_Tests.swift
+++ UnitTests/BraintreeCoreTests/BTAnalyticsMetadata_Tests.swift
@@ -4,22 +4,19 @@ import XCTest
 
 final class BTAnalyticsMetadata_Tests: XCTestCase {
 
-    func testMetadataContainsAllData() {
+    func testMetadata_ContainsValidData() {
         let metadata = BTAnalyticsMetadata.metadata
 
         XCTAssertEqual(metadata["platform"] as? String, "iOS")
-        XCTAssertNotNil(metadata["platformVersion"] as? String)
-        XCTAssertEqual(metadata["sdkVersion"] as? String, BTCoreConstants.braintreeSDKVersion)
-        XCTAssertNotNil(metadata["merchantAppId"] as? String)
-        XCTAssertNotNil(metadata["merchantAppName"] as? String)
+        XCTAssertTrue((metadata["sdkVersion"] as! String).matches("^\\d+\\.\\d+\\.\\d+(-[0-9a-zA-Z-]+)?$"))
+        XCTAssertEqual(metadata["merchantAppId"] as? String, "com.apple.dt.xctest.tool")
+        XCTAssertEqual(metadata["merchantAppName"] as? String, "xctest")
         XCTAssertNotNil(metadata["merchantAppVersion"] as? String)
+        XCTAssertNotNil(metadata["iosDeviceName"] as? String)
+        XCTAssertTrue((metadata["iosSystemName"] as! String).matches("iOS|iPadOS"))
         XCTAssertEqual(metadata["deviceManufacturer"] as? String, "Apple")
-        XCTAssertNotNil(metadata["deviceModel"] as? String)
-        XCTAssertNotNil(metadata["deviceAppGeneratedPersistentUuid"] as? String)
-        XCTAssertNotNil(metadata["iosIdentifierForVendor"] as? String)
-        XCTAssertNotNil(metadata["iosPackageManager"] as? String)
-        XCTAssertNotNil(metadata["isSimulator"] as? Bool)
-        XCTAssertNotNil(metadata["deviceScreenOrientation"] as? String)
+        XCTAssertTrue((metadata["deviceModel"] as! String).matches("iPhone\\d,\\d|i386|x86_64|arm64"))
+        XCTAssertTrue((metadata["iosPackageManager"] as! String).matches("Carthage or Other|CocoaPods|Swift Package Manager"))
+        XCTAssertEqual(metadata["isSimulator"] as? Bool, true)
     }
-
 }
