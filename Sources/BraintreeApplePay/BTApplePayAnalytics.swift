fcf9f4bb049b1227b99d7bf81e9926064e0b3cf2
Jax DesMarais-Leder <jdesmarais@paypal.com>
2023-03-03 14:05:10 -0600

    Audit Apple Pay Analytics Events (#959)
    - Update ApplePay analytics events to prepare for switch to FPTI
    - Update tests
    
    Co-authored-by: Victoria Park <vicpark@paypal.com>
    Co-authored-by: Sammy Cannillo <scannillo@paypal.com>
    Co-authored-by: Jax DesMarais-Leder <jdesmarais@paypal.com>

---
+++ Sources/BraintreeApplePay/BTApplePayAnalytics.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+// NEXT_MAJOR_VERSION: Move this class to an enum once the entire SDK is in Swift
+class BTApplePayAnalytics {
+
+    // MARK: - Payment Request Events
+
+    static let paymentRequestStarted = "apple-pay:payment-request:started"
+    static let paymentRequestFailed = "apple-pay:payment-request:failed"
+    static let paymentRequestSucceeded = "apple-pay:payment-request:succeeded"
+
+    // MARK: - Tokenize Request Events
+
+    static let tokenizeStarted = "apple-pay:tokenize:started"
+    static let tokenizeFailed = "apple-pay:tokenize:failed"
+    static let tokenizeNetworkConnectionLost = "apple-pay:tokenize:network-connection:failed"
+    static let tokenizeSucceeded = "apple-pay:tokenize:succeeded"
+}
