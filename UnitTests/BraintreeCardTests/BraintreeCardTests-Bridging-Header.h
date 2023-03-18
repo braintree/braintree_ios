7697e677bad67117234ba6149949bc10708f624f
Jax DesMarais-Leder <jdesmarais@paypal.com>
2023-03-17 21:27:55 -0500

    update tests

--- UnitTests/BraintreeCardTests/BraintreeCardTests-Bridging-Header.h
+++ UnitTests/BraintreeCardTests/BraintreeCardTests-Bridging-Header.h
@@ -1,4 +1,3 @@
-#import <BraintreeCard/BTAuthenticationInsight_Internal.h>
 #import <BraintreeCard/BTCard_Internal.h>
 #import <BraintreeCard/BTCardClient_Internal.h>
 #import <BraintreeCard/BTCardNonce_Internal.h>
