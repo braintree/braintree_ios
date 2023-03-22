321c4fccc811ec3bc089a1010554596fed7832f5
Victoria Park <vicpark@paypal.com>
2023-03-22 13:19:46 -0700

    paypal analytics reorder BTPayPalAnalytics items

--- Sources/BraintreePayPal/BTPayPalAnalytics.swift
+++ Sources/BraintreePayPal/BTPayPalAnalytics.swift
@@ -10,13 +10,14 @@ class BTPayPalAnalytics {
     static let tokenizeSucceeded = "paypal:tokenize:succeeded"
     static let browserLoginCanceled = "paypal:tokenize:browser-login:canceled"
    
-    // MARK: - Tokenize Events Additional Detail analytic Messages
-    static let tokenizeNetworkConnectionFailed = "paypal:tokenize:network-connection:failed"
-
+    // MARK: - Browser Presentation Events
+  
     static let browserPresentationStarted = "paypal:tokenize:browser-presentation:started"
     static let browserPresentationSucceeded = "paypal:tokenize:browser-presentation:succeeded"
     static let browserPresentationFailed = "paypal:tokenize:browser-presentation:failed"
     
+    // MARK: - Browser Login Events
+    static let tokenizeNetworkConnectionFailed = "paypal:tokenize:network-connection:failed"
     static let browserLoginFailed = "paypal:tokenize:browser-login:failed"
     // specific cancel from permisison alert
     static let webSessionAlertCanceled = "paypal:tokenize:web-session-alert:canceled"
