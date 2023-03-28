be88a53fea66230ef10369e4209ea9241e71c6a2
Sammy Cannillo <scannillo@paypal.com>
2023-03-28 17:27:44 -0500

    Create BTThreeDSecureRequest.swift & shell for public, internal, and private method definitions

---
+++ Sources/BraintreeThreeDSecure/BTThreeDSecureRequestSwift.swift
@@ -0,0 +1,124 @@
+import Foundation
+import BraintreeCore
+
+@objcMembers public class BTThreeDSecureRequestSwift: NSObject {
+    
+    // MARK: - Public Properties
+    
+    public let nonce: String
+    
+    public let amount: Decimal
+    
+    public let accountType: BTThreeDSecureAccountType?
+    
+    public let billingAddress: BTThreeDSecurePostalAddress?
+    
+    public let mobilePhoneNumber: String?
+    
+    public let email: String?
+    
+    public let shippingMethod: BTThreeDSecureShippingMethod?
+    
+    public let additionalInformation: BTThreeDSecureAdditionalInformation?
+    
+    public let challengeRequested: Bool?
+    
+    public let exemptionRequested: Bool?
+    
+    public let requestedExcemptionType: BTThreeDSecureRequestedExemptionType?
+
+    public let dataOnlyRequested: Bool?
+    
+    public let cardAddChallenge: BTThreeDSecureCardAddChallenge?
+    
+    public let v2UICustomization: BTThreeDSecureV2UICustomization?
+    
+    public weak var threeDSecureRequestDelegate: BTThreeDSecureRequestDelegate?
+    
+    // MARK: - Internal Properties
+    
+    weak var paymentFlowClientDelegate: BTPaymentFlowClientDelegate?
+    let dfReferenceID: String = ""
+    var accountTypeAsString: String = ""
+    let shippingMethodAsString: String = ""
+    let requestedExemptionTypeAsString: String = ""
+    
+    // MARK: - Initializer
+    
+    public init(
+        nonce: String, amount: Decimal,
+        accountType: BTThreeDSecureAccountType? = nil,
+        billingAddress: BTThreeDSecurePostalAddress? = nil,
+        mobilePhoneNumber: String? = nil,
+        email: String? = nil,
+        shippingMethod: BTThreeDSecureShippingMethod? = nil,
+        additionalInformation: BTThreeDSecureAdditionalInformation? = nil,
+        challengeRequested: Bool? = nil,
+        exemptionRequested: Bool? = nil,
+        requestedExcemptionType: BTThreeDSecureRequestedExemptionType? = nil,
+        dataOnlyRequested: Bool? = nil,
+        cardAddChallenge: BTThreeDSecureCardAddChallenge? = nil,
+        v2UICustomization: BTThreeDSecureV2UICustomization? = nil,
+        threeDSecureRequestDelegate: BTThreeDSecureRequestDelegate? = nil
+    ) {
+        self.nonce = nonce
+        self.amount = amount
+        self.accountType = accountType
+        self.billingAddress = billingAddress
+        self.mobilePhoneNumber = mobilePhoneNumber
+        self.email = email
+        self.shippingMethod = shippingMethod
+        self.additionalInformation = additionalInformation
+        self.challengeRequested = challengeRequested
+        self.exemptionRequested = exemptionRequested
+        self.requestedExcemptionType = requestedExcemptionType
+        self.dataOnlyRequested = dataOnlyRequested
+        self.cardAddChallenge = cardAddChallenge
+        self.v2UICustomization = v2UICustomization
+        self.threeDSecureRequestDelegate = threeDSecureRequestDelegate
+    }
+    
+    // MARK: - Internal Methods
+    
+    func prepareLookup(apiClient: BTAPIClient, completion: (NSError?) -> Void) {
+        
+    }
+    
+    func processLookupResult(lookupResult: BTThreeDSecureResult, configuration: BTConfiguration) {
+        
+    }
+    
+    // MARK: - Private Methods
+    
+    func handleRequest(request: BTPaymentFlowRequest, apiClient: BTAPIClient, paymentClientDelegate: BTPaymentFlowClientDelegate) {
+        
+    }
+    
+    func startRequest(request: BTPaymentFlowRequest, configuration: BTConfiguration) {
+        
+    }
+    
+    func performV2Authentication(lookupResult: BTThreeDSecureResult) {
+        
+    }
+    
+    func handleOpenURL(url: NSURL) {
+        
+    }
+    
+    func logThreeDSecureCompletedAnalyticsForResult(result: BTThreeDSecureResult, apiClient: BTAPIClient) {
+        
+    }
+    
+    func paymentFlowName() -> String {
+        return "three-d-secure"
+    }
+    
+    func stringForBool(boolean: Bool) -> String {
+        return boolean ? "true" : "false"
+    }
+    
+    func onLookupComplete(_ request: BTThreeDSecureRequest, lookupResult result: BTThreeDSecureResult, next: (() -> Void)) {
+        next()
+    }
+}
