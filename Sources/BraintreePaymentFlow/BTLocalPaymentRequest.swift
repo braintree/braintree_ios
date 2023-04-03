137864040787e4862f2c51ab1ffdbaf0369baa26
Sammy Cannillo <scannillo@paypal.com>
2023-04-03 09:01:36 -0500

    WIP - convert BTLocalPaymentRequest

--- Sources/BraintreePaymentFlow/BTLocalPaymentRequest.swift
+++ Sources/BraintreePaymentFlow/BTLocalPaymentRequest.swift
@@ -1,7 +1,13 @@
+#if canImport(BraintreeCore)
+import BraintreeCore
+#endif
+
+#if canImport(BraintreeDataCollector)
 import BraintreeDataCollector
+#endif
 
 /// Used to initialize a local payment flow
-public class BTLocalPaymentRequestSwift: BTPaymentFlowRequest {
+@objcMembers public class BTLocalPaymentRequestSwift: BTPaymentFlowRequest {
     
     // MARK: - Public Properties
     
@@ -65,17 +71,106 @@ extension BTLocalPaymentRequestSwift: BTPaymentFlowRequestDelegate {
         paymentClientDelegate delegate: BTPaymentFlowClientDelegate
     ) {
         paymentFlowClientDelegate = delegate
-        apiClient.fetchOrReturnRemoteConfiguration { configuration, configurationError in
-            if let configurationError {
-                delegate.onPaymentComplete(nil, error: configurationError)
+        let localPaymentRequest = request as! BTLocalPaymentRequest
+        
+        apiClient.fetchOrReturnRemoteConfiguration { [weak self] configuration, error in
+            guard let self else { return }
+            
+            if let error {
+                delegate.onPaymentComplete(nil, error: error)
             }
             
             let dataCollector = BTDataCollector(apiClient: apiClient)
-            correlationID = dataCollector.clientMetadataID(nil)
-            
-            let integrationError: NSError
-            
-            
+            self.correlationID = dataCollector.clientMetadataID(nil)
+
+            var integrationError: NSError
+
+            if configuration?.isLocalPaymentEnabled? {
+                NSLog("%@ Enable PayPal for this merchant in the Braintree Control Panel to use Local Payments.", BTLogLevelDescription.string(for: .critical))
+                integrationError = NSError(domain: BTPaymentFlowErrorDomain, code: BTPaymentFlowErrorType.disabled, userInfo: [NSLocalizedDescriptionKey:"Enable PayPal for this merchant in the Braintree Control Panel to use Local Payments."])
+            } else if (localPaymentRequest.localPaymentFlowDelegate == nil) {
+                NSLog("%@ BTLocalPaymentRequest localPaymentFlowDelegate can not be nil.", BTLogLevelDescription.string(for: .critical))
+                integrationError = NSError(domain: BTPaymentFlowErrorDomain, code: BTPaymentFlowErrorType.integration, userInfo: [NSLocalizedDescriptionKey:"Failed to begin payment flow: BTLocalPaymentRequest localPaymentFlowDelegate can not be nil."])
+            } else if (localPaymentRequest.amount == nil) {
+                NSLog("%@ BTLocalPaymentRequest amount and paymentType can not be nil.", BTLogLevelDescription.string(for: .critical))
+                integrationError = NSError(domain: BTPaymentFlowErrorDomain, code: BTPaymentFlowErrorType.integration, userInfo: [NSLocalizedDescriptionKey:"Failed to begin payment flow: BTLocalPaymentRequest amount and paymentType can not be nil."])
+            }
+
+            if integrationError != nil {
+                delegate.onPaymentComplete(nil, error: integrationError)
+                return
+            }
+
+            var params = [
+                "amount": localPaymentRequest.amount,
+                "funding_source": localPaymentRequest.paymentType,
+                "intent": "sale",
+                "return_url": "\(BTCoreConstants.callbackURLScheme)://x-callback-url/braintree/local-payment/success",
+                "cancel_url": "\(BTCoreConstants.callbackURLScheme)://x-callback-url/braintree/local-payment/cancel"
+            ]
+
+            if let countryCode = localPaymentRequest.paymentTypeCountryCode {
+                params["payment_type_country_code"] = countryCode
+            }
+
+            // TODO - don't think we ned this chaos and constant nil checking
+            if let address = localPaymentRequest.address {
+                params["line1"] = address.streetAddress
+                params["line2"] = address.extendedAddress
+                params["city"] = address.locality
+                params["state"] = address.region
+                params["postal_code"] = address.postalCode
+                params["country_code"] = address.countryCodeAlpha2
+            }
+
+            if let currencyCode = localPaymentRequest.currencyCode {
+                params["currency_ios_code"]
+            }
+
+            if let givenName = localPaymentRequest.givenName {
+                params["first_name"] = givenName
+            }
+
+            if let surname = localPaymentRequest.surname {
+                params["last_name"] = surname
+            }
+
+            if let email = localPaymentRequest.email {
+                params["payer_email"] = email
+            }
+
+            if let phone = localPaymentRequest.phone {
+                params["phone"] = phone
+            }
+
+            if let merchantAccountID = localPaymentRequest.merchantAccountID {
+                params["merchant_account_id"] = merchantAccountID
+            }
+
+            if let bic = localPaymentRequest.bic {
+                params["bic"] = bic
+            }
+
+            var experienceProfile: [String: Any] = [
+                "no_shipping": localPaymentRequest.isShippingAddressRequired,
+            ]
+
+            if let displayName = localPaymentRequest.displayName {
+                experienceProfile["brand_name"] = displayName
+            }
+
+            params["experience_profile"] = experienceProfile
+
+            apiClient.post("v1/local_payments/create", parameters: params) { body, response, error in
+                if let error {
+                    // LEFT OFF HERE
+                    self.paymentID = body?["paymentResource"]["paymentToken"].asString()
+                    let approvalURLString = body?["paymentResource"]["redirectUrl"]
+                    let url = URL(string: approvalURLString)
+
+                    // continue
+                }
+            }
         }
     }
 
