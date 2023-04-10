565c767055aa062b58d7a2f216b5925a5bd97bc6
Victoria Park <vicpark@paypal.com>
2023-04-10 10:56:47 -0700

    Venmo analytics audit (#994)
    * venmo analytics audit
    
    Signed-off-by: Steven Shropshire <steven.shropshire@paypal.com>
    
    * added missed fail events and pr feedback
    
    * pr feedback
    
    * pr feedback grouping messages
    
    ---------
    
    Signed-off-by: Steven Shropshire <steven.shropshire@paypal.com>

---
+++ Sources/BraintreeVenmo/BTVenmoAnalytics.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+class BTVenmoAnalytics {
+    
+    // MARK: - Conversion Events
+    
+    static let tokenizeStarted = "venmo:tokenize:started"
+    static let tokenizeFailed = "venmo:tokenize:failed"
+    static let tokenizeSucceeded = "venmo:tokenize:succeeded"    
+    static let appSwitchCanceled = "venmo:tokenize:app-switch:canceled"
+    
+    // MARK: - Additional Detail Events
+    
+    static let tokenizeNetworkConnectionLost = "venmo:tokenize:network-connection:failed"
+    static let appSwitchSucceeded = "venmo:tokenize:app-switch:succeeded"
+    static let appSwitchFailed = "venmo:tokenize:app-switch:failed"
+}
