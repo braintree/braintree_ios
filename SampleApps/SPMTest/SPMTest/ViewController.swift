a96476b50f61b1d7e26f7a77c0176696c4363ff9
braintreeps <code@getbraintree.com>
2023-04-18 16:51:34 +0000

    Revert "Remove SPMTest app to avoid Carthage timeout"
    This reverts commit 16ccdfdc28d9fb204f661dec502332f2a2c9ec9f.

---
+++ SampleApps/SPMTest/SPMTest/ViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+import BraintreeAmericanExpress
+import BraintreeApplePay
+import BraintreeCard
+import BraintreeCore
+import BraintreeDataCollector
+import BraintreePaymentFlow
+import BraintreePayPal
+import BraintreeThreeDSecure
+import BraintreeVenmo
+import BraintreePayPalNativeCheckout
+import BraintreeSEPADirectDebit
+
+class ViewController: UIViewController {
+
+}
