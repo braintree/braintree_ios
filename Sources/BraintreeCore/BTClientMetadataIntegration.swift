95fe7557bccca28fbc78d2926648f9aadb459cef
scannillo <35243507+scannillo@users.noreply.github.com>
2023-04-28 09:52:50 -0500

    Remove unknown & dropIn2 integration types (#1021)

--- Sources/BraintreeCore/BTClientMetadataIntegration.swift
+++ Sources/BraintreeCore/BTClientMetadataIntegration.swift
@@ -6,23 +6,13 @@
     /// Drop-in
     case dropIn
     
-    /// Drop-in 2
-    case dropIn2
-    
-    /// Unknown Integration
-    case unknown
-    
     /// String value representing the integration.
     var stringValue: String {
         switch self {
-        case .unknown:
-            return "unknown"
-        case .dropIn:
-            return "dropin"
-        case .dropIn2:
-            return "dropin2"
         case .custom:
             return "custom"
+        case .dropIn:
+            return "dropin"
         }
     }
 }
