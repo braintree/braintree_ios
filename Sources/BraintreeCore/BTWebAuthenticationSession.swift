2e45c0fdcd8e5e08b8e3fe4abcd496eb590ca8fc
Jax DesMarais-Leder <jdesmarais@paypal.com>
2023-05-25 14:09:29 -0500

    add BTWebAuthenticationSession to core

--- Sources/BraintreeCore/BTWebAuthenticationSession.swift
+++ Sources/BraintreeCore/BTWebAuthenticationSession.swift
@@ -1,12 +1,12 @@
 import Foundation
 import AuthenticationServices
 
-#if canImport(BraintreeCore)
-import BraintreeCore
-#endif
+///  :nodoc: This method is exposed for internal Braintree use only. Do not use. It is not covered by Semantic Versioning and may change or be removed at any time.
+@_documentation(visibility: private)
+public class BTWebAuthenticationSession: NSObject {
 
-class WebAuthenticationSession: NSObject {
-    func start(
+    ///  :nodoc: This method is exposed for internal Braintree use only. Do not use. It is not covered by Semantic Versioning and may change or be removed at any time.
+    public func start(
         url: URL,
         context: ASWebAuthenticationPresentationContextProviding,
         sessionDidDisplay: @escaping (Bool) -> Void,
