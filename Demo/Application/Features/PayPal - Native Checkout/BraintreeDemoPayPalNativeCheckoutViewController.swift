412c1cf0a5629499adb110e1a659855ef354461e
Stephanie <127455800+stechiu@users.noreply.github.com>
2023-07-13 14:01:52 -0700

    Added BA With Purchase. Refactored button code

--- Demo/Application/Features/PayPal - Native Checkout/BraintreeDemoPayPalNativeCheckoutViewController.swift
+++ Demo/Application/Features/PayPal - Native Checkout/BraintreeDemoPayPalNativeCheckoutViewController.swift
@@ -5,27 +5,30 @@ import BraintreePayPalNativeCheckout
 class BraintreeDemoPayPalNativeCheckoutViewController: BraintreeDemoPaymentButtonBaseViewController {
     lazy var payPalNativeCheckoutClient = BTPayPalNativeCheckoutClient(apiClient: apiClient)
 
+    func checkoutPaymentButton(title: String) -> UIButton {
+      let button = UIButton(type: .system)
+      button.setTitle(title, for: .normal)
+      button.setTitleColor(.blue, for: .normal)
+      button.setTitleColor(.lightGray, for: .highlighted)
+      button.setTitleColor(.lightGray, for: .disabled)
+      button.translatesAutoresizingMaskIntoConstraints = false
+      return button
+    }
+
     override func createPaymentButton() -> UIView! {
-        let payPalCheckoutButton = UIButton(type: .system)
-        payPalCheckoutButton.setTitle("One Time Checkout", for: .normal)
-        payPalCheckoutButton.setTitleColor(.blue, for: .normal)
-        payPalCheckoutButton.setTitleColor(.lightGray, for: .highlighted)
-        payPalCheckoutButton.setTitleColor(.lightGray, for: .disabled)
+        let payPalCheckoutButton = checkoutPaymentButton(title: "One Time Checkout")
         payPalCheckoutButton.addTarget(self, action: #selector(tappedPayPalCheckout), for: .touchUpInside)
-        payPalCheckoutButton.translatesAutoresizingMaskIntoConstraints = false
 
-        let payPalVaultButton = UIButton(type: .system)
-        payPalVaultButton.setTitle("Vault Checkout", for: .normal)
-        payPalVaultButton.setTitleColor(.blue, for: .normal)
-        payPalVaultButton.setTitleColor(.lightGray, for: .highlighted)
-        payPalVaultButton.setTitleColor(.lightGray, for: .disabled)
-        payPalVaultButton.addTarget(self, action: #selector(tappedPayPalVault), for: .touchUpInside)
-        payPalVaultButton.translatesAutoresizingMaskIntoConstraints = false
+        let payPalBAWithoutPurchase = checkoutPaymentButton(title: "Billing Agreements Without Purchase Checkout")
+        payPalBAWithoutPurchase.addTarget(self, action: #selector(tappedPayPalVault), for: .touchUpInside)
 
-        let stackView = UIStackView(arrangedSubviews: [payPalCheckoutButton, payPalVaultButton])
+        let payPalBAWithPurchase = checkoutPaymentButton(title: "Billing Agreements With Purchase Checkout")
+        payPalBAWithPurchase.addTarget(self, action: #selector(tappedBAWithPurchase), for: .touchUpInside)
+
+        let stackView = UIStackView(arrangedSubviews: [payPalCheckoutButton, payPalBAWithoutPurchase, payPalBAWithPurchase])
         stackView.axis = .vertical
-        stackView.spacing = 5
         stackView.alignment = .center
+      stackView.distribution = .fillEqually
         stackView.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate(
@@ -33,8 +36,11 @@ class BraintreeDemoPayPalNativeCheckoutViewController: BraintreeDemoPaymentButto
                 payPalCheckoutButton.topAnchor.constraint(equalTo: stackView.topAnchor),
                 payPalCheckoutButton.heightAnchor.constraint(equalToConstant: 19.5),
 
-                payPalVaultButton.topAnchor.constraint(equalTo: payPalCheckoutButton.bottomAnchor, constant: 5),
-                payPalVaultButton.heightAnchor.constraint(equalToConstant: 19.5)
+                payPalBAWithoutPurchase.topAnchor.constraint(equalTo: payPalCheckoutButton.bottomAnchor, constant: 20),
+                payPalBAWithoutPurchase.heightAnchor.constraint(equalToConstant: 19.5),
+
+                payPalBAWithPurchase.topAnchor.constraint(equalTo: payPalBAWithoutPurchase.bottomAnchor, constant: 20),
+                payPalBAWithPurchase.heightAnchor.constraint(equalToConstant: 19.5)
             ]
         )
 
@@ -45,7 +51,7 @@ class BraintreeDemoPayPalNativeCheckoutViewController: BraintreeDemoPaymentButto
         progressBlock("Tapped PayPal - Native Checkout using BTPayPalNativeCheckout")
         sender.setTitle("Processing...", for: .disabled)
         sender.isEnabled = false
-                
+
         let request = BTPayPalNativeCheckoutRequest(amount: "4.30")
         payPalNativeCheckoutClient.tokenize(request) { nonce, error in
             sender.isEnabled = true
@@ -58,16 +64,34 @@ class BraintreeDemoPayPalNativeCheckoutViewController: BraintreeDemoPaymentButto
         }
     }
 
-    @objc func tappedPayPalVault(_ sender: UIButton) {
-        progressBlock("Tapped PayPal - Vault using BTPayPalNativeCheckout")
-        sender.setTitle("Processing...", for: .disabled)
-        sender.isEnabled = false
+        @objc func tappedPayPalVault(_ sender: UIButton) {
+            progressBlock("Tapped PayPal - Vault using BTPayPalNativeCheckout")
+            sender.setTitle("Processing...", for: .disabled)
+            sender.isEnabled = false
+            
+            let request = BTPayPalNativeVaultRequest()
+            
+            payPalNativeCheckoutClient.tokenize(request) { nonce, error in
+                sender.isEnabled = true
+              
+                guard let nonce = nonce else {
+                  self.progressBlock(error?.localizedDescription)
+                  return
+                }
+                self.completionBlock(nonce)
+          }
+      }
 
-        let request = BTPayPalNativeVaultRequest()
-
-        payPalNativeCheckoutClient.tokenize(request) { nonce, error in
-            sender.isEnabled = true
-
+        @objc func tappedBAWithPurchase(_ sender: UIButton) {
+            progressBlock("Tapped PayPal - BA With Purchase using BTPayPalNativeCheckout")
+            sender.setTitle("Processing...", for: .disabled)
+            sender.isEnabled = false
+            
+            let request =  BTPayPalNativeCheckoutRequest(amount: "4.30", requestBillingAgreement: true)
+            
+            payPalNativeCheckoutClient.tokenize(request) { nonce, error in
+                sender.isEnabled = true
+            
             guard let nonce = nonce else {
                 self.progressBlock(error?.localizedDescription)
                 return
