3176f8b8a9170c839968afc61252456cc31821c7
Stephanie <127455800+stechiu@users.noreply.github.com>
2023-07-31 12:06:46 -0700

    Refactored button method code

--- Demo/Application/Features/PayPal - Native Checkout/BraintreeDemoPayPalNativeCheckoutViewController.swift
+++ Demo/Application/Features/PayPal - Native Checkout/BraintreeDemoPayPalNativeCheckoutViewController.swift
@@ -5,29 +5,27 @@ import BraintreePayPalNativeCheckout
 class BraintreeDemoPayPalNativeCheckoutViewController: BraintreeDemoPaymentButtonBaseViewController {
     lazy var payPalNativeCheckoutClient = BTPayPalNativeCheckoutClient(apiClient: apiClient)
 
-    func checkoutPaymentButton(title: String) -> UIButton {
+    func checkoutPaymentButton(title: String, action: Selector) -> UIButton {
         let button = UIButton(type: .system)
         button.setTitle(title, for: .normal)
         button.setTitleColor(.blue, for: .normal)
         button.setTitleColor(.lightGray, for: .highlighted)
         button.setTitleColor(.lightGray, for: .disabled)
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: action, for: .touchUpInside)
         return button
     }
 
     override func createPaymentButton() -> UIView! {
-        let payPalCheckoutButton = checkoutPaymentButton(title: "One Time Checkout")
-        payPalCheckoutButton.addTarget(self, action: #selector(tappedPayPalCheckout), for: .touchUpInside)
+        let payPalCheckoutButton = checkoutPaymentButton(title: "One Time Checkout", action: #selector(tappedPayPalCheckout)
 
         // Buyers are shown a billing agreement without purchase
         // For more information: https://developer.paypal.com/braintree/docs/guides/paypal/vault/ios/v5
-        let vaultCheckoutButton = checkoutPaymentButton(title: "Vault Checkout")
-        vaultCheckoutButton.addTarget(self, action: #selector(tappedVaultCheckout), for: .touchUpInside)
+        let vaultCheckoutButton = checkoutPaymentButton(title: "Vault Checkout", action: #selector(tappedVaultCheckout)
 
         // Buyers are shown a billing agreement with purchase
         // For more information: https://developer.paypal.com/braintree/docs/guides/paypal/checkout-with-vault/ios/v5
-        let checkoutWithVaultButton = checkoutPaymentButton(title: "Checkout With Vault")
-        checkoutWithVaultButton.addTarget(self, action: #selector(tappedCheckoutWithVault), for: .touchUpInside)
+        let checkoutWithVaultButton = checkoutPaymentButton(title: "Checkout With Vault", action: #selector(tappedCheckoutWithVault)
 
         let stackView = UIStackView(arrangedSubviews: [payPalCheckoutButton, vaultCheckoutButton, checkoutWithVaultButton])
         stackView.axis = .vertical
