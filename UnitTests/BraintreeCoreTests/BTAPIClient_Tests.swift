c36f2381292aa3600a75796effb9d63575ce2e0c
Jax DesMarais-Leder <jdesmarais@paypal.com>
2023-09-25 09:16:58 -0500

    update BTAPIClient_Tests

--- UnitTests/BraintreeCoreTests/BTAPIClient_Tests.swift
+++ UnitTests/BraintreeCoreTests/BTAPIClient_Tests.swift
@@ -248,11 +248,11 @@ class BTAPIClient_Tests: XCTestCase {
 
             let firstNonce = paymentMethodNonces[0];
             XCTAssertEqual(firstNonce.nonce, "fake-nonce1")
-            XCTAssertEqual(firstNonce.type, "CreditCard")
+            XCTAssertEqual(firstNonce.type, "AMEX")
 
             let secondNonce = paymentMethodNonces[1]
             XCTAssertEqual(secondNonce.nonce, "fake-nonce2")
-            XCTAssertEqual(secondNonce.type, "PayPalAccount")
+            XCTAssertEqual(secondNonce.type, "PayPal")
 
             expectation.fulfill()
         }
