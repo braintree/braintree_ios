a4b62adb7610ac44058c3f7cdf255251e95c393c
Sammy Cannillo <scannillo@paypal.com>
2023-12-06 15:47:35 -0600

    Cleanup docstrings

--- Sources/BraintreeCore/UIApplication+URLOpener.swift
+++ Sources/BraintreeCore/UIApplication+URLOpener.swift
@@ -1,6 +1,8 @@
 import UIKit
 
 /// :nodoc: This protocol is exposed for internal Braintree use only. Do not use. It is not covered by Semantic Versioning and may change or be removed at any time.
+///
+/// Used to mock `UIApplication`.
 @_documentation(visibility: private)
 public protocol URLOpener {
     
