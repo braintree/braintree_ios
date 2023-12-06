177d2f9a459f67d6bfee50be01e19706e6ceb8a5
Sammy Cannillo <scannillo@paypal.com>
2023-12-06 15:41:24 -0600

    Cleanup - add nodocs to URLOpener protocol

--- Sources/BraintreeCore/UIApplication+URLOpener.swift
+++ Sources/BraintreeCore/UIApplication+URLOpener.swift
@@ -1,20 +1,11 @@
-//
-//  UIApplication+URLOpener.swift
-//  BraintreeCore
-//
-//  Created by Samantha Cannillo on 12/6/23.
-//
-
 import UIKit
 
+/// :nodoc: This protocol is exposed for internal Braintree use only. Do not use. It is not covered by Semantic Versioning and may change or be removed at any time.
+@_documentation(visibility: private)
 public protocol URLOpener {
     
     func canOpenURL(_ url: URL) -> Bool
-    func open(
-        _ url: URL,
-        options: [UIApplication.OpenExternalURLOptionsKey : Any],
-        completionHandler completion: ((Bool) -> Void)?
-    )
+    func open(_ url: URL, options: [UIApplication.OpenExternalURLOptionsKey: Any], completionHandler completion: ((Bool) -> Void)?)
 }
 
 extension UIApplication: URLOpener { }
