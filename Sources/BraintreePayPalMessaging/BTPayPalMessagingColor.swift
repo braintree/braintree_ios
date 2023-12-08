1d453927c604546f6ef0806dcbc2766f5af771fa
Jax DesMarais-Leder <jdesmarais@paypal.com>
2023-12-08 09:48:41 -0600

    add BTPayPalMessagingColor enum

---
+++ Sources/BraintreePayPalMessaging/BTPayPalMessagingColor.swift
@@ -0,0 +1,31 @@
+import Foundation
+import PayPalMessages
+
+/// Text and logo color option for a PayPal Message
+public enum BTPayPalMessagingColor {
+
+    /// Black text with a color logo
+    case black
+
+    /// White text with a white logo
+    case white
+
+    /// Black text with a black logo
+    case monochrome
+
+    /// Black text with a desaturated logo
+    case grayscale
+
+    var messageColorRawValue: PayPalMessageColor {
+        switch self {
+        case .black:
+            return .black
+        case .white:
+            return .white
+        case .monochrome:
+            return .monochrome
+        case .grayscale:
+            return .grayscale
+        }
+    }
+}
