7e4a5c18d89ce19f9587fd9d06b9adf0e2c3c5b5
Jax DesMarais-Leder <jdesmarais@paypal.com>
2023-12-08 09:49:55 -0600

    add BTPayPalMessagingDelegate protocol

---
+++ Sources/BraintreePayPalMessaging/BTPayPalMessagingDelegate.swift
@@ -0,0 +1,20 @@
+import Foundation
+import PayPalMessages
+
+public protocol BTPayPalMessagingDelegate {
+
+    /// Function invoked when the message is tapped
+    func didSelect(_ payPalMessagingView: BTPayPalMessagingView)
+
+    /// Function invoked when a user has begun the PayPal Credit application
+    func willApply(_ payPalMessagingView: BTPayPalMessagingView)
+
+    /// Function invoked when the message first starts to fetch data
+    func willAppear(_ payPalMessagingView: BTPayPalMessagingView)
+
+    /// Function invoked when the message has rendered
+    func didAppear(_ payPalMessagingView: BTPayPalMessagingView)
+
+    /// Function invoked when the message encounters an error
+    func onError(_ payPalMessagingView: BTPayPalMessagingView, error: Error)
+}
