9b6f87fcbb0b6d990cc1b54ef7649b2d77b3bc32
Sammy Cannillo <scannillo@paypal.com>
2023-12-11 17:00:10 -0600

    Fixup - address duplicate info.plist build error

--- Sources/BraintreeShopperInsights/BTShopperInsightsRequest.swift
+++ Sources/BraintreeShopperInsights/BTShopperInsightsRequest.swift
@@ -50,7 +50,7 @@ public struct Phone {
     ///   - countryCode: The buyer's country code prefix to the national telephone number. An identifier for a specific country. Must not contain special characters.
     ///   - nationalNumber: The buyer's national phone number. Must not contain special characters. Must not contain special characters.
     /// - Note: This feature is in beta. It's public API may change or be removed in future releases.
-    init(countryCode: String, nationalNumber: String) {
+    public init(countryCode: String, nationalNumber: String) {
         self.countryCode = countryCode
         self.nationalNumber = nationalNumber
     }
