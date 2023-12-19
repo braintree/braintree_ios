e0379d353044fe7d651d12be69f38a6335b380cf
scannillo <35243507+scannillo@users.noreply.github.com>
2023-12-19 09:49:29 -0600

    ShopperInsights - Merchant Analytics (#1162)

---
+++ Sources/BraintreeShopperInsights/BTShopperInsightsAnalytics.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+enum BTShopperInsightsAnalytics {
+    
+    // MARK: - Merchant Triggered Events
+      
+    static let paypalPresented = "shopper-insights:paypal-presented"
+    static let paypalSelected = "shopper-insights:paypal-selected"
+    static let venmoPresented = "shopper-insights:venmo-presented"
+    static let venmoSelected = "shopper-insights:venmo-selected"
+}
