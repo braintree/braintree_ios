e6f044cd0b1a6eee6e54017cc380a1aa6230a04e
Sudha Vankadara <vsudha.rani@gmail.com>
2024-01-03 12:22:09 -0800

    fixing enums and adding unittests

--- Sources/BraintreePayPal/BTPayPalLineItem.swift
+++ Sources/BraintreePayPal/BTPayPalLineItem.swift
@@ -9,29 +9,33 @@ import Foundation
     case credit
 }
 
-/// Use this option to specify  the Upc type of the lien item. 
-@objc public enum BTPayPalLineItemUpcType: Int {
+
+/// Use this option to specify  the Upc type of the lien item.
+public enum BTPayPalLineItemUpcType: String {
+    
     ///  Upc Type A
-    case UPC-A
-
+    case UPC_A = "UPC-A"
+    
     /// Upc Type B
-    case UPC-B
-
+    case UPC_B = "UPC-B"
+    
     /// Upc Type C
-    case UPC-C
-
+    case UPC_C = "UPC-C"
+    
     /// Upc Type D
-    case UPC-D
-
+    case UPC_D = "UPC-D"
+    
     /// Upc Type E
-    case UPC-E
-
+    case UPC_E = "UPC-E"
+    
     /// Upc Type 2
-    case UPC-2
-
+    case UPC_2 = "UPC-2"
+    
     /// Upc Type 5
-    case UPC-5
+    case UPC_5 = "UPC-5"
+    
 }
+
 
 /// A PayPal line item to be displayed in the PayPal checkout flow.
 @objcMembers public class BTPayPalLineItem: NSObject {
@@ -55,21 +59,21 @@ import Foundation
 
     /// Optional: Item description. Maximum 127 characters.
     public let itemDescription: String? = nil
-
-    /// Optional: Product code for the item. Maximum 127 characters.
-    public let productCode: String? = nil
-
+    
     /// Optional: The URL to product information.
     public let url: URL? = nil
 
+    /// Optional: Product or UPC code for the item. Maximum 127 characters.
+    public let productCode: String? = nil
+    
     /// Optional: The URL to product image information.
-    public let imageUrl: URL? = nil
+    public var imageUrl: URL? = nil
 
-    /// Optional: UPC code for the item. 
-    public let upcCode: String? = nil
+        /// Optional: UPC code for the item.
+    public var upcCode: String? = nil
 
-    /// Optional: UPC type for the item.
-    public let upcType: BTPayPalLineItemUpcType? = nil
+        /// Optional: UPC type for the item.
+    public var upcType: BTPayPalLineItemUpcType? = nil
 
     // MARK: - Public Initializer
     
@@ -87,11 +91,11 @@ import Foundation
         self.kind = kind
     }
     
-    // MARK: - Internal Methods
+    // MARK: - Public Methods
 
     /// Returns the line item in a dictionary.
     /// - Returns: A dictionary with the line item information formatted for a request.
-    func requestParameters() -> [String: String] {
+    public func requestParameters() -> [String: String] {
         var requestParameters = [
             "quantity": quantity,
             "unit_amount": unitAmount,
@@ -118,15 +122,15 @@ import Foundation
         if let imageUrl, imageUrl != URL(string: "") {
             requestParameters["image_url"] = imageUrl.absoluteString
         }
-        
+
         if let upcCode, upcCode != "" {
             requestParameters["upc_code"] = upcCode
         }
         
-        if let upcType, upcType != "" {
+        if let upcType, upcType != BTPayPalLineItemUpcType(rawValue: "") {
             requestParameters["upc_type"] = upcType.rawValue
         }
-        
+                
         return requestParameters
     }
 }
