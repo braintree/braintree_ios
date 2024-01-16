fcb09d4ec2dc385e3850b0bf95498617673a4f89
scannillo <35243507+scannillo@users.noreply.github.com>
2024-01-16 10:15:40 -0600

    Update Sources/BraintreeCore/URL+IsPayPal.swift
    Co-authored-by: agedd <105314544+agedd@users.noreply.github.com>

--- Sources/BraintreeCore/URL+IsPayPal.swift
+++ Sources/BraintreeCore/URL+IsPayPal.swift
@@ -2,7 +2,7 @@ import Foundation
 
 extension URL {
     
-    /// Used to determine if a the URL is a paypal.com domain in order to format API requests accordingly
+    /// Used to determine if the URL is a paypal.com domain in order to format API requests accordingly
     var isPayPalURL: Bool {
         absoluteString.contains("api-m.paypal.com") || absoluteString.contains("api-m.sandbox.paypal.com")
     }
