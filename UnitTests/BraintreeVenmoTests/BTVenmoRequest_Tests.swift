6eaa68e9ccc4cf63bc58a299e07fc8abfd5671e2
Jax DesMarais-Leder <jdesmarais@paypal.com>
2024-02-14 13:04:49 -0600

    add BTVenmoRequest_Tests for isFinalAmount

--- UnitTests/BraintreeVenmoTests/BTVenmoRequest_Tests.swift
+++ UnitTests/BraintreeVenmoTests/BTVenmoRequest_Tests.swift
@@ -18,4 +18,25 @@ class BTVenmoRequest_Tests: XCTestCase {
         XCTAssertEqual(request.collectCustomerShippingAddress, false)
         XCTAssertEqual(request.collectCustomerBillingAddress, false)
     }
+
+    func testIsFinalAmount_whenIsFinalAmountSetAsTrue_returnsTrue() {
+        let request = BTVenmoRequest(paymentMethodUsage: .singleUse)
+        request.isFinalAmount = true
+        
+        XCTAssertEqual(request.isFinalAmount, true)
+
+    }
+
+    func testIsFinalAmount_whenIsFinalAmountSetAsFalse_returnsFalse() {
+        let request = BTVenmoRequest(paymentMethodUsage: .singleUse)
+        request.isFinalAmount = false
+
+        XCTAssertEqual(request.isFinalAmount, false)
+
+    }
+
+    func testIsFinalAmount_whenIsFinalAmountNotSet_defaultsToFalse() {
+        let request = BTVenmoRequest(paymentMethodUsage: .singleUse)
+        XCTAssertEqual(request.isFinalAmount, false)
+    }
 }
