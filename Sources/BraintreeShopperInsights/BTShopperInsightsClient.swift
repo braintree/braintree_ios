d57b773c54cd95bc3d73786baabe6059ee1cfc46
Victoria Park <vicpark@paypal.com>
2024-02-28 09:48:20 -0800

    DocStrings on Country Restrictions (#1197)
    * docStrings on country restrictions
    
    * fix typo in docStrings
    
    * Sammy PR feedback
    
    * pr feedback 2

--- Sources/BraintreeShopperInsights/BTShopperInsightsClient.swift
+++ Sources/BraintreeShopperInsights/BTShopperInsightsClient.swift
@@ -29,7 +29,9 @@ public class BTShopperInsightsClient {
     /// - Parameters:
     ///   - request: A `BTShopperInsightsRequest` containing the buyer's user information
     /// - Returns: A `BTShopperInsightsResult` instance
-    /// - Note: This feature is in beta. It's public API may change or be removed in future releases.
+    /// - Note: This feature is in beta. Its public API may change or be removed in future releases.
+    ///         PayPal recommendation is only available for US, AU, FR, DE, ITA, NED, ESP, Switzerland and UK merchants.
+    ///         Venmo recommendation is only available for US merchants.
     public func getRecommendedPaymentMethods(request: BTShopperInsightsRequest) async throws -> BTShopperInsightsResult {
         apiClient.sendAnalyticsEvent(BTShopperInsightsAnalytics.recommendedPaymentsStarted)
         
