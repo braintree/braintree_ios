9b7047760e3cea54d3d4d5d973d700be85269941
Jax DesMarais-Leder <jdesmarais@paypal.com>
2024-03-01 08:24:55 -0600

    Venmo Universal Links (#1193)
    * Add `BTVenmoRequest.fallbackToWeb`
        * If set to true customers will fallback to a web based Venmo flow if the Venmo app is not installed
    * This feature has been added to the Demo app, select "Venmo Universal Links" in the Venmo Integration to test this flow
    * Add Unit Tests

--- Sources/BraintreeVenmo/BTVenmoAppSwitchRedirectURL.swift
+++ Sources/BraintreeVenmo/BTVenmoAppSwitchRedirectURL.swift
@@ -6,47 +6,33 @@ import BraintreeCore
 
 struct BTVenmoAppSwitchRedirectURL {
 
-    let xCallbackTemplate: String = "scheme://x-callback-url/path"
-    let venmoScheme: String = "com.venmo.touch.v2"
+    // MARK: - Internal Properties
 
     /// The base app switch URL for Venmo. Does not include specific parameters.
-    var baseAppSwitchURL: URL? {
+    static var baseAppSwitchURL: URL? {
         appSwitchBaseURLComponents().url
     }
 
-    /// Create an app switch URL
-    /// - Parameters:
-    ///   - returnURLScheme:  The return URL scheme, e.g. "com.yourcompany.Your-App.payments"
-    ///   - merchantID: The merchant ID
-    ///   - accessToken: The access token used by the Venmo app to tokenize on behalf of the merchant
-    ///   - bundleDisplayName: The bundle display name for the current app
-    ///   - environment: The environment, e.g. "production" or "sandbox"
-    ///   - paymentContextID: The Venmo payment context ID (optional)
-    ///   - metadata: Additional Braintree metadata
-    ///   - Returns: The resulting URL, or `nil` if any of the required parameters are `nil`.
-    func appSwitch(
+    // MARK: - Private Properties
+
+    static private let xCallbackTemplate: String = "scheme://x-callback-url/path"
+    static private let venmoScheme: String = "com.venmo.touch.v2"
+
+    private var queryParameters: [String: Any?] = [:]
+
+    // MARK: - Initializer
+
+    init(
         returnURLScheme: String,
+        paymentContextID: String,
+        metadata: BTClientMetadata,
         forMerchantID merchantID: String?,
         accessToken: String?,
         bundleDisplayName: String?,
-        environment: String?,
-        paymentContextID: String?,
-        metadata: BTClientMetadata?
-    ) -> URL? {
-        let successReturnURL = returnURL(with: returnURLScheme, result: "success")
-        let errorReturnURL = returnURL(with: returnURLScheme, result: "error")
-        let cancelReturnURL = returnURL(with: returnURLScheme, result: "cancel")
-
-        guard let successReturnURL,
-                let errorReturnURL,
-                let cancelReturnURL,
-                let accessToken,
-                let metadata,
-                let bundleDisplayName,
-                let environment,
-                let merchantID
-        else {
-            return nil
+        environment: String?
+    ) throws {
+        guard let accessToken, let bundleDisplayName, let environment, let merchantID else {
+            throw BTVenmoError.invalidRedirectURLParameter
         }
 
         let venmoMetadata: [String: String] = [
@@ -60,36 +46,48 @@ struct BTVenmoAppSwitchRedirectURL {
         let serializedBraintreeData = try? JSONSerialization.data(withJSONObject: braintreeData)
         let base64EncodedBraintreeData = serializedBraintreeData?.base64EncodedString()
 
-        var appSwitchParameters: [String: Any] = [
-            "x-success": successReturnURL,
-            "x-error": errorReturnURL,
-            "x-cancel": cancelReturnURL,
+        queryParameters = [
+            "x-success": constructRedirectURL(with: returnURLScheme, result: "success"),
+            "x-error": constructRedirectURL(with: returnURLScheme, result: "error"),
+            "x-cancel": constructRedirectURL(with: returnURLScheme, result: "cancel"),
             "x-source": bundleDisplayName,
             "braintree_merchant_id": merchantID,
             "braintree_access_token": accessToken,
             "braintree_environment": environment,
-            "braintree_sdk_data": base64EncodedBraintreeData ?? ""
+            "resource_id": paymentContextID,
+            "braintree_sdk_data": base64EncodedBraintreeData ?? "",
+            "customerClient": "MOBILE_APP"
         ]
+    }
 
-        if let paymentContextID {
-            appSwitchParameters["resource_id"] = paymentContextID
-        }
+    // MARK: - Internal Methods
 
-        var components = appSwitchBaseURLComponents()
-        components.percentEncodedQuery = BTURLUtils.queryString(from: appSwitchParameters as NSDictionary)
+    func universalLinksURL() -> URL? {
+        let universalLinkURL = URL(string: "https://venmo.com/go/checkout")!
+
+        var urlComponent = URLComponents(url: universalLinkURL, resolvingAgainstBaseURL: false)!
+        urlComponent.percentEncodedQuery = BTURLUtils.queryString(from: queryParameters as NSDictionary)
+
+        return urlComponent.url
+    }
+
+    func urlSchemeURL() -> URL? {
+        var components = BTVenmoAppSwitchRedirectURL.appSwitchBaseURLComponents()
+        components.percentEncodedQuery = BTURLUtils.queryString(from: queryParameters as NSDictionary)
+        
         return components.url
     }
 
-    // MARK: - Internal Helper Methods
+    // MARK: - Private Helper Methods
 
-    func returnURL(with scheme: String, result: String) -> URL? {
-        var components = URLComponents(string: xCallbackTemplate)
+    private func constructRedirectURL(with scheme: String, result: String) -> URL? {
+        var components = URLComponents(string: BTVenmoAppSwitchRedirectURL.xCallbackTemplate)
         components?.scheme = scheme
         components?.percentEncodedPath = "/vzero/auth/venmo/\(result)"
         return components?.url
     }
 
-    func appSwitchBaseURLComponents() -> URLComponents {
+    private static func appSwitchBaseURLComponents() -> URLComponents {
         var components: URLComponents = URLComponents(string: xCallbackTemplate) ?? URLComponents()
         components.scheme = venmoScheme
         components.percentEncodedPath = "/vzero/auth"
