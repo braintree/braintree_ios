2379c90074233ff6bc5fb3695cea7cc41a227dcd
Sammy Cannillo <scannillo@paypal.com>
2024-03-07 15:01:36 -0600

    Add NSLog in case of error sending FPTI API request

--- Sources/BraintreeCore/Analytics/BTAnalyticsService.swift
+++ Sources/BraintreeCore/Analytics/BTAnalyticsService.swift
@@ -84,7 +84,11 @@ class BTAnalyticsService: Equatable {
             }
 
             let postParameters = self.createAnalyticsEvent(config: configuration, sessionID: self.apiClient.metadata.sessionID, event: event)
-            self.http?.post("v1/tracking/batch/events", parameters: postParameters, completion: { _,_,_ in })
+            self.http?.post("v1/tracking/batch/events", parameters: postParameters, completion: { _,_, error in
+                if let error {
+                    NSLog("[Braintree SDK] Failed to send analytics: \(error.localizedDescription)")
+                }
+            })
         }
     }
 
