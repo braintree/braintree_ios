f810e19875d2950f937411216a1da36f165041de
Jax DesMarais-Leder <jdesmarais@paypal.com>
2024-03-18 15:36:17 -0500

    add new enablePayPalAppSwitch property; create new BTPayPalVaultBaseRequest to allow only PayPal module to use this param

--- Sources/BraintreePayPal/BTPayPalVaultRequest.swift
+++ Sources/BraintreePayPal/BTPayPalVaultRequest.swift
@@ -5,7 +5,7 @@ import BraintreeCore
 #endif
 
 ///  Options for the PayPal Vault flow.
-@objcMembers open class BTPayPalVaultRequest: BTPayPalRequest {
+@objcMembers open class BTPayPalVaultBaseRequest: BTPayPalRequest {
 
     // MARK: - Public Properties
 
@@ -15,7 +15,9 @@ import BraintreeCore
     // MARK: - Initializer
 
     /// Initializes a PayPal Native Vault request
-    /// - Parameter offerCredit: Optional: Offers PayPal Credit if the customer qualifies. Defaults to `false`.
+    /// - Parameters:
+    ///   - offerCredit: Optional: Offers PayPal Credit if the customer qualifies. Defaults to `false`.
+    ///   - enablePayPalAppSwitch: Optional: Used to determine if the customer will use the PayPal app switch flow. Defaults to `false`.
     public init(offerCredit: Bool = false) {
         self.offerCredit = offerCredit
 
@@ -49,5 +51,28 @@ import BraintreeCore
         }
 
         return baseParameters.merging(vaultParameters) { $1 }
+    }
+
+}
+
+///  Options for the PayPal Vault flow.
+@objcMembers public class BTPayPalVaultRequest: BTPayPalVaultBaseRequest {
+
+    // MARK: - Public Properties
+
+    /// Optional: Used to determine if the customer will use the PayPal app switch flow.
+    /// Defaults to `false`.
+    /// - Note: This property is currently available in limited release
+    public var enablePayPalAppSwitch: Bool
+
+    // MARK: - Initializer
+
+    /// Initializes a PayPal Native Vault request
+    /// - Parameters:
+    ///   - offerCredit: Optional: Offers PayPal Credit if the customer qualifies. Defaults to `false`.
+    ///   - enablePayPalAppSwitch: Optional: Used to determine if the customer will use the PayPal app switch flow. Defaults to `false`.
+    public init(offerCredit: Bool = false, enablePayPalAppSwitch: Bool = false) {
+        self.enablePayPalAppSwitch = enablePayPalAppSwitch
+        super.init(offerCredit: offerCredit)
     }
 }
