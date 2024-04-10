ff3ecaac055e89169af1b2db6844240c0f68a7e8
agedd <105314544+agedd@users.noreply.github.com>
2024-04-10 14:43:24 -0500

    Update to Cardinal 2.2.5-9 (#1264)
    * pull in latest cardinal changes
    
    * pull in latest cardinal build with code signature and privacy manifest changes
    
    * add CHANGELOG entry
    
    * update cart file with updated cardinal version
    
    * address pr comments
    
    * fix typo in CHANGELOG.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,8 @@
 ## unreleased
 * Require Xcode 15.0+ and Swift 5.9+ (per [App Store requirements](https://developer.apple.com/news/?id=khzvxn8a))
 * Updated expiring pinned vendor SSL certificates
+* BraintreeThreeDSecure
+  * Bump CardinalMobile SDK to version 2.2.5-9 with code signing and a privacy manifest file.
 * BraintreeDataCollector
   * Bump to PPRiskMagnes version 5.5.0 with fix for Xcode 15.3 Swift Pacakge Manager validation bug (fixes #1229))
 
--- Frameworks/XCFrameworks/CardinalMobile.xcframework/ios-arm64_x86_64-simulator/CardinalMobile.framework/Headers/ChallengeStatusReceiver.h
+++ Frameworks/XCFrameworks/CardinalMobile.xcframework/ios-arm64_x86_64-simulator/CardinalMobile.framework/Headers/ChallengeStatusReceiver.h
@@ -6,9 +6,9 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "CompletionEvent.h"
-#import "RuntimeErrorEvent.h"
-#import "ProtocolErrorEvent.h"
+#import <CardinalMobile/CompletionEvent.h>
+#import <CardinalMobile/RuntimeErrorEvent.h>
+#import <CardinalMobile/ProtocolErrorEvent.h>
 
 /**
  * A callback object that confrotnts to ChallengeStatusReceiver protocol
@@ -23,30 +23,30 @@
  * When a transaction is completed, a transaction status shall be available.
  * @param completionEvent Information about completion of the challenge process.
  */
-- (void) completed: (CompletionEvent *) completionEvent;
+- (void) challengeStatusReceiverCompleted: (CompletionEvent *) completionEvent;
 
 
 /**
  * Called when the Cardholder selects the option to cancel the transaction on the challenge screen.
  */
-- (void) cancelled;
+- (void) challengeStatusReceiverCancelled;
 
 /**
  * Called when the challenge process reaches or exceeds the timeout interval that is specified during the doChallenge call on the 3DS SDK.
  */
-- (void) timedout;
+- (void) challengeStatusReceiverTimedout;
 
 /**
  * Called when the 3DS SDK receives an EMV 3-D Secure protocol-defined error message from the ACS.
  * @param protocolErrorEvent Error code and details.
  */
-- (void) protocolError: (ProtocolErrorEvent *) protocolErrorEvent;
+- (void) challengeStatusReceiverProtocolError: (ProtocolErrorEvent *) protocolErrorEvent;
 
 /**
  * Called when the 3DS SDK encounters errors during the challenge process
  * These errors include all errors except those covered by the protocolError method.
  * @param runtimeErrorEvent Error code and details.
  */
-- (void) runtimeError: (RuntimeErrorEvent *) runtimeErrorEvent;
+- (void) challengeStatusReceiverRuntimeError: (RuntimeErrorEvent *) runtimeErrorEvent;
 
 @end
