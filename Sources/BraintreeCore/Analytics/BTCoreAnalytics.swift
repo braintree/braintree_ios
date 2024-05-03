01dda528e88b8532813b735574e478659618f2f8
Jax DesMarais-Leder <jdesmarais@paypal.com>
2024-05-03 10:47:17 -0500

    make BTCoreAnalytics internal

--- Sources/BraintreeCore/Analytics/BTCoreAnalytics.swift
+++ Sources/BraintreeCore/Analytics/BTCoreAnalytics.swift
@@ -1,8 +1,6 @@
 import Foundation
 
-/// :nodoc: This enum is exposed for internal Braintree use only. Do not use. It is not covered by Semantic Versioning and may change or be removed at any time.
-@_documentation(visibility: private)
-public enum BTCoreAnalytics {
+enum BTCoreAnalytics {
 
     static let apiRequestLatency = "core:api-request-latency"
 }
