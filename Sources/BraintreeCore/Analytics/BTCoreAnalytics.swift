037d0865ac9408426a8c98c97e15adc74e49bc23
Jax DesMarais-Leder <jdesmarais@paypal.com>
2024-05-03 09:33:47 -0500

    add BTCoreAnalytics enum

---
+++ Sources/BraintreeCore/Analytics/BTCoreAnalytics.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+/// :nodoc: This enum is exposed for internal Braintree use only. Do not use. It is not covered by Semantic Versioning and may change or be removed at any time.
+@_documentation(visibility: private)
+public enum BTCoreAnalytics {
+
+    static let apiRequestLatency = "core:api-request-latency"
+}
