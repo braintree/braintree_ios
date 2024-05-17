c55987fa988af1633a657cbcebaf3bbacad8be29
Sammy Cannillo <scannillo@paypal.com>
2024-05-16 19:16:49 -0500

    Cleanup - add docstrings to ClientAuthorization protocol

--- Sources/BraintreeCore/Authorization/ClientAuthorization.swift
+++ Sources/BraintreeCore/Authorization/ClientAuthorization.swift
@@ -1,15 +1,25 @@
 import UIKit
 
+/// :nodoc: Form of authorization to interact with the Braintree gateway.
+@_documentation(visibility: private)
 public protocol ClientAuthorization {
     
     var type: AuthorizationType { get }
+    
+    /// The URL
     var configURL: URL { get }
+    
+    /// The bearer token to use for authenticating API calls. 
+    /// The authorization fingerprint if a client token, or the full tokenization key if a tokenization key.
     var bearer: String { get }
     
-    /// The original Client token or Tokenization Key string
+    /// The original, full string value of the authorization string provided by the merchant. 
+    /// The full client token or tokenization key.
     var originalValue: String { get } // TODO: rawValue re-name
 }
 
+/// :nodoc:
+@_documentation(visibility: private)
 public enum AuthorizationType {
     case tokenizationKey
     case clientToken
