0075555dda7500eb38faa42e4c7c117732c927f9
Sammy Cannillo <scannillo@paypal.com>
2024-05-17 14:24:24 -0500

    Fixup - undo removal of dictionary extension used in unit test

--- UnitTests/BraintreeCoreTests/BTHTTP_Tests.swift
+++ UnitTests/BraintreeCoreTests/BTHTTP_Tests.swift
@@ -865,3 +865,9 @@ final class BTHTTP_Tests: XCTestCase {
         completion()
     }
 }
+
+extension [String: Any] {
+    static func ==(lhs: [String: Any], rhs: [String: Any] ) -> Bool {
+        return NSDictionary(dictionary: lhs).isEqual(to: rhs)
+    }
+}
