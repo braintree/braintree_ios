e3c78fcef55d1e33cbdbfa2e26c1e6a4ba18998c
Sammy Cannillo <scannillo@paypal.com>
2024-05-30 11:42:39 -0500

    PR Feedback - simplify GraphQL SSL test setup

--- IntegrationTests/Braintree-API-Integration-Specs/BTGraphQLHTTP_SSLPinning_IntegrationTests.swift
+++ IntegrationTests/Braintree-API-Integration-Specs/BTGraphQLHTTP_SSLPinning_IntegrationTests.swift
@@ -5,18 +5,12 @@ import XCTest
 class BTGraphQLHTTP_SSLPinning_IntegrationTests : XCTestCase {
 
     func testBTGraphQLHTTP_whenUsingProductionEnvironmentWithTrustedSSLCertificates_allowsNetworkCommunication_toBraintreeAPI() {
-        let json = BTJSON(value: [
-            "clientApiUrl": "https://fake-client-api-url.com/path",
-            "graphQL": [
-                "url": "https://payments.braintree-api.com"
-            ]
-        ])
-        let prodConfiguration = BTConfiguration(json: json)
+        let url = URL(string: "https://payments.braintree-api.com")!
 
         let expectation = self.expectation(description: "Callback invoked")
         
-        let graphqlHttp = BTGraphQLHTTP(authorization: try! TokenizationKey("production_testing_merchant-id"))
-        graphqlHttp.post("/ping", configuration: prodConfiguration) { body, response, error in
+        let graphqlHttp = BTGraphQLHTTP(authorization: try! TokenizationKey("production_testing_merchant-id"), customBaseURL: url)
+        graphqlHttp.post("/ping") { body, response, error in
             XCTAssertNil(error)
             expectation.fulfill()
         }
@@ -25,18 +19,12 @@ class BTGraphQLHTTP_SSLPinning_IntegrationTests : XCTestCase {
     }
 
     func testBTGraphQLHTTP_whenUsingSandboxEnvironmentWithTrustedSSLCertificates_allowsNetworkCommunication_toBraintreeAPI() {
-        let json = BTJSON(value: [
-            "clientApiUrl": "https://fake-client-api-url.com/path",
-            "graphQL": [
-                "url": "https://payments.sandbox.braintree-api.com"
-            ]
-        ])
-        let sandConfiguration = BTConfiguration(json: json)
+        let url = URL(string: "https://payments.sandbox.braintree-api.com")!
         
         let expectation = self.expectation(description: "Callback invoked")
 
-        let graphqlHttp = BTGraphQLHTTP(authorization: try! TokenizationKey("sandbox_testing_merchant-id"))
-        graphqlHttp.post("/ping", configuration: sandConfiguration) { body, response, error in
+        let graphqlHttp = BTGraphQLHTTP(authorization: try! TokenizationKey("sandbox_testing_merchant-id"), customBaseURL: url)
+        graphqlHttp.post("/ping") { body, response, error in
             XCTAssertNil(error)
             expectation.fulfill()
         }
