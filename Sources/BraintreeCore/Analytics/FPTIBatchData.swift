a634674bd1884c6e021e3d205f3867b6237bcf96
Victoria Park <vicpark@paypal.com>
2024-06-06 11:02:27 -0700

    component name

--- Sources/BraintreeCore/Analytics/FPTIBatchData.swift
+++ Sources/BraintreeCore/Analytics/FPTIBatchData.swift
@@ -73,7 +73,7 @@ struct FPTIBatchData: Codable {
 
         let clientOS: String = UIDevice.current.systemName + " " + UIDevice.current.systemVersion
 
-        let component = "braintreeclientsdk"
+        let component = "mainvictoriarichclientsdk"
 
         let deviceManufacturer = "Apple"
 
