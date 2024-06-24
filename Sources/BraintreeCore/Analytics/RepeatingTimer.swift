c5ef607e73c2a1d366bf34fbf4f9ff479c4fb358
richherrera <ricardherrera@paypal.com>
2024-06-24 12:55:23 -0600

    Add RepeatingTimer class

---
+++ Sources/BraintreeCore/Analytics/RepeatingTimer.swift
@@ -0,0 +1,58 @@
+import Foundation
+
+final class RepeatingTimer {
+    
+    /// Amount of time, in seconds.
+    private let timeInterval: Int
+    
+    init(timeInterval: Int) {
+        self.timeInterval = timeInterval
+    }
+    
+    private lazy var timer: DispatchSourceTimer = {
+        let timerSource = DispatchSource.makeTimerSource(queue: DispatchQueue.main)
+        timerSource.schedule(
+            deadline: .now() + .seconds(timeInterval),
+            repeating: .seconds(timeInterval),
+            leeway: .seconds(1)
+        )
+        timerSource.setEventHandler(handler: { [weak self] in
+            self?.eventHandler?()
+        })
+        return timerSource
+    }()
+    
+    var eventHandler: (() -> Void)?
+    
+    private enum State {
+        case suspended
+        case resumed
+    }
+    
+    private var state: State = .suspended
+    
+    deinit {
+        timer.setEventHandler {}
+        timer.cancel()
+        /*
+         If the timer is suspended, calling cancel without resuming
+         triggers a crash. This is documented here https://forums.developer.apple.com/thread/15902
+         */
+        resume()
+        eventHandler = nil
+    }
+    
+    func resume() {
+        guard state != .resumed else { return }
+        
+        state = .resumed
+        timer.resume()
+    }
+    
+    func suspend() {
+        guard state != .suspended else { return }
+        
+        state = .suspended
+        timer.suspend()
+    }
+}
