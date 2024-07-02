13c929c928c94101ebfe035266650143766504d2
richherrera <ricardherrera@paypal.com>
2024-07-02 12:27:50 -0600

    Add ConfigurationLoader logic

--- Sources/BraintreeCore/ConfigurationLoader.swift
+++ Sources/BraintreeCore/ConfigurationLoader.swift
@@ -1,1 +1,70 @@
+import Foundation
 
+final class ConfigurationLoader {
+    
+    private let configPath = "v1/configuration"
+    private let configurationCache: ConfigurationCache
+    private let http: BTHTTP
+    
+    init(http: BTHTTP, configurationCache: ConfigurationCache = ConfigurationCache.shared) {
+        self.http = http
+        self.configurationCache = configurationCache
+    }
+    
+    deinit {
+        http.session.finishTasksAndInvalidate()
+    }
+    
+    func getConfig(_ authorization: ClientAuthorization, completion: @escaping (BTConfiguration?, Error?) -> Void) {
+        // Fetches or returns the configuration and caches the response in the GET BTHTTP call if successful
+        //
+        // Rules:
+        //   - If cachedConfiguration is present, return it without a request
+        //   - If cachedConfiguration is not present, fetch it and cache the successful response
+        //   - If fetching fails, return error
+        
+        if let cachedConfig = try? configurationCache.getFromCache(authorization: authorization.bearer) {
+            completion(cachedConfig, nil)
+            return
+        }
+
+        http.get(configPath, parameters: BTConfigurationRequest()) { [weak self] body, response, error in
+            guard let self else {
+                completion(nil, BTAPIClientError.deallocated)
+                return
+            }
+
+            if let error {
+                completion(nil, error)
+                return
+            } else if response?.statusCode != 200 || body == nil {
+                completion(nil, BTAPIClientError.configurationUnavailable)
+                return
+            } else {
+                guard let body else {
+                    completion(nil, BTAPIClientError.configurationUnavailable)
+                    return
+                }
+                
+                let configuration = BTConfiguration(json: body)
+
+                try? configurationCache.putInCache(authorization: authorization.bearer, configuration: configuration)
+                
+                completion(configuration, nil)
+                return
+            }
+        }
+    }
+    
+    func getConfig(_ authorization: ClientAuthorization) async throws -> BTConfiguration {
+        try await withCheckedThrowingContinuation { continuation in
+            getConfig(authorization) { configuration, error in
+                if let error {
+                    continuation.resume(throwing: error)
+                } else if let configuration {
+                    continuation.resume(returning: configuration)
+                }
+            }
+        }
+    }
+}
