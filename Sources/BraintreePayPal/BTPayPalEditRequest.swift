7be938a789751128b71a243076cbe680c7367398
Victoria Park <vicpark@paypal.com>
2024-07-08 12:33:52 -0700

    Add BTPayPalEditRequest

---
+++ Sources/BraintreePayPal/BTPayPalEditRequest.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+#if canImport(BraintreeCore)
+import BraintreeCore
+#endif
+
+/// Options for the PayPal Edit FI flow
+/// - Warning: This feature is currently in beta and may change or be removed in future releases.
+@objcMembers open class BTPayPalEditRequest {
+    public let token: String
+
+    /// Initializes a PayPal Edit Request for the Edit FI flow
+    /// - Parameters:
+    ///   - token: Required: Used to initiate tokenize call to edit funding instrument in customer's PayPal account.
+    public init(token: String) {
+        self.token = token
+    }
+}
