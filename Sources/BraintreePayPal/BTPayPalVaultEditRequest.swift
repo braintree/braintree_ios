be06e4a287dfb1e1c124ef246e1146c899fe05b0
Victoria Park <vicpark@paypal.com>
2024-08-07 09:51:18 -0700

    Add Optional merchantAccountID to PayPalVaultEditRequest (#1383)
    * add optional merchantAccountID to PayPalVaultEditRequest
    
    * jax PR feedback
    
    * PR feedback: change request to struct, docString typo

--- Sources/BraintreePayPal/BTPayPalVaultEditRequest.swift
+++ Sources/BraintreePayPal/BTPayPalVaultEditRequest.swift
@@ -2,16 +2,19 @@ import Foundation
 
 /// Options for the PayPal edit funding instrument flow
 /// - Warning: This feature is currently in beta and may change or be removed in future releases.
-public class BTPayPalVaultEditRequest {
+public struct BTPayPalVaultEditRequest {
 
     private let editPayPalVaultID: String
+    private let merchantAccountID: String?
 
     //   TODO: specify endpoint for merchant to retrieve the token
     /// Initializes a PayPal Edit Request for the edit funding instrument flow
     /// - Parameters:
     ///   - editPayPalVaultID: Required: The `edit_paypal_vault_id` returned from the server side request
+    ///   merchantAccountID: optional ID of the merchant account; if one is not provided the default will be used
     /// - Warning: This feature is currently in beta and may change or be removed in future releases.
-    public init(editPayPalVaultID: String) {
+    public init(editPayPalVaultID: String, merchantAccountID: String? = nil) {
         self.editPayPalVaultID = editPayPalVaultID
+        self.merchantAccountID = merchantAccountID
     }
 }
