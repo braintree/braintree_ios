eee00753c0d1c97f3f25c523480871bfba007999
Victoria Park <vicpark@paypal.com>
2024-08-07 08:43:42 -0700

    PR feedback: change request to struct, docString typo

--- Sources/BraintreePayPal/BTPayPalVaultEditRequest.swift
+++ Sources/BraintreePayPal/BTPayPalVaultEditRequest.swift
@@ -2,7 +2,7 @@ import Foundation
 
 /// Options for the PayPal edit funding instrument flow
 /// - Warning: This feature is currently in beta and may change or be removed in future releases.
-public class BTPayPalVaultEditRequest {
+public struct BTPayPalVaultEditRequest {
 
     private let editPayPalVaultID: String
     private let merchantAccountID: String?
@@ -11,7 +11,7 @@ public class BTPayPalVaultEditRequest {
     /// Initializes a PayPal Edit Request for the edit funding instrument flow
     /// - Parameters:
     ///   - editPayPalVaultID: Required: The `edit_paypal_vault_id` returned from the server side request
-    ///   merchantAccountIdD: optional ID of the merchant account; if one is not provided the default will be used
+    ///   merchantAccountID: optional ID of the merchant account; if one is not provided the default will be used
     /// - Warning: This feature is currently in beta and may change or be removed in future releases.
     public init(editPayPalVaultID: String, merchantAccountID: String? = nil) {
         self.editPayPalVaultID = editPayPalVaultID
