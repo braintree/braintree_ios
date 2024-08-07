f8e3b40610116df418fc4e6890d304113f522e3b
Victoria Park <vicpark@paypal.com>
2024-08-07 09:10:03 -0700

    Update BTPayPalVaultEditResult Object

--- Sources/BraintreePayPal/BTPayPalVaultEditResult.swift
+++ Sources/BraintreePayPal/BTPayPalVaultEditResult.swift
@@ -1,8 +1,33 @@
 import Foundation
+import BraintreeCore
 
 /// A result of the Edit FI flow used to display a customers updated payment details in your UI
 /// - Warning: This feature is currently in beta and may change or be removed in future releases.
-public class BTPayPalVaultEditResult {
+public struct BTPayPalVaultEditResult {
 
-    // TODO: implement in a future PR
+    // MARK: - Public Properties
+
+    /// This ID is used to link subsequent retry attempts if payment is declined
+    public let clientMetadataID: String
+
+    /// ID of the payer
+    public let payerID: String?
+
+    /// email address of the payer
+    public let email: String?
+
+    /// first name of the payer
+    public let firstName: String?
+
+    /// last name of the payer
+    public let lastName: String?
+
+    /// phone number of the payer
+    public let phone: String?
+
+    /// shipping address of the payer
+    public let shippingAddress: BTPostalAddress?
+
+    /// description of the funding source
+    public let fundingSourceDescription: String?
 }
