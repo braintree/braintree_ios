b9635032f48f97a5560d0c840ed4b5d646d1becd
Victoria Park <vicpark@paypal.com>
2024-08-26 09:24:26 -0700

    Add riskCorrelationId to EditRequest

--- Sources/BraintreePayPal/BTPayPalVaultEditRequest.swift
+++ Sources/BraintreePayPal/BTPayPalVaultEditRequest.swift
@@ -6,6 +6,7 @@ public struct BTPayPalVaultEditRequest {
 
     private let editPayPalVaultID: String
     private let merchantAccountID: String?
+    private let riskCorrelationID: String?
 
     //   TODO: specify endpoint for merchant to retrieve the token
     /// Initializes a PayPal Edit Request for the edit funding instrument flow
@@ -13,8 +14,9 @@ public struct BTPayPalVaultEditRequest {
     ///   - editPayPalVaultID: Required: The `edit_paypal_vault_id` returned from the server side request
     ///   merchantAccountID: optional ID of the merchant account; if one is not provided the default will be used
     /// - Warning: This feature is currently in beta and may change or be removed in future releases.
-    public init(editPayPalVaultID: String, merchantAccountID: String? = nil) {
+    public init(editPayPalVaultID: String, merchantAccountID: String? = nil, riskCorrelationID: String? = nil) {
         self.editPayPalVaultID = editPayPalVaultID
         self.merchantAccountID = merchantAccountID
+        self.riskCorrelationID = riskCorrelationID
     }
 }
