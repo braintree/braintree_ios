e2061dd6f94ba3d466dcc233688757f08f788654
Victoria Park <vicpark@paypal.com>
2024-09-03 09:28:09 -0700

    Update BTPayPalVaultEditREsult to return only riskCorrelationID

--- Sources/BraintreePayPal/BTPayPalVaultEditResult.swift
+++ Sources/BraintreePayPal/BTPayPalVaultEditResult.swift
@@ -11,26 +11,5 @@ public struct BTPayPalVaultEditResult {
     // MARK: - Public Properties
 
     /// This ID is used to link subsequent retry attempts if payment is declined
-    public let clientMetadataID: String
-
-    /// ID of the payer
-    public let payerID: String?
-
-    /// email address of the payer
-    public let email: String?
-
-    /// first name of the payer
-    public let firstName: String?
-
-    /// last name of the payer
-    public let lastName: String?
-
-    /// phone number of the payer
-    public let phone: String?
-
-    /// shipping address of the payer
-    public let shippingAddress: BTPostalAddress?
-
-    /// description of the funding source
-    public let fundingSourceDescription: String?
+    public let riskCorrelationID: String
 }
