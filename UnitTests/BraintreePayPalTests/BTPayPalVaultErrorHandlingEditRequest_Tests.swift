bd669e91bc804051ec244e69152b8731ddf5376f
Victoria Park <vicpark@paypal.com>
2024-09-06 12:42:07 -0700

    Add test for BTPayPalVaultErrorHandlingEditRequest

--- UnitTests/BraintreePayPalTests/BTPayPalVaultErrorHandlingEditRequest_Tests.swift
+++ UnitTests/BraintreePayPalTests/BTPayPalVaultErrorHandlingEditRequest_Tests.swift
@@ -2,7 +2,7 @@ import XCTest
 @testable import BraintreeCore
 @testable import BraintreePayPal
 
-class BTPayPalEditRequest_Tests: XCTestCase {
+class BTPayPalVaultErrorHandlingEditRequest_Tests: XCTestCase {
 
     override func setUp() {
         super.setUp()
@@ -10,15 +10,15 @@ class BTPayPalEditRequest_Tests: XCTestCase {
 
     func test_returnsAllParams() {
         let expectedEditPayPalVaultID = "fake-edit-paypal-vault-id"
-        let expectedCorrelationID = "test-ID"
-        var editRequest = BTPayPalVaultEditRequest(editPayPalVaultID: expectedEditPayPalVaultID)
-        editRequest.riskCorrelationID = expectedCorrelationID
+        let expectedRiskCorrelationID = "test-errorHandling-1"
+        let editRequest = BTPayPalVaultErrorHandlingEditRequest(editPayPalVaultID: expectedEditPayPalVaultID, riskCorrelationID: expectedRiskCorrelationID)
 
         let parameters = editRequest.parameters()
 
         XCTAssertEqual(parameters["edit_paypal_vault_id"] as? String, expectedEditPayPalVaultID)
         XCTAssertEqual(parameters["return_url"] as? String, "sdk.ios.braintree://onetouch/v1/success")
         XCTAssertEqual(parameters["cancel_url"] as? String, "sdk.ios.braintree://onetouch/v1/cancel")
-        XCTAssertEqual(parameters["risk_correlation_id"] as? String, expectedCorrelationID)
+        XCTAssertEqual(parameters["cancel_url"] as? String, "sdk.ios.braintree://onetouch/v1/cancel")
+        XCTAssertEqual(parameters["risk_correlation_id"] as? String, expectedRiskCorrelationID)
     }
 }
