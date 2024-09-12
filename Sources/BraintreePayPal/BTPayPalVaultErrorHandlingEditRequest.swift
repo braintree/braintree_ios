9cacac8095d3a0069b59923fbcd60ff88348a525
Jax DesMarais-Leder <jdesmarais@paypal.com>
2024-09-12 10:16:26 -0500

    update docstrings

--- Sources/BraintreePayPal/BTPayPalVaultErrorHandlingEditRequest.swift
+++ Sources/BraintreePayPal/BTPayPalVaultErrorHandlingEditRequest.swift
@@ -11,11 +11,11 @@ public class BTPayPalVaultErrorHandlingEditRequest: BTPayPalVaultEditRequest {
 
     let riskCorrelationID: String
 
-    /// Initializes a PayPal Edit Request for the edit funding instrument flow
+    /// Initializes a PayPal Edit Request for the edit funding instrument flow with retry on failed attempts
     /// - Parameters:
     ///   - editPayPalVaultID: Required: The `edit_paypal_vault_id` returned from the server side requests,
     ///   `gateway.payment_method.find("payment_method_token")` or `gateway.customer.find("customer_id")`
-    ///   - riskCorrelationID: Required: Unique id for each transaction used in subsequent retry in case of failure
+    ///   - riskCorrelationID: Required: Unique ID for each transaction used in subsequent retry in case of failure
     /// - Warning: This feature is currently in beta and may change or be removed in future releases.
     public init(editPayPalVaultID: String, riskCorrelationID: String) {
         self.riskCorrelationID = riskCorrelationID
