d7a515a1481c0a280f06819b0d0dfcea93fa34d5
Jax DesMarais-Leder <jdesmarais@paypal.com>
2024-09-12 10:01:21 -0500

    remove old docstrings

--- Sources/BraintreePayPal/BTPayPalVaultEditRequest.swift
+++ Sources/BraintreePayPal/BTPayPalVaultEditRequest.swift
@@ -18,7 +18,6 @@ public class BTPayPalVaultEditRequest {
     /// - Parameters:
     ///   - editPayPalVaultID: Required: The `edit_paypal_vault_id` returned from the server side requests,
     ///   `gateway.payment_method.find("payment_method_token")` or `gateway.customer.find("customer_id")`
-    ///   - merchantAccountID: Optional: ID of the merchant account; if one is not provided the default will be used
     /// - Warning: This feature is currently in beta and may change or be removed in future releases.
     public init(editPayPalVaultID: String) {
         self.editPayPalVaultID = editPayPalVaultID
--- Sources/BraintreePayPal/BTPayPalVaultErrorHandlingEditRequest.swift
+++ Sources/BraintreePayPal/BTPayPalVaultErrorHandlingEditRequest.swift
@@ -16,7 +16,6 @@ public class BTPayPalVaultErrorHandlingEditRequest: BTPayPalVaultEditRequest {
     ///   - editPayPalVaultID: Required: The `edit_paypal_vault_id` returned from the server side requests,
     ///   `gateway.payment_method.find("payment_method_token")` or `gateway.customer.find("customer_id")`
     ///   - riskCorrelationID: Required: Unique id for each transaction used in subsequent retry in case of failure
-    ///   - merchantAccountID: Optional: ID of the merchant account; if one is not provided the default will be used
     /// - Warning: This feature is currently in beta and may change or be removed in future releases.
     public init(editPayPalVaultID: String, riskCorrelationID: String) {
         self.riskCorrelationID = riskCorrelationID
