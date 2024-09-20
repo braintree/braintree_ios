9125473f90701a7c28aef9140eaf7dffbf6a574b
Jax DesMarais-Leder <jdesmarais@paypal.com>
2024-09-20 11:16:28 -0500

    remove accidently deleted 3ds ui test method; rename file to be UI specific

--- Demo/UI Tests/Helpers/DateGenerator.swift
+++ Demo/UI Tests/Helpers/DateGenerator.swift
@@ -1,8 +1,14 @@
 import Foundation
 
-class DateGenerator {
-    
-    static let sharedInstance = DateGenerator()
+class UITestDateGenerator {
+
+    static let sharedInstance = UITestDateGenerator()
+
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/yy"
+        return formatter
+    }()
 
     let calendar = Calendar.init(identifier: .gregorian)
 
@@ -11,5 +17,10 @@ class DateGenerator {
     func threeDSecure2TestingDate() -> String {
         let year = calendar.component(.year, from: Date()) + 3
         return "01/\(year)"
+    }
+
+    func futureDate() -> String {
+        let futureDate = calendar.date(byAdding: .year, value: 3, to: Date())!
+        return dateFormatter.string(from: futureDate)
     }
 }
