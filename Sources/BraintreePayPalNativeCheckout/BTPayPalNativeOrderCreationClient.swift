8e492fc108ad079bc243852aa31e7abdf8e1e7bb
Victoria Park <vicpark@paypal.com>
2024-09-26 10:34:49 -0700

    update BTPayPalNativeOrderCreationClient to use async fetchConfiguration

--- Sources/BraintreePayPalNativeCheckout/BTPayPalNativeOrderCreationClient.swift
+++ Sources/BraintreePayPalNativeCheckout/BTPayPalNativeOrderCreationClient.swift
@@ -30,54 +30,55 @@ class BTPayPalNativeOrderCreationClient {
         with request: BTPayPalRequest,
         completion: @escaping (Result<BTPayPalNativeOrder, BTPayPalNativeCheckoutError>) -> Void
     ) {
-        apiClient.fetchOrReturnRemoteConfiguration { configuration, error in
-            guard let config = configuration, error == nil else {
-                completion(.failure(.fetchConfigurationFailed))
-                return
-            }
+        Task { @MainActor in
+            do {
+                let config = try await apiClient.fetchConfiguration()
 
-            guard let paypalEnabled = config.json?["paypalEnabled"].isTrue, paypalEnabled else {
-                completion(.failure(.payPalNotEnabled))
-                return
-            }
-
-            guard let payPalClientID = config.json?["paypal"]["clientId"].asString() else {
-                completion(.failure(.payPalClientIDNotFound))
-                return
-            }
-
-            let payPalEnvironment: PayPalCheckout.Environment?
-            if config.environment == "production" {
-                payPalEnvironment = .live
-            } else if config.environment == "sandbox" {
-                payPalEnvironment = .sandbox
-            } else {
-                payPalEnvironment = nil
-            }
-
-            guard let environment = payPalEnvironment else {
-                completion(.failure(.invalidEnvironment))
-                return
-            }
-
-            self.apiClient.post(
-                request.hermesPath,
-                parameters: request.parameters(with: config)
-            ) { json, _, error in
-                guard let hermesResponse = BTPayPalNativeHermesResponse(json: json), error == nil else {
-                    let underlyingError = error ?? BTPayPalNativeCheckoutError.invalidJSONResponse
-                    completion(.failure(.orderCreationFailed(underlyingError)))
+                guard let paypalEnabled = config.json?["paypalEnabled"].isTrue, paypalEnabled else {
+                    completion(.failure(.payPalNotEnabled))
                     return
                 }
 
-                let order = BTPayPalNativeOrder(
-                    payPalClientID: payPalClientID,
-                    environment: environment,
-                    orderID: hermesResponse.orderID
-                )
+                guard let payPalClientID = config.json?["paypal"]["clientId"].asString() else {
+                    completion(.failure(.payPalClientIDNotFound))
+                    return
+                }
 
-                self.payPalContextID = order.orderID
-                completion(.success(order))
+                let payPalEnvironment: PayPalCheckout.Environment?
+                if config.environment == "production" {
+                    payPalEnvironment = .live
+                } else if config.environment == "sandbox" {
+                    payPalEnvironment = .sandbox
+                } else {
+                    payPalEnvironment = nil
+                }
+
+                guard let environment = payPalEnvironment else {
+                    completion(.failure(.invalidEnvironment))
+                    return
+                }
+
+                self.apiClient.post(
+                    request.hermesPath,
+                    parameters: request.parameters(with: config)
+                ) { json, _, error in
+                    guard let hermesResponse = BTPayPalNativeHermesResponse(json: json), error == nil else {
+                        let underlyingError = error ?? BTPayPalNativeCheckoutError.invalidJSONResponse
+                        completion(.failure(.orderCreationFailed(underlyingError)))
+                        return
+                    }
+
+                    let order = BTPayPalNativeOrder(
+                        payPalClientID: payPalClientID,
+                        environment: environment,
+                        orderID: hermesResponse.orderID
+                    )
+
+                    self.payPalContextID = order.orderID
+                    completion(.success(order))
+                }
+            } catch {
+                completion(.failure(.fetchConfigurationFailed))
             }
         }
     }
