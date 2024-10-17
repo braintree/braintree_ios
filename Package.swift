3ab5cebd0be09e505f33a4c973ba83964849e7ce
stechiu <stechiu@paypal.com>
2024-10-17 13:34:18 -0700

    Revert "Merge branch 'v7' of https://github.com/braintree/braintree_ios into v7-venmo-universal-link"
    This reverts commit 438dfc7af31d42a040bd55957f3e95ce1840f79e, reversing
    changes made to 1a24560d78b449a4bcde006d0f4d26cbd17cfb25.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Braintree",
-    platforms: [.iOS(.v16)],
+    platforms: [.iOS(.v14)],
     products: [
         .library(
             name: "BraintreeAmericanExpress",
