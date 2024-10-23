3e2de79d65c01a415c6c9a68cc97bd81dc40890b
richherrera <ricardherrera@paypal.com>
2024-10-23 12:36:11 -0600

    Remove public access level

--- Sources/BraintreeLocalPayment/BTLocalPaymentRequest.swift
+++ Sources/BraintreeLocalPayment/BTLocalPaymentRequest.swift
@@ -11,58 +11,58 @@ import BraintreeDataCollector
 /// Used to initialize a local payment flow
 @objcMembers public class BTLocalPaymentRequest: NSObject {
     
-    // MARK: - Public Properties
+    // MARK: - Internal Properties
     
     /// The type of payment.
-    public var paymentType: String?
+    var paymentType: String?
     
     ///  The country code of the local payment.
     ///
     ///  This value must be one of the supported country codes for a given local payment type listed at the link below. For local payments supported in multiple countries, this value may determine which banks are presented to the customer.
     ///
     /// https://developer.paypal.com/braintree/docs/guides/local-payment-methods/client-side-custom/ios/v5#invoke-payment-flow
-    public var paymentTypeCountryCode: String?
+    var paymentTypeCountryCode: String?
     
     /// Optional: A non-default merchant account to use for tokenization.
-    public var merchantAccountID: String?
+    var merchantAccountID: String?
     
     /// Optional: The address of the customer. An error will occur if this address is not valid.
-    public var address: BTPostalAddress?
+    var address: BTPostalAddress?
     
     /// The amount for the transaction.
-    public var amount: String?
+    var amount: String?
     
     /// Optional: A valid ISO currency code to use for the transaction. Defaults to merchant currency code if not set.
-    public var currencyCode: String?
+    var currencyCode: String?
     
     /// Optional: The merchant name displayed inside of the local payment flow.
-    public var displayName: String?
+    var displayName: String?
     
     /// Optional: Payer email of the customer.
-    public var email: String?
+    var email: String?
     
     /// Optional: Given (first) name of the customer.
-    public var givenName: String?
+    var givenName: String?
     
     /// Optional: Surname (last name) of the customer.
-    public var surname: String?
+    var surname: String?
     
     /// Optional: Phone number of the customer.
-    public var phone: String?
+    var phone: String?
     
     ///  Indicates whether or not the payment needs to be shipped. For digital goods, this should be false. Defaults to false.
-    public var isShippingAddressRequired: Bool = false
+    var isShippingAddressRequired: Bool = false
     
     /// Optional: Bank Identification Code of the customer (specific to iDEAL transactions).
-    public var bic: String?
+    var bic: String?
     
-    public weak var localPaymentFlowDelegate: BTLocalPaymentRequestDelegate?
-    
-    // MARK: - Internal Properties
+    weak var localPaymentFlowDelegate: BTLocalPaymentRequestDelegate?
 
     var paymentID: String?
     var correlationID: String?
     
+    // MARK: - Initializer
+
     public init(
         paymentType: String? = nil,
         paymentTypeCountryCode: String? = nil,
