cb531ad8bac5eaf14c24df6a484259232cccc4fa
stechiu <stechiu@paypal.com>
2024-10-28 14:44:22 -0700

    Reset v7 files

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Braintree",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v16)],
     products: [
         .library(
             name: "BraintreeAmericanExpress",
