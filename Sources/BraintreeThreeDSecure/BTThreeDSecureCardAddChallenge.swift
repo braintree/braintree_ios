b2bec4a7a7638818e90bb3b13d658912b0b9a9a4
ageddam <ageddam@paypal.com>
2024-11-05 12:32:05 -0600

    Revert "cleanup"
    This reverts commit 34998b25cc2c6482e6b81f94790473fd5467ead2.

---
+++ Sources/BraintreeThreeDSecure/BTThreeDSecureCardAddChallenge.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+// NEXT_MAJOR_VERSION remove BTThreeDSecureCardAddChallenge
+/// The card add challenge request
+@objc public enum BTThreeDSecureCardAddChallenge: Int {
+
+    /// Unspecified
+    case unspecified
+
+    /// Requested
+    case requested
+
+    /// Not Requested
+    case notRequested
+}
