4ae22c7d5261d8992eb36d551c49c679000b05d5
richherrera <ricardherrera@paypal.com>
2024-11-21 12:28:58 -0600

    Add contact information struct

---
+++ Sources/BraintreePayPal/BTContactInformation.swift
@@ -0,0 +1,8 @@
+/// Contact information of the recipient for the order
+struct BTContactInformation {
+    
+    /// Email address of the recipient
+    let recipientEmail: String?
+    
+    let recipientPhoneNumber: BTPayPalPhoneNumber?
+}
