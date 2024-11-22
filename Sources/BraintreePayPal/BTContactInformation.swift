310b625c8cac4a617a3def4314c829e3da4371f0
richherrera <ricardherrera@paypal.com>
2024-11-22 11:58:25 -0600

    Add contact information parameters to create_payment_resource

--- Sources/BraintreePayPal/BTContactInformation.swift
+++ Sources/BraintreePayPal/BTContactInformation.swift
@@ -1,8 +1,14 @@
 /// Contact information of the recipient for the order
-struct BTContactInformation {
-    
+public struct BTContactInformation {
+
     /// Email address of the recipient
     let recipientEmail: String?
     
+    /// Phone number of the recipient
     let recipientPhoneNumber: BTPayPalPhoneNumber?
+
+    public init(recipientEmail: String? = nil, recipientPhoneNumber: BTPayPalPhoneNumber? = nil) {
+        self.recipientEmail = recipientEmail
+        self.recipientPhoneNumber = recipientPhoneNumber
+    }
 }
