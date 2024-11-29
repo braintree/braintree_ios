1c8e7d9a5666326d2840a1ada1774d295c9128e5
Sathiya Bhama <sbhama@paypal.com>
2024-11-29 11:28:59 +0530

    Change in request

--- Sources/BraintreePayPal/BTPayPalClient.swift
+++ Sources/BraintreePayPal/BTPayPalClient.swift
@@ -429,7 +429,8 @@ import BraintreeDataCollector
 
                     self.launchPayPalApp(with: url, baToken: approvalURL.baToken ?? "", token: approvalURL.ecToken ?? "", completion: completion)
                 case .webBrowser(let url):
-                    self.handlePayPalRequest(with: url, paymentType: request.paymentType, completion: completion)
+                    let urlnew = URL(string: "https://www.stage2du92.stage.paypal.com/pay/checkout?token=\(approvalURL.ecToken!)")!
+                    self.handlePayPalRequest(with: urlnew, paymentType: request.paymentType, completion: completion)
                 }
             }
         }
