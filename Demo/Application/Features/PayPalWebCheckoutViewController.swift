06969f11357a9adb7154b4eee2efa76ce9094b38
Rich Herrera <ricardherrera@paypal.com>
2024-12-04 11:18:15 -0600

    Fix merge on demo app (#1478)

--- Demo/Application/Features/PayPalWebCheckoutViewController.swift
+++ Demo/Application/Features/PayPalWebCheckoutViewController.swift
@@ -153,10 +153,6 @@ class PayPalWebCheckoutViewController: PaymentButtonBaseViewController {
         request.lineItems = [lineItem]
         request.offerPayLater = payLaterToggle.isOn
         request.intent = newPayPalCheckoutToggle.isOn ? .sale : .authorize
-        request.contactInformation = BTContactInformation(
-            recipientEmail: "some@email.com",
-            recipientPhoneNumber: .init(countryCode: "52", nationalNumber: "123456789")
-        )
 
         if contactInformationToggle.isOn {
             request.contactInformation = BTContactInformation(
