75674cad07e394d770d560385be1d6c9abe8faa9
Justin Warmkessel <Jwarmkessel@paypal.com>
2024-12-19 12:59:38 -0800

    Update docs

--- Sources/BraintreeShopperInsights/BTButtonOrder.swift
+++ Sources/BraintreeShopperInsights/BTButtonOrder.swift
@@ -4,30 +4,30 @@ import Foundation
 /// - Warning: This module is in beta. It's public API may change or be removed in future releases.
 public enum BTButtonOrder: String {
 
-    /// 0
+    /// First place
     case first = "1"
 
-    /// 1
+    /// Second place
     case second = "2"
 
-    /// 2
+    /// Third place
     case third = "3"
 
-    /// 3
+    /// Fourth place
     case fourth = "4"
 
-    /// 4
+    /// Fifth place
     case fifth = "5"
 
-    /// 5
+    /// Sixth place
     case sixth = "6"
 
-    /// 6
+    /// Seventh place
     case seventh = "7"
 
-    /// 7
+    /// Eighth place
     case eighth = "8"
 
-    /// 8
+    /// Greater than Eighth place
     case other = "other"
 }
