27c0a4422e6bb5a5475590cfba54a0e890c06ea0
richherrera <ricardherrera@paypal.com>
2025-01-06 14:25:29 -0600

    Add error to catch encodable exception

--- Sources/BraintreePayPal/BTPayPalError.swift
+++ Sources/BraintreePayPal/BTPayPalError.swift
@@ -44,6 +44,9 @@ public enum BTPayPalError: Error, CustomNSError, LocalizedError, Equatable {
 
     /// 13. Missing PayPal Request
     case missingPayPalRequest
+    
+    /// 14. Unable to create Encodable
+    case failedToCreateEncodable
 
     public static var errorDomain: String {
         "com.braintreepayments.BTPayPalErrorDomain"
@@ -79,6 +82,8 @@ public enum BTPayPalError: Error, CustomNSError, LocalizedError, Equatable {
             return 12
         case .missingPayPalRequest:
             return 13
+        case .failedToCreateEncodable:
+            return 14
         }
     }
 
@@ -114,6 +119,8 @@ public enum BTPayPalError: Error, CustomNSError, LocalizedError, Equatable {
             return "Missing BA Token for PayPal App Switch."
         case .missingPayPalRequest:
             return "The PayPal Request was missing or invalid."
+        case .failedToCreateEncodable:
+            return "Unable to create Encodable object"
         }
     }
 
