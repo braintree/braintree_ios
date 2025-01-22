f6c5710fca685e68affa3b28e265c687bc11606a
warmkesselj <150195168+warmkesselj@users.noreply.github.com>
2025-01-22 10:29:52 -0800

    Update Sources/BraintreeShopperInsights/BTShopperInsightsClient.swift
    Co-authored-by: Jax DesMarais-Leder <jdesmarais@paypal.com>

--- Sources/BraintreeShopperInsights/BTShopperInsightsClient.swift
+++ Sources/BraintreeShopperInsights/BTShopperInsightsClient.swift
@@ -110,7 +110,7 @@ public class BTShopperInsightsClient {
     /// Call this method when a button has been selected/tapped by the buyer.
     /// This method sends analytics to help improve the Shopper Insights feature experience.
     /// - Parameter buttonType: Type of button presented - PayPal, Venmo, or Other
-    /// - Warning: This function is in beta.
+    /// - Warning: This function is in beta. It's public API may change or be removed in future releases.
     public func sendSelectedEvent(for buttonType: BTButtonType) {
         apiClient.sendAnalyticsEvent(
             BTShopperInsightsAnalytics.buttonSelected,
