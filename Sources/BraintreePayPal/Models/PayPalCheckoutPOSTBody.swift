c6d62a0c41b90afa233b21b2d0bf52000ace8f31
richherrera <ricardherrera@paypal.com>
2025-01-27 13:29:11 -0600

    Add contact information

--- Sources/BraintreePayPal/Models/PayPalCheckoutPOSTBody.swift
+++ Sources/BraintreePayPal/Models/PayPalCheckoutPOSTBody.swift
@@ -22,6 +22,8 @@ struct PayPalCheckoutPOSTBody: Encodable {
     private var currencyCode: String?
     private var lineItems: [BTPayPalLineItem]?
     private var merchantAccountID: String?
+    private var recipientPhoneNumber: BTPayPalPhoneNumber?
+    private var recipientEmail: String?
     private var requestBillingAgreement: Bool?
     private var riskCorrelationID: String?
     private var shippingCallbackURL: String?
@@ -83,6 +85,18 @@ struct PayPalCheckoutPOSTBody: Encodable {
             self.userAuthenticationEmail = userAuthenticationEmail
         }
         
+        if let recipientEmail = payPalRequest.contactInformation?.recipientEmail {
+            self.recipientEmail = recipientEmail
+        }
+        
+        if let recipientPhoneNumber = payPalRequest.contactInformation?.recipientPhoneNumber {
+            self.recipientPhoneNumber = recipientPhoneNumber
+        }
+        
+        if let shippingCallbackURL = payPalRequest.shippingCallbackURL {
+            self.shippingCallbackURL = shippingCallbackURL.absoluteString
+        }
+        
         self.userPhoneNumber = payPalRequest.userPhoneNumber
         self.returnURL = BTCoreConstants.callbackURLScheme + "://\(PayPalRequestConstants.callbackURLHostAndPath)success"
         self.cancelURL = BTCoreConstants.callbackURLScheme + "://\(PayPalRequestConstants.callbackURLHostAndPath)cancel"
@@ -99,6 +113,8 @@ struct PayPalCheckoutPOSTBody: Encodable {
         case lineItems = "line_items"
         case merchantAccountID = "merchant_account_id"
         case offerPayLater = "offer_pay_later"
+        case recipientPhoneNumber = "international_phone"
+        case recipientEmail = "recipient_email"
         case requestBillingAgreement = "request_billing_agreement"
         case returnURL = "return_url"
         case riskCorrelationID = "correlation_id"
