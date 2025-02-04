8002beddfd2c4fa34deea3914004c6838920640a
Jax DesMarais-Leder <jdesmarais@paypal.com>
2025-02-04 11:21:00 -0600

    Update Demo/Application/Features/ShopperInsightsViewController.swift

--- Demo/Application/Features/ShopperInsightsViewController.swift
+++ Demo/Application/Features/ShopperInsightsViewController.swift
@@ -6,7 +6,7 @@ import BraintreeShopperInsights
 
 class ShopperInsightsViewController: PaymentButtonBaseViewController {
     
-    lazy var shopperInsightsClient = BTShopperInsightsClient(apiClient: apiClient, shopperSessionID: "123456")
+    lazy var shopperInsightsClient = BTShopperInsightsClient(apiClient: apiClient, shopperSessionID: "test-shopper-session-id")
     lazy var payPalClient = BTPayPalClient(apiClient: apiClient)
     lazy var venmoClient = BTVenmoClient(apiClient: apiClient)
     
