8853ee884834c9a313c556cbcf2dc0943bca7942
Justin Warmkessel <Jwarmkessel@paypal.com>
2025-02-25 14:18:53 -0800

    Test

--- Demo/UI Tests/ThreeDSecure UI Tests/ThreeDSecure_V2_UITests.swift
+++ Demo/UI Tests/ThreeDSecure UI Tests/ThreeDSecure_V2_UITests.swift
@@ -20,39 +20,36 @@ class ThreeDSecure_V2_UITests: XCTestCase {
         waitForElementToAppear(app.cardNumberTextField)
         app.enterCardDetailsWith(cardNumber: "4000000000001091", expirationDate: expirationDate)
         app.tokenizeButton.tap()
-        sleep(2)
 
         waitForElementToAppear(app.staticTexts["Purchase Authentication"], timeout: .threeDSecureTimeout)
 
         let textField = app.textFields.element(boundBy: 1)
         waitForElementToBeHittable(textField)
         textField.forceTapElement()
-        sleep(2)
         textField.typeText("1234")
 
+        waitForElementToBeHittable(app.cardinalSubmitButton)
         app.cardinalSubmitButton.forceTapElement()
-        sleep(2)
 
         waitForElementToAppear(app.liabilityShiftedMessage)
     }
     
     func testThreeDSecurePaymentFlowV2_challengeFlow_andFails() {
         waitForElementToAppear(app.cardNumberTextField)
+
         app.enterCardDetailsWith(cardNumber: "4000000000001109", expirationDate: expirationDate)
         app.tokenizeButton.tap()
-        sleep(2)
 
         waitForElementToAppear(app.staticTexts["Purchase Authentication"], timeout: .threeDSecureTimeout)
 
         let textField = app.textFields.element(boundBy: 1)
-
         waitForElementToBeHittable(textField)
+
         textField.forceTapElement()
-        sleep(2)
         textField.typeText("1234")
 
+        waitForElementToBeHittable(app.cardinalSubmitButton)
         app.cardinalSubmitButton.forceTapElement()
-        sleep(2)
 
         waitForElementToAppear(app.liabilityCouldNotBeShiftedMessage, timeout: 30)
     }
