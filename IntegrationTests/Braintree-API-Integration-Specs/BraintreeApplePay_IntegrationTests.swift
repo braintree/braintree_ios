b4497c36742cef061a56894d236f6a5b9b260ce4
Justin Warmkessel <Jwarmkessel@paypal.com>
2025-03-04 11:23:34 -0800

    Update signature

--- IntegrationTests/Braintree-API-Integration-Specs/BraintreeApplePay_IntegrationTests.swift
+++ IntegrationTests/Braintree-API-Integration-Specs/BraintreeApplePay_IntegrationTests.swift
@@ -7,7 +7,7 @@ class BraintreeApplePay_IntegrationTests: XCTestCase {
 
     func testTokenizeApplePayPayment_whenApplePayEnabledInControlPanel_returnsANonce() {
         let apiClient = BTAPIClient(authorization: BTIntegrationTestsConstants.sandboxTokenizationKey)!
-        let applePayClient = BTApplePayClient(apiClient: apiClient)
+        let applePayClient = BTApplePayClient(authorization: "sandbox_9dbg82cq_dcpspy2brwdjr3qn")
         let expectation = expectation(description: "Tokenize Apple Pay payment")
 
         applePayClient.tokenize(PKPayment()) { tokenizedApplePayAccount, error in
@@ -26,7 +26,7 @@ class BraintreeApplePay_IntegrationTests: XCTestCase {
 
     func testTokenizeApplePayPayment_whenApplePayDisabledInControlPanel_returnsError() {
         let apiClient = BTAPIClient(authorization: BTIntegrationTestsConstants.sandboxTokenizationKeyApplePayDisabled)!
-        let applePayClient = BTApplePayClient(apiClient: apiClient)
+        let applePayClient = BTApplePayClient(authorization: "sandbox_9dbg82cq_dcpspy2brwdjr3qn")
         let expectation = expectation(description: "Tokenize Apple Pay payment")
 
         applePayClient.tokenize(PKPayment()) { nonce, error in
