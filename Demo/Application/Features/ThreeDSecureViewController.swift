62148d07158fc7090b686cfcf937046c4ad20df8
Justin Warmkessel <Jwarmkessel@paypal.com>
2025-03-11 14:31:59 -0700

    Update

--- Demo/Application/Features/ThreeDSecureViewController.swift
+++ Demo/Application/Features/ThreeDSecureViewController.swift
@@ -10,7 +10,7 @@ class ThreeDSecureViewController: PaymentButtonBaseViewController {
     var callbackCountLabel = UILabel()
     var callbackCount: Int = 0
     
-    lazy var threeDSecureClient = BTThreeDSecureClient(authorization: "sandbox_9dbg82cq_dcpspy2brwdjr3qn")
+    lazy var threeDSecureClient = BTThreeDSecureClient(authorization: authorization)
 
     override func viewDidLoad() {
         super.viewDidLoad()
