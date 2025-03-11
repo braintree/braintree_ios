7dcb157572daa25b56533819686d69ed9be0d8fb
Justin Warmkessel <Jwarmkessel@paypal.com>
2025-03-11 09:27:37 -0700

    Demo constants

---
+++ Demo/Application/Base/DemoConstants.swift
@@ -0,0 +1,4 @@
+struct DemoConstants {
+    /// Replace <CLIENT_AUTHORIZATION> with your actual Braintree client authorization string.
+    static let sandboxTokenizationKey = "sandbox_9dbg82cq_dcpspy2brwdjr3qn"
+}
