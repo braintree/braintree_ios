02786e98c5f06558629b526e3b510ddaf7d53013
ageddam <ageddam@paypal.com>
2025-03-31 12:40:48 -0500

    address pr feedback

--- Demo/Application/Features/IdealViewController.swift
+++ Demo/Application/Features/IdealViewController.swift
@@ -47,7 +47,7 @@ class IdealViewController: PaymentButtonBaseViewController {
     }
 
     private func startPaymentWithBank() {
-        localPaymentClient = BTLocalPaymentClient(authorization: "sandbox_f252zhq7_hh4cpc39zq4rgjcg")
+        localPaymentClient = BTLocalPaymentClient(authorization: authorization)
 
         let postalAddress = BTPostalAddress()
         postalAddress.countryCodeAlpha2 = "NL"
