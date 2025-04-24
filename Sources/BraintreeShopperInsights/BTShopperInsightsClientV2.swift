415afb8cf0dad090224cca605bb0f92c7994b7c7
richherrera <ricardherrera@paypal.com>
2025-04-24 11:15:15 -0600

    Add app installed checks and analytics events

--- Sources/BraintreeShopperInsights/BTShopperInsightsClientV2.swift
+++ Sources/BraintreeShopperInsights/BTShopperInsightsClientV2.swift
@@ -1,3 +1,5 @@
+import UIKit
+
 #if canImport(BraintreeCore)
 import BraintreeCore
 #endif
@@ -10,15 +12,66 @@ import BraintreeCore
 /// - Warning: This feature is in beta. It's public API may change or be removed in future releases.
 public class BTShopperInsightsClientV2 {
     
+    // MARK: - Internal Properties
+    
+    /// Defaults to `UIApplication.shared`, but exposed for unit tests to mock calls to `canOpenURL`.
+    var application: URLOpener = UIApplication.shared
+    
     // MARK: - Private Properties
     
     private let apiClient: BTAPIClient
     
     /// Creates a `BTShopperInsightsClientV2`
     /// - Parameters:
-    ///     - apiClient: A `BTAPIClient` instance.
+    ///    - apiClient: A `BTAPIClient` instance.
     /// - Warning: This init is beta. It's public API may change or be removed in future releases. This feature only works with a client token.
     public init(apiClient: BTAPIClient) {
         self.apiClient = apiClient
+    }
+    
+    /// Call this method when the PayPal or Venmo button has been successfully displayed to the buyer.
+    /// This method sends analytics to help improve the Shopper Insights feature experience.
+    /// - Parameters:
+    ///    - buttonType: Type of button presented - PayPal, Venmo, or Other
+    ///    - presentmentDetails: Detailed information, including button order, experiment type, and
+    ///     page type about the payment button that is sent to analytics to help improve the Shopper Insights
+    ///     feature experience.
+    ///    - sessionID: The shopper session ID
+    /// - Warning: This function is in beta. It's public API may change or be removed in future releases.
+    public func sendPresentedEvent(for buttonType: BTButtonType, presentmentDetails: BTPresentmentDetails, sessionID: String) {
+        apiClient.sendAnalyticsEvent(
+            BTShopperInsightsAnalytics.buttonPresented,
+            buttonOrder: presentmentDetails.buttonOrder.rawValue,
+            buttonType: buttonType.rawValue,
+            merchantExperiment: presentmentDetails.experimentType.formattedExperiment,
+            pageType: presentmentDetails.pageType.rawValue,
+            shopperSessionID: sessionID
+        )
+    }
+
+    /// Call this method when a button has been selected/tapped by the buyer.
+    /// This method sends analytics to help improve the Shopper Insights feature experience.
+    /// - Parameters:
+    ///    - buttonType: Type of button presented - PayPal, Venmo, or Other
+    ///    - sessionID: The shopper session ID
+    /// - Warning: This function is in beta. It's public API may change or be removed in future releases.
+    public func sendSelectedEvent(for buttonType: BTButtonType, sessionID: String) {
+        apiClient.sendAnalyticsEvent(
+            BTShopperInsightsAnalytics.buttonSelected,
+            buttonType: buttonType.rawValue,
+            shopperSessionID: sessionID
+        )
+    }
+    
+    /// Indicates whether the PayPal App is installed.
+    /// - Warning: This method is currently in beta and may change or be removed in future releases.
+    public func isPayPalAppInstalled() -> Bool {
+        application.isPayPalAppInstalled()
+    }
+
+    /// Indicates whether the Venmo App is installed.
+    /// - Warning: This method is currently in beta and may change or be removed in future releases.
+    public func isVenmoAppInstalled() -> Bool {
+        application.isVenmoAppInstalled()
     }
 }
