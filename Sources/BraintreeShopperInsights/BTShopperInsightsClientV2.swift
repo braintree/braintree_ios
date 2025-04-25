00c87405e4e98d9fdc69b5203c24510a139ed76c
Rich Herrera <ricardherrera@paypal.com>
2025-04-25 10:13:53 -0600

    [PRV2] Add BTShopperInsightsClientV2 (#1559)
    * Add BTShopperInsightsClientv2
    
    * Deprecate BTShopperInsightsClient
    
    * Add docstrings & macros validation
    
    * Fix docstring and add mark

---
+++ Sources/BraintreeShopperInsights/BTShopperInsightsClientV2.swift
@@ -0,0 +1,26 @@
+#if canImport(BraintreeCore)
+import BraintreeCore
+#endif
+
+///  Use `BTShopperInsightsClientV2` to optimize your checkout experience by prioritizing the customer’s preferred payment methods in your UI.
+///
+///  By customizing each customer’s checkout experience, you can improve conversion, increase sales/repeat buys and boost user retention/loyalty.
+///
+///  The use of this client is a completely separate integration path from the deprecated `BTShopperInsightsClient`
+/// - Warning: This feature is in beta. It's public API may change or be removed in future releases.
+public class BTShopperInsightsClientV2 {
+    
+    // MARK: - Private Properties
+    
+    private let apiClient: BTAPIClient
+    
+    // MARK: - Initializers
+    
+    /// Creates a `BTShopperInsightsClientV2`
+    /// - Parameters:
+    ///    - apiClient: A `BTAPIClient` instance.
+    /// - Warning: This init is beta. It's public API may change or be removed in future releases. This feature only works with a client token.
+    public init(apiClient: BTAPIClient) {
+        self.apiClient = apiClient
+    }
+}
