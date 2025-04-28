b77fab03619b52357f1824815d0948d95ecc0eb8
stechiu <stechiu@paypal.com>
2025-04-28 12:59:11 -0700

    Removed unnecessary parameters

--- Sources/BraintreePayPal/RecurringBillingMetadata/BTRecurringBillingAmountBreakdown.swift
+++ Sources/BraintreePayPal/RecurringBillingMetadata/BTRecurringBillingAmountBreakdown.swift
@@ -8,10 +8,6 @@ public struct BTRecurringBillingAmountBreakdown {
     private let itemTotal: String
     private let shipping: String
     private let taxTotal: String
-    private let handling: String?
-    private let insurance: String?
-    private let shippingDiscount: String?
-    private let discount: String?
 
     // MARK: - Initializer
 
@@ -27,19 +23,11 @@ public struct BTRecurringBillingAmountBreakdown {
     public init(
         itemTotal: String,
         shipping: String,
-        taxTotal: String,
-        handling: String? = nil,
-        insurance: String? = nil,
-        shippingDiscount: String? = nil,
-        discount: String? = nil
+        taxTotal: String
     ) {
         self.itemTotal = itemTotal
         self.shipping = shipping
         self.taxTotal = taxTotal
-        self.handling = handling
-        self.insurance = insurance
-        self.shippingDiscount = shippingDiscount
-        self.discount = discount
     }
 
     //MARK: - Internal Methods
@@ -50,22 +38,6 @@ public struct BTRecurringBillingAmountBreakdown {
             "shipping": shipping,
             "taxTotal": taxTotal
         ]
-
-        if let handling {
-            parameters["handling"] = handling
-        }
-
-        if let insurance {
-            parameters["insurance"] = insurance
-        }
-
-        if let shippingDiscount {
-            parameters["shipping_discount"] = shippingDiscount
-        }
-
-        if let discount {
-            parameters["discount"] = discount
-        }
         
         return parameters
     }
