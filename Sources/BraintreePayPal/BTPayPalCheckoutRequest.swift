8302650732795fc121ed470fa0d9944b3043d6b7
stechiu <stechiu@paypal.com>
2025-05-09 09:54:12 -0700

    Fixed lint error

--- Sources/BraintreePayPal/BTPayPalCheckoutRequest.swift
+++ Sources/BraintreePayPal/BTPayPalCheckoutRequest.swift
@@ -181,8 +181,8 @@ import BraintreeCore
 
     // MARK: Public Methods
 
-    /// :nodoc: Exposed publicly for use by PayPal Native Checkout module. This method is not covered by semantic versioning.
     // swiftlint:disable:next cyclomatic_complexity
+    /// :nodoc: Exposed publicly for use by PayPal Native Checkout module. This method is not covered by semantic versioning.
     @_documentation(visibility: private)
     public override func parameters(
         with configuration: BTConfiguration,
