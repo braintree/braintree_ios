042112df8134ee3024a148ac6c7d47b2e5830b22
stechiu <stechiu@paypal.com>
2025-05-09 12:01:02 -0700

    Added missing doc strings

--- Sources/BraintreePayPal/BTPayPalVaultBaseRequest.swift
+++ Sources/BraintreePayPal/BTPayPalVaultBaseRequest.swift
@@ -19,6 +19,7 @@ import BraintreeCore
     ///   - offerCredit: Optional: Offers PayPal Credit if the customer qualifies. Defaults to `false`.
     ///   - userAuthenticationEmail: Optional: User email to initiate a quicker authentication flow in cases where the user has a PayPal Account with the same email.
     ///   - enablePayPalAppSwitch: Optional: Used to determine if the customer will use the PayPal app switch flow. Defaults to `false`.
+    ///   - recurringBillingDetails: Optional: Recurring billing product details.
     ///   - recurringBillingPlanType: Optional: Recurring billing plan type, or charge pattern.
     ///   - userAuthenticationEmail: Optional: User email to initiate a quicker authentication flow in cases where the user has a PayPal Account with the same email.
     public init(
