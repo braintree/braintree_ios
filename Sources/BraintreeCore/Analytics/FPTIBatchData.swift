ed773a9e9a61bcf803faf5a217f15665ffe0070f
richherrera <ricardherrera@paypal.com>
2025-05-20 16:39:17 -0600

    Update component

--- Sources/BraintreeCore/Analytics/FPTIBatchData.swift
+++ Sources/BraintreeCore/Analytics/FPTIBatchData.swift
@@ -157,7 +157,7 @@ struct FPTIBatchData: Codable {
 
         let clientOS: String = UIDevice.current.systemName + " " + UIDevice.current.systemVersion
 
-        let component = "rich_0520_03_braintreeclientsdk"
+        let component = "rich_0520_04_braintreeclientsdk"
 
         let deviceManufacturer = "Apple"
 
