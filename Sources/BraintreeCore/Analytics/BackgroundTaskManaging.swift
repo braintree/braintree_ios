15e283260be90a2409ea011150661a6252d003a4
richherrera <ricardherrera@paypal.com>
2025-05-29 12:37:18 -0600

    Fix lint

--- Sources/BraintreeCore/Analytics/BackgroundTaskManaging.swift
+++ Sources/BraintreeCore/Analytics/BackgroundTaskManaging.swift
@@ -1,7 +1,10 @@
 import UIKit
 
 protocol BackgroundTaskManaging {
-    nonisolated func beginBackgroundTask(withName taskName: String?, expirationHandler handler: (@MainActor @Sendable () -> Void)?) -> UIBackgroundTaskIdentifier
+    nonisolated func beginBackgroundTask(
+        withName taskName: String?,
+        expirationHandler handler: (@MainActor @Sendable () -> Void)?
+    ) -> UIBackgroundTaskIdentifier
     nonisolated func endBackgroundTask(_ identifier: UIBackgroundTaskIdentifier)
 }
 
