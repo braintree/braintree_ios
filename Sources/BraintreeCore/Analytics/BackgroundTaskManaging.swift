e064d589ab283357bfb5548627413101c8daeeb5
richherrera <ricardherrera@paypal.com>
2025-05-30 10:43:26 -0600

    Add MainActor attribute

--- Sources/BraintreeCore/Analytics/BackgroundTaskManaging.swift
+++ Sources/BraintreeCore/Analytics/BackgroundTaskManaging.swift
@@ -1,11 +1,11 @@
 import UIKit
 
-protocol BackgroundTaskManaging {
-    nonisolated func beginBackgroundTask(
+@MainActor protocol BackgroundTaskManaging {
+    func beginBackgroundTask(
         withName taskName: String?,
         expirationHandler handler: (@MainActor @Sendable () -> Void)?
     ) -> UIBackgroundTaskIdentifier
-    nonisolated func endBackgroundTask(_ identifier: UIBackgroundTaskIdentifier)
+    func endBackgroundTask(_ identifier: UIBackgroundTaskIdentifier)
 }
 
 extension UIApplication: BackgroundTaskManaging { }
