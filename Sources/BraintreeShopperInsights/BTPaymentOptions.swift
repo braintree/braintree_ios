a3427c5b03ac9c4ebd44c2fb8c16abad6010b0e9
richherrera <ricardherrera@paypal.com>
2025-06-02 10:27:25 -0600

    Add properties and docstrings

--- Sources/BraintreeShopperInsights/BTPaymentOptions.swift
+++ Sources/BraintreeShopperInsights/BTPaymentOptions.swift
@@ -1,1 +1,12 @@
+import Foundation
 
+/// A single payment recommendation
+/// - Warning: This feature is in beta. It's public API may change or be removed in future releases.
+public struct BTPaymentOptions {
+
+    /// The payment option type
+    public let paymentOption: String
+    
+    /// The rank of the payment option
+    public let recommendedPriority: Int
+}
