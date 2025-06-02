e61281f9bd03bfe50d6c3209f7ef3999064d3a58
Jax DesMarais-Leder <jdesmarais@paypal.com>
2025-06-02 14:32:37 -0500

    Update Sources/BraintreePayPal/BTPayPalRequestUserAction.swift
    Co-authored-by: Stephanie <127455800+stechiu@users.noreply.github.com>

--- Sources/BraintreePayPal/BTPayPalRequestUserAction.swift
+++ Sources/BraintreePayPal/BTPayPalRequestUserAction.swift
@@ -3,11 +3,11 @@
 ///  - Note: By default the final button will show the localized word for "Continue" for Checkout requests or
 ///  "Save and Continue" for Vault requests and implies that the final amount billed is not yet known.
 ///
+///  Setting the `BTPayPalCheckoutRequest.userAction` to `.payNow` changes
+///  the button text to "Pay Now", conveying to the user that billing will take place immediately.
+///
 ///  Setting the `BTPayPalVaultRequest.userAction` to `.setupNow` changes the button text to "Setup Now", conveying to
 ///  the user that the funding instrument will be set up for future payments.
-
-///  Setting the `BTPayPalCheckoutRequest.userAction` userAction to `.payNow` changes
-///  the button text to "Pay Now", conveying to the user that billing will take place immediately.
 @objc public enum BTPayPalRequestUserAction: Int {
     /// Default
     case none
