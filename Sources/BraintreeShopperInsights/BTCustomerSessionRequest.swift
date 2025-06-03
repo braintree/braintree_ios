54d22496ad8c3c8857e5f85498487175685286d3
richherrera <ricardherrera@paypal.com>
2025-06-03 14:09:45 -0600

    Update BTCustomerSessionRequest to mimic android

--- Sources/BraintreeShopperInsights/BTCustomerSessionRequest.swift
+++ Sources/BraintreeShopperInsights/BTCustomerSessionRequest.swift
@@ -1,36 +1,51 @@
 import Foundation
 
-/// A `BTCustomerSessionRequest` that specifies options for the Payment Ready v2 flow.
+/// A `BTCustomerSessionRequest`for creating a customer session.
+/// - Warning: This feature is in beta. It's public API may change or be removed in future releases.
 public struct BTCustomerSessionRequest {
-    
-    /// The customer's details
-    let customer: Customer
-    /// The list of purchase units containing the amount and currency code.
-    let purchaseUnits: [BTPurchaseUnit]?
-}
-
-public struct Customer {
     
     /// The customer's email address hashed via SHA256 algorithm.
     let hashedEmail: String?
+    
     /// The customer's phone number hased via SHA256 algorithm.
     let hashedPhoneNumber: String?
+    
     /// Checks whether the PayPal app is installed on the device.
     let paypalAppInstalled: Bool?
+    
     /// Checks whether the Venmo app is installed on the device
     let venmoAppInstalled: Bool?
+    
+    /// The list of purchase units containing the amount and currency code.
+    let purchaseUnits: [BTPurchaseUnit]?
+    
+    public init(
+        hashedEmail: String? = nil,
+        hashedPhoneNumber: String? = nil,
+        paypalAppInstalled: Bool? = nil,
+        venmoAppInstalled: Bool? = nil,
+        purchaseUnits: [BTPurchaseUnit]? = nil
+    ) {
+        self.hashedEmail = hashedEmail
+        self.hashedPhoneNumber = hashedPhoneNumber
+        self.paypalAppInstalled = paypalAppInstalled
+        self.venmoAppInstalled = venmoAppInstalled
+        self.purchaseUnits = purchaseUnits
+    }
 }
 
+/// Amounts of the items purchased.
+/// - Warning: This feature is in beta. It's public API may change or be removed in future releases.
 public struct BTPurchaseUnit {
     
-    /// The amount that is charged for each purchase unit.
-    let amount: Amount
-}
-
-public struct Amount {
+    /// The amount of money, either a whole number or a number with up to 3 decimal places.
+    let amount: String
     
-    /// The value of the amount
-    let value: String?
-    /// The currency code for the amount
-    let currencyCode: String?
+    /// The currency code for the monetary amount.
+    let currencyCode: String
+    
+    public init(amount: String, currencyCode: String) {
+        self.amount = amount
+        self.currencyCode = currencyCode
+    }
 }
