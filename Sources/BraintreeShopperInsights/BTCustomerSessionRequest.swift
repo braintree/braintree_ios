0e64c406969d04dad94e863f8c2ddf329338dff7
richherrera <ricardherrera@paypal.com>
2025-06-04 13:39:55 -0600

    Move docstrings

--- Sources/BraintreeShopperInsights/BTCustomerSessionRequest.swift
+++ Sources/BraintreeShopperInsights/BTCustomerSessionRequest.swift
@@ -4,21 +4,20 @@ import Foundation
 /// - Warning: This feature is in beta. It's public API may change or be removed in future releases.
 public struct BTCustomerSessionRequest {
     
-    /// The customer's email address hashed via SHA256 algorithm.
     let hashedEmail: String?
-    
-    /// The customer's phone number hased via SHA256 algorithm.
     let hashedPhoneNumber: String?
-    
-    /// Checks whether the PayPal app is installed on the device.
     let paypalAppInstalled: Bool?
-    
-    /// Checks whether the Venmo app is installed on the device
     let venmoAppInstalled: Bool?
-    
-    /// The list of purchase units containing the amount and currency code.
     let purchaseUnits: [BTPurchaseUnit]?
     
+    /// Creates a BTCustomerSessionRequest
+    /// - Parameters:
+    ///   - hashedEmail: Optional: The customer's email address hashed via SHA256 algorithm.
+    ///   - hashedPhoneNumber: Optional: The customer's phone number hased via SHA256 algorithm.
+    ///   - paypalAppInstalled: Optional: Checks whether the PayPal app is installed on the device.
+    ///   - venmoAppInstalled: Optional: Checks whether the Venmo app is installed on the device
+    ///   - purchaseUnits: Optional: The list of purchase units containing the amount and currency code.
+    /// - Warning: This feature is in beta. It's public API may change or be removed in future releases.
     public init(
         hashedEmail: String? = nil,
         hashedPhoneNumber: String? = nil,
