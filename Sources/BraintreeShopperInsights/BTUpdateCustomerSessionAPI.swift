53f1490625cd7972e8a3a0f66f28a2902183d238
Rich Herrera <ricardherrera@paypal.com>
2025-06-09 10:08:01 -0600

    [PRV2] Add UpdateCustomerSession GraphQL Mutation (#1582)
    * address pr feedback and add helper graphql request builder
    
    * cleanup, fix cocoapods path, address pr feedback
    
    * fix docstring for request object
    
    * cleanup and address pr feedback
    
    * cleanup
    
    * address pr feedback
    
    * add initial structure files
    
    * Add files
    
    * Rename some properties
    
    * Change initializer process
    
    * Test async calls
    
    * Update BTCustomerSessionRequest to mimic android
    
    * Sort files
    
    * Move BTPurchaseUnit
    
    * Remove unnecessary docstrings
    
    * Update GraphQL model
    
    * Update UTs
    
    * Sort files
    
    * Move docstrings
    
    * Move docstrings
    
    * Remove unnecessary self
    
    * Remove duplicated folder
    
    * Updated GraphQLBody
    
    * Update GraphQL variables
    
    * Remove unnecessary selfs's
    
    * Pass the session ID to the GraphQL query
    
    * Update docstring
    
    * Update docstrings
    
    * Rename class
    
    * Add and fix UTs
    
    * Rename payPalAppInstalled property
    
    * Use async/await instead of callback
    
    * Update UTs
    
    * Update tests
    
    * Fix graphQL body
    
    * Update API
    
    * Add docstring
    
    * Add teardown
    
    * Add graph ql tests
    
    * Sort files
    
    * Add query assert
    
    * Add query assert
    
    * Update Sources/BraintreeShopperInsights/BTUpdateCustomerSessionAPI.swift
    
    Co-authored-by: Jax DesMarais-Leder <jdesmarais@paypal.com>
    
    ---------
    
    Co-authored-by: ageddam <ageddam@paypal.com>
    Co-authored-by: Jax DesMarais-Leder <jdesmarais@paypal.com>

---
+++ Sources/BraintreeShopperInsights/BTUpdateCustomerSessionAPI.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+#if canImport(BraintreeCore)
+import BraintreeCore
+#endif
+
+/// The API used to update a customer session using the `UpdateCustomerSession` GraphQL mutation.
+final class BTUpdateCustomerSessionAPI {
+    
+    // MARK: - Properties
+    
+    private let apiClient: BTAPIClient
+    
+    // MARK: - Initializer
+    
+    /// Creates a `BTUpdateCustomerSessionAPI`
+    /// - Parameters:
+    ///    - apiClient: A `BTAPIClient` instance
+    init(apiClient: BTAPIClient) {
+        self.apiClient = apiClient
+    }
+    
+    /// This method will call the `UpdateCustomerSession` GQL mutation, which returns a session ID if successful.
+    /// - Parameters:
+    ///    - request: A `BTCustomerSessionRequest`
+    ///    - sessionID: The session ID to update.
+    ///    - Returns: A `String` containing the session ID
+    ///    - Throws: An error if the request fails or if the response is invalid.
+    func execute(
+        _ request: BTCustomerSessionRequest,
+        sessionID: String
+    ) async throws -> String {
+        do {
+            let graphQLParams = UpdateCustomerSessionMutationGraphQLBody(request: request, sessionID: sessionID)
+            
+            let (body, _) = try await apiClient.post("", parameters: graphQLParams, httpType: .graphQLAPI)
+            
+            guard let body else {
+                throw BTShopperInsightsError.emptyBodyReturned
+            }
+            
+            guard let sessionID = body["data"]["updateCustomerSession"]["sessionId"].asString() else {
+                throw BTHTTPError.httpResponseInvalid
+            }
+            
+            return sessionID
+        } catch {
+            throw error
+        }
+    }
+}
