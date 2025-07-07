cef5239c50b7c3c99c569b8ed6dffa370466eb3a
richherrera <ricardherrera@paypal.com>
2025-07-07 13:17:17 -0600

    Fix lint

--- Sources/BraintreeCore/BTWebAuthenticationSession.swift
+++ Sources/BraintreeCore/BTWebAuthenticationSession.swift
@@ -4,6 +4,7 @@ import AuthenticationServices
 /// :nodoc: This class is exposed for internal Braintree use only. Do not use. It is not covered by Semantic Versioning and may change or be removed at any time.
 @_documentation(visibility: private)
 public class BTWebAuthenticationSession: NSObject {
+
     /// :nodoc: This property is exposed for internal Braintree use only. Do not use. It is not covered by Semantic Versioning and may change or be removed at any time.
     public var prefersEphemeralWebBrowserSession: Bool?
 
