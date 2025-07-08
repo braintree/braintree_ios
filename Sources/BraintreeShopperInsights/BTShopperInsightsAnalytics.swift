06cf4144eb06f13a7105302cb3ecc80307e9e09f
ageddam <ageddam@paypal.com>
2025-07-08 10:57:00 -0500

    add payment ready v2 analytic event names

--- Sources/BraintreeShopperInsights/BTShopperInsightsAnalytics.swift
+++ Sources/BraintreeShopperInsights/BTShopperInsightsAnalytics.swift
@@ -3,13 +3,31 @@ import Foundation
 enum BTShopperInsightsAnalytics {
     
     // MARK: - Merchant Triggered Events
-      
+    
     static let buttonPresented = "shopper-insights:button-presented"
     static let buttonSelected = "shopper-insights:button-selected"
-
+    
     // MARK: - SDK Triggered Events
     
     static let recommendedPaymentsStarted = "shopper-insights:get-recommended-payments:started"
     static let recommendedPaymentsSucceeded = "shopper-insights:get-recommended-payments:succeeded"
     static let recommendedPaymentsFailed = "shopper-insights:get-recommended-payments:failed"
+    
+    // MARK: - Payment Ready V2 Events
+    
+    static let createCustomerSessionStarted = "shopper-insights:create-customer-session:started"
+    static let createCustomerSessionSucceeded = "shopper-insights:create-customer-session:succeeded"
+    static let createCustomerSessionFailed = "shopper-insights:create-customer-session:failed"
+    
+    static let updateCustomerSessionStarted = "shopper-insights:update-customer-session:started"
+    static let updateCustomerSessionSucceeded = "shopper-insights:update-customer-session:succeeded"
+    static let updateCustomerSessionFailed = "shopper-insights:update-customer-session:failed"
+    
+    static let getCustomerRecommendationsStarted = "shopper-insights:get-customer-recommendations:started"
+    static let getCustomerRecommendationsSucceeded = "shopper-insights:get-customer-recommendations:succeeded"
+    static let getCustomerRecommendationsFailed = "shopper-insights:get-customer-recommendations:failed"
+    
+    static let manageCustomerSessionWithRecommendationsStarted = "shopper-insights:manage-customer-session-with-recommendations:started"
+    static let manageCustomerSessionWithRecommendationsSucceeded = "shopper-insights:manage-customer-session-with-recommendations:succeeded"
+    static let manageCustomerSessionWithRecommendationsFailed = "shopper-insights:manage-customer-session-with-recommendations:failed"
 }
