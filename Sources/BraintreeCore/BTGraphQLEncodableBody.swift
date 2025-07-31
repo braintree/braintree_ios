eb2d9645b8c12fb114ce71cbcc6e37376881097f
Justin Warmkessel <jwarmkessel@paypal.com>
2025-07-31 09:41:25 -0700

    Remove the original optional comment.

--- Sources/BraintreeCore/BTGraphQLEncodableBody.swift
+++ Sources/BraintreeCore/BTGraphQLEncodableBody.swift
@@ -6,7 +6,6 @@ public protocol BTGraphQLEncodableBody: Encodable {
     /// The GraphQL mutation or query string to be executed by the backend.
     var query: String { get }
     
-    /// The variables to be used in the GraphQL query.
-    /// This is optional, as some queries may not require variables.
+    /// The variables to be used in the GraphQL query.    
     var variables: Variables { get }
 }
