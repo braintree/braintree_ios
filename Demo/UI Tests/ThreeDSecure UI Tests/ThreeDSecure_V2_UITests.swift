5cf7dbe1c9ac166e810fa2c83ae8e65cdba536df
Justin Warmkessel <jwarmkessel@paypal.com>
2025-08-18 12:49:34 -0700

    Update textField

--- Demo/UI Tests/ThreeDSecure UI Tests/ThreeDSecure_V2_UITests.swift
+++ Demo/UI Tests/ThreeDSecure UI Tests/ThreeDSecure_V2_UITests.swift
@@ -24,7 +24,8 @@ class ThreeDSecure_V2_UITests: XCTestCase {
 
         waitForElementToAppear(app.staticTexts["Purchase Authentication"], timeout: .threeDSecureTimeout)
 
-        let textField = app.textFields.element(boundBy: 1)
+        let textField = app.textFields.allElementsBoundByIndex.first { $0.isHittable } ?? app.secureTextFields.allElementsBoundByIndex.first { $0.isHittable } ?? app.textFields.element(boundBy: 0)
+        
         waitForElementToBeHittable(textField)
         textField.forceTapElement()
         sleep(2)
@@ -34,47 +35,29 @@ class ThreeDSecure_V2_UITests: XCTestCase {
         sleep(2)
 
         waitForElementToAppear(app.liabilityShiftedMessage)
-        
-        //ddd
-        
+    }
+    
+    func testThreeDSecurePaymentFlowV2_challengeFlow_andFails() {
         waitForElementToAppear(app.cardNumberTextField)
         app.enterCardDetailsWith(cardNumber: "4000000000001109", expirationDate: expirationDate)
         app.tokenizeButton.tap()
         sleep(2)
 
         waitForElementToAppear(app.staticTexts["Purchase Authentication"], timeout: .threeDSecureTimeout)
+
         
-        let newTextField = app.secureTextFields.element(boundBy: 1)
-        waitForElementToBeHittable(newTextField)
-        newTextField.forceTapElement()
+        let textField = app.textFields.allElementsBoundByIndex.first { $0.isHittable } ?? app.secureTextFields.allElementsBoundByIndex.first { $0.isHittable } ?? app.textFields.element(boundBy: 0)
+        
+        waitForElementToBeHittable(textField)
+        textField.forceTapElement()
         sleep(2)
-        newTextField.typeText("1234")
+        textField.typeText("1234")
 
         app.cardinalSubmitButton.forceTapElement()
         sleep(2)
 
         waitForElementToAppear(app.liabilityCouldNotBeShiftedMessage, timeout: 30)
     }
-    
-//    func testThreeDSecurePaymentFlowV2_challengeFlow_andFails() {
-//        waitForElementToAppear(app.cardNumberTextField)
-//        app.enterCardDetailsWith(cardNumber: "4000000000001109", expirationDate: expirationDate)
-//        app.tokenizeButton.tap()
-//        sleep(2)
-//
-//        waitForElementToAppear(app.staticTexts["Purchase Authentication"], timeout: .threeDSecureTimeout)
-//        
-//        let textField = app.secureTextFields.element(boundBy: 1)
-//        waitForElementToBeHittable(textField)
-//        textField.forceTapElement()
-//        sleep(2)
-//        textField.typeText("1234")
-//
-//        app.cardinalSubmitButton.forceTapElement()
-//        sleep(2)
-//
-//        waitForElementToAppear(app.liabilityCouldNotBeShiftedMessage, timeout: 30)
-//    }
 
     func testThreeDSecurePaymentFlowV2_returnsToApp_whenCancelTapped() {
         waitForElementToAppear(app.cardNumberTextField)
