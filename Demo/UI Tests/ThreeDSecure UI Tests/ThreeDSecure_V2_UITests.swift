ae4cd43516518047911a7296caaedee1d0a42597
Justin Warmkessel <jwarmkessel@paypal.com>
2025-08-18 11:42:22 -0700

    Try merging the two tests

--- Demo/UI Tests/ThreeDSecure UI Tests/ThreeDSecure_V2_UITests.swift
+++ Demo/UI Tests/ThreeDSecure UI Tests/ThreeDSecure_V2_UITests.swift
@@ -34,17 +34,17 @@ class ThreeDSecure_V2_UITests: XCTestCase {
         sleep(2)
 
         waitForElementToAppear(app.liabilityShiftedMessage)
-    }
-    
-    func testThreeDSecurePaymentFlowV2_challengeFlow_andFails() {
+        
+        //ddd
+        
         waitForElementToAppear(app.cardNumberTextField)
         app.enterCardDetailsWith(cardNumber: "4000000000001109", expirationDate: expirationDate)
         app.tokenizeButton.tap()
         sleep(2)
 
         waitForElementToAppear(app.staticTexts["Purchase Authentication"], timeout: .threeDSecureTimeout)
-
-        let textField = app.textFields.element(boundBy: 1)
+        
+        let textField = app.secureTextFields.element(boundBy: 1)
         waitForElementToBeHittable(textField)
         textField.forceTapElement()
         sleep(2)
@@ -55,6 +55,26 @@ class ThreeDSecure_V2_UITests: XCTestCase {
 
         waitForElementToAppear(app.liabilityCouldNotBeShiftedMessage, timeout: 30)
     }
+    
+//    func testThreeDSecurePaymentFlowV2_challengeFlow_andFails() {
+//        waitForElementToAppear(app.cardNumberTextField)
+//        app.enterCardDetailsWith(cardNumber: "4000000000001109", expirationDate: expirationDate)
+//        app.tokenizeButton.tap()
+//        sleep(2)
+//
+//        waitForElementToAppear(app.staticTexts["Purchase Authentication"], timeout: .threeDSecureTimeout)
+//        
+//        let textField = app.secureTextFields.element(boundBy: 1)
+//        waitForElementToBeHittable(textField)
+//        textField.forceTapElement()
+//        sleep(2)
+//        textField.typeText("1234")
+//
+//        app.cardinalSubmitButton.forceTapElement()
+//        sleep(2)
+//
+//        waitForElementToAppear(app.liabilityCouldNotBeShiftedMessage, timeout: 30)
+//    }
 
     func testThreeDSecurePaymentFlowV2_returnsToApp_whenCancelTapped() {
         waitForElementToAppear(app.cardNumberTextField)
