047bc5ea267e54f9088f21acc6b62422dfc244c3
richherrera <ricardherrera@paypal.com>
2025-09-04 10:22:08 -0600

    Lint

--- Sources/BraintreeCore/BTClientMetadataIntegration.swift
+++ Sources/BraintreeCore/BTClientMetadataIntegration.swift
@@ -1,4 +1,5 @@
-/// :nodoc: This enum is exposed for internal Braintree use only. Do not use. It is not covered by Semantic Versioning and may change or be removed at any time.
+/// :nodoc: This enum is exposed for internal Braintree use only. Do not use.
+/// It is not covered by Semantic Versioning and may change or be removed at any time.
 /// Integration Types
 @_documentation(visibility: private)
 @objc public enum BTClientMetadataIntegration: Int {
@@ -8,7 +9,8 @@
     /// Drop-in
     case dropIn
 
-    /// :nodoc: This property is exposed for internal Braintree use only. Do not use. It is not covered by Semantic Versioning and may change or be removed at any time.
+    /// :nodoc: This property is exposed for internal Braintree use only. Do not use.
+    /// It is not covered by Semantic Versioning and may change or be removed at any time.
     /// String value representing the integration.
     public var stringValue: String {
         switch self {
