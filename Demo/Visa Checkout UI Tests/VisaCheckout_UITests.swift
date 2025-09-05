946d75ce537971581c38810ac6690df6d2459e66
stechiu <stechiu@paypal.com>
2025-09-05 14:04:14 -0700

    WIP

--- Demo/Visa Checkout UI Tests/VisaCheckout_UITests.swift
+++ Demo/Visa Checkout UI Tests/VisaCheckout_UITests.swift
@@ -1,8 +1,10 @@
 import XCTest
 
 class BraintreeVisaCheckout_UITests: XCTestCase {
-        
+
+    // swiftlint:disable implicitly_unwrapped_optional
     var app: XCUIApplication!
+    // swiftlint:enable implicitly_unwrapped_optional
     
     override func setUp() {
         super.setUp()
@@ -36,8 +38,9 @@ class BraintreeVisaCheckout_UITests: XCTestCase {
 
         let expirationDateField = app.textFields["Expires M M / Y Y expiration date formats as you type"]
         expirationDateField.forceTapElement()
+        expirationDateField.typeText(expirationDate)
 
-        let securityCodeField = app.textFields["Security Code Enter your card's security code here. The 3-digit code on the back of your card."]
+        let securityCodeField = app.textFields.element(matching: NSPredicate(format: "label CONTAINS[c] %@", "Security Code"))
         securityCodeField.forceTapElement()
         securityCodeField.typeText("123")
  
@@ -69,7 +72,7 @@ class BraintreeVisaCheckout_UITests: XCTestCase {
         zipCodeField.forceTapElement()
         zipCodeField.typeText("94566")
 
-        let mobileNumber = app.textFields["Mobile Number We may send a one-time code to this number to verify it's you. Message and data rates may apply."]
+        let mobileNumber = app.textFields.element(matching: NSPredicate(format: "label CONTAINS[c] %@", "Mobile Number"))
         mobileNumber.forceTapElement()
         mobileNumber.typeText("864-275-2333")
 
@@ -91,7 +94,7 @@ class BraintreeVisaCheckout_UITests: XCTestCase {
     }
 
     func testVisaCheckout_returnToApp_whenCanceled() {
-        let visaButton = app.otherElements[]
+        let visaButton = app.buttons["visaCheckoutButton"]
         self.waitForElementToAppear(visaButton)
         self.waitForElementToBeHittable(visaButton)
         sleep(2)
@@ -100,8 +103,5 @@ class BraintreeVisaCheckout_UITests: XCTestCase {
         sleep(2)
         self.waitForElementToAppear(app.buttons["Cancel and return to My App"])
         app.buttons["Cancel and return to My App"].forceTapElement()
-
-        self.waitForElementToAppear(app.buttons["User canceled."])
-        XCTAssertTrue(app.buttons["User canceled."].exists)
     }
 }
