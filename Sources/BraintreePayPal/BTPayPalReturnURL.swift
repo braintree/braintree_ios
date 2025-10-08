709946997e656d408c2ef1ab1181566b1faf86f9
Brandon Aubrey <baubrey91@gmail.com>
2025-10-08 11:27:53 -0700

    Added Validation Check for BTGW

--- Sources/BraintreePayPal/BTPayPalReturnURL.swift
+++ Sources/BraintreePayPal/BTPayPalReturnURL.swift
@@ -38,13 +38,13 @@ struct BTPayPalReturnURL {
     /// Evaluates whether the url represents a valid PayPal return URL.
     /// - Parameter url: an app switch or ASWebAuthenticationSession return URL
     /// - Returns: `true` if the url represents a valid PayPal app switch return
-    static func isValid(_ url: URL) -> Bool {
+    static func isValid(_ url: URL, fallbackUrlScheme: String?) -> Bool {
         let isHTTPSScheme = url.scheme == "https"
         let containsAppSwitchPath = url.path.contains("braintreeAppSwitchPayPal")
         let containsExpectedPath = url.path.contains("cancel") || url.path.contains("success")
         let isValidAppSwitchURL = isHTTPSScheme && containsAppSwitchPath && containsExpectedPath
         
-        return isValidAppSwitchURL
+        return isValidAppSwitchURL || fallbackUrlScheme == url.scheme
     }
 
     static func isValidURLAction(url: URL, didPayPalServerAttemptAppSwitch: Bool) -> Bool {
