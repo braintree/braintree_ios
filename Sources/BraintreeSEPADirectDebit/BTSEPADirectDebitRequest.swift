96d7c7502c9b447ee47a6f0722f8572d7a33cac0
ageddam <ageddam@paypal.com>
2025-10-13 15:34:17 -0500

    update properties to optional

--- Sources/BraintreeSEPADirectDebit/BTSEPADirectDebitRequest.swift
+++ Sources/BraintreeSEPADirectDebit/BTSEPADirectDebitRequest.swift
@@ -9,30 +9,30 @@ import BraintreeCore
 
     // MARK: - Internal Properties
     
-    let accountHolderName: String
-    let iban: String
-    let customerID: String
-    let billingAddress: BTPostalAddress
+    let accountHolderName: String?
+    let iban: String?
+    let customerID: String?
+    let billingAddress: BTPostalAddress?
     let mandateType: BTSEPADirectDebitMandateType?
     let merchantAccountID: String?
     let locale: String?
 
     /// Initialize a new SEPA Direct Debit request.
     /// - Parameters:
-    ///   - accountHolderName:Required. The account holder name.
-    ///   - iban: Required. The full IBAN.
-    ///   - customerID: Required. The customer ID.
-    ///   - billingAddress: Required. The user's billing address.
+    ///   - accountHolderName: Optional. The account holder name.
+    ///   - iban: Optional. The full IBAN.
+    ///   - customerID: Optional. The customer ID.
+    ///   - billingAddress: Optional. The user's billing address.
     ///   - mandateType: Optional. The `BTSEPADebitMandateType`. If not set, defaults to `.oneOff
     ///   - merchantAccountID: Optional. A non-default merchant account to use for tokenization.
     ///   - locale: Optional. A locale code to use for creating a mandate.
     ///   See https://developer.paypal.com/reference/locale-codes/ for a list of possible values.
     ///   Locale code should be supplied as a BCP-47 formatted locale code.
     public init(
-        accountHolderName: String,
-        iban: String,
-        customerID: String,
-        billingAddress: BTPostalAddress,
+        accountHolderName: String? = nil,
+        iban: String? = nil,
+        customerID: String? = nil,
+        billingAddress: BTPostalAddress? = nil,
         mandateType: BTSEPADirectDebitMandateType? = .oneOff,
         merchantAccountID: String? = nil,
         locale: String? = nil
