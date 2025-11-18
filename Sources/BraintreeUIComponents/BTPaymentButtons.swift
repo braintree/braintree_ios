5dffd7f6b9d1e07a0b030ff42d753258205b1441
stechiu <stechiu@paypal.com>
2025-11-17 18:41:52 -0800

    Added logo image assets. Created payment button

--- Sources/BraintreeUIComponents/BTPaymentButtons.swift
+++ Sources/BraintreeUIComponents/BTPaymentButtons.swift
@@ -1,10 +1,44 @@
 import Foundation
+import SwiftUI
 
 #if canImport(BraintreeCore)
 import BraintreeCore
 #endif
 
-public class BTPaymentButtons {
+struct BTPaymentButton: ButtonStyle {
+    let buttonColor: Color
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(buttonColor)
+            .cornerRadius(4)
+            .frame(width: 300, height: 45, alignment: .center)
+    }
+}
 
-    // TODO: - Add implementation
+struct ContentView: View {
+
+    var body: some View {
+        Button(action: {
+            print("Button pressed!")
+        }) {
+            HStack {
+                Spacer()
+                Image(.payPalLogoBlack)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 26)
+                Spacer()
+            }
+        }
+        .buttonStyle(BTPaymentButton(buttonColor: Color(red: 96 / 255, green: 205 / 255, blue: 255 / 255)))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+
+    static var previews: some View {
+        ContentView()
+    }
 }
