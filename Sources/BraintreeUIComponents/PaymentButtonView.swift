8857902df0f357ca235da174d619f2fd1cd4eaed
buzzamus <brbusby@paypal.com>
2025-11-24 11:17:05 -0600

    Create PaymentButtonStyle to handle button state

--- Sources/BraintreeUIComponents/PaymentButtonView.swift
+++ Sources/BraintreeUIComponents/PaymentButtonView.swift
@@ -10,39 +10,11 @@ struct PaymentButtonView<Color: PaymentButtonColorProtocol>: View {
     let accessibilityHint: String
     let action: () -> Void
 
-    @State private var isPressed = false
-
-    /// This is the width range for the payment button.
-    private var widthRange: CGFloat {
-        guard let width else { return 300 }
-        return min(max(width, 131), 300)
-    }
-
     var body: some View {
         Button(action: action) {
-            HStack {
-                if let logoImageName = color.logoImageName {
-                    Image(logoImageName, bundle: .uiComponents)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 14)
-                }
-            }
-            .frame(width: widthRange)
+            EmptyView()
         }
-        .frame(height: 45)
-        .background(isPressed ? color.tappedButtonColor : color.backgroundColor)
-        .cornerRadius(4)
-        .overlay(
-            RoundedRectangle(cornerRadius: 4)
-                .stroke(
-                    color.hasOutline ? .black : .clear,
-                    lineWidth: color.hasOutline ? 1 : 0
-                )
-        )
-        .onLongPressGesture(minimumDuration: 0, maximumDistance: .infinity, perform: {}, onPressingChanged: { pressing in
-            isPressed = pressing
-        })
+        .buttonStyle(PaymentButtonStyle(color: color, width: width))
         .accessibilityLabel(accessibilityLabel)
         .accessibilityHint(accessibilityHint)
     }
