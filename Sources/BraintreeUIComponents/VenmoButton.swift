4cc7db1c259c4a201c9a90d9662b056e2d7540ec
buzzamus <brbusby@paypal.com>
2025-11-24 15:12:36 -0600

    Add BTVenmoRequest, completion handler to VenmoButton

--- Sources/BraintreeUIComponents/VenmoButton.swift
+++ Sources/BraintreeUIComponents/VenmoButton.swift
@@ -1,7 +1,11 @@
 import SwiftUI
+import BraintreeVenmo
 
 /// Venmo payment button. Available in the colors primary (Venmo blue), black, and white.
 public struct VenmoButton: View {
+
+    /// The Venmo request
+    let request: BTVenmoRequest
 
     /// The style of the Venmo payment button. Available in the colors primary (Venmo blue), black, and white.
     let color: VenmoButtonColor?
@@ -9,28 +13,38 @@ public struct VenmoButton: View {
     /// The width of the Venmo payment button. Minimum width is 131 points. Maximum width is 300 points.
     let width: CGFloat?
 
-    /// The Venmo payment button action.
-    let action: () -> Void
+    /// The completion handler to handle Venmo tokenize request success or failure
+    let completion: (BTVenmoAccountNonce?, Error?) -> Void
 
     // MARK: - Initializer
 
     /// Creates a Venmo button
+    /// - Parameter request: A Venmo request.
     /// - Parameter color: Optional. The desired button color with corresponding Venmo logo. Defaults to `.primary`.
     /// - Parameter width: Optional. The width of the button. Defaults to 300px.
-    /// - Parameter action: the completion handler to handle Venmo tokenize request success or failure on button press
-    public init(color: VenmoButtonColor? = .primary, width: CGFloat? = 300, action: @escaping () -> Void) {
+    /// - Parameter completion: the completion handler to handle Venmo tokenize request success or failure on button press
+    public init(
+        request: BTVenmoRequest,
+        color: VenmoButtonColor? = .primary,
+        width: CGFloat? = 300,
+        completion: @escaping (BTVenmoAccountNonce?, Error?) -> Void
+    ) {
+        self.request = request
         self.color = color
         self.width = width
-        self.action = action
+        self.completion = completion
     }
     public var body: some View {
         PaymentButtonView(
             color: color ?? .primary,
             width: width,
             accessibilityLabel: "Pay with Venmo",
-            accessibilityHint: "Complete payment using Venmo",
-            action: action
-        )
+            accessibilityHint: "Complete payment using Venmo"
+        ) {
+            // TODO: Implement Venmo flow when button is tapped
+            // This will create BTVenmoClient and call tokenize
+            // Then call completion with result
+        }
     }
 }
 struct VenmoButton_Previews: PreviewProvider {
@@ -38,10 +52,23 @@ struct VenmoButton_Previews: PreviewProvider {
     static var previews: some View {
         VStack {
             // defaults to primary, width 300
-            VenmoButton {}
-            VenmoButton(color: .black, width: 250) {}
+            VenmoButton(request: BTVenmoRequest(paymentMethodUsage: .singleUse), completion: VenmoButton_Previews.closure)
+
+            VenmoButton(
+                request: BTVenmoRequest(paymentMethodUsage: .singleUse),
+                color: .black,
+                width: 250,
+                completion: VenmoButton_Previews.closure
+            )
             // respects minimum width boundary
-            VenmoButton(color: .white, width: 1) {}
+            VenmoButton(
+                request: BTVenmoRequest(paymentMethodUsage: .singleUse),
+                color: .white,
+                width: 1,
+                completion: VenmoButton_Previews.closure
+            )
         }
     }
+
+    static func closure(_: BTVenmoAccountNonce?, _: Error?) {}
 }
