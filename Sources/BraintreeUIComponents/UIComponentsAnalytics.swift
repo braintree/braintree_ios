ead3d4b6721b10806d96b0321e67e6e4b8192d66
Brent Busby <brent.busby@yahoo.com>
2025-12-08 09:44:05 -0600

    Update Sources/BraintreeUIComponents/UIComponentsAnalytics.swift
    Co-authored-by: Jax DesMarais-Leder <jdesmarais@paypal.com>

--- Sources/BraintreeUIComponents/UIComponentsAnalytics.swift
+++ Sources/BraintreeUIComponents/UIComponentsAnalytics.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 enum UIComponentsAnalytics {
-    static let paypalButtonPresented = "ui-components:paypal-button:presented"
-    static let paypalButtonSelected = "ui-components:paypal-button:selected"
+    static let payPalButtonPresented = "ui-components:paypal-button:presented"
+    static let payPalButtonSelected = "ui-components:paypal-button:selected"
     static let venmoButtonPresented = "ui-components:venmo-button:presented"
     static let venmoButtonSelected = "ui-components:venmo-button:selected"
 }
