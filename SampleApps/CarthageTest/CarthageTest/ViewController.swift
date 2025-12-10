6d264a5bccf8b51af44a6705a6bcabc985593d53
ageddam <ageddam@paypal.com>
2025-12-10 13:49:15 -0600

    display buttons in default blue color

--- SampleApps/CarthageTest/CarthageTest/ViewController.swift
+++ SampleApps/CarthageTest/CarthageTest/ViewController.swift
@@ -35,12 +35,47 @@ class ViewController: UIViewController {
         )
         let sepaDirectDebitClient = BTSEPADirectDebitClient(authorization: authorization)
         
-        // draw PayPal branded button
+        setupVenmoButton()
+        setupPayPalButton()
+    }
+    
+    private func setupVenmoButton() {
+        let venmoRequest = BTVenmoRequest(paymentMethodUsage: .singleUse)
+        
+        let venmoButton = VenmoButton(
+            authorization: authorization,
+            universalLink: URL(string: "https://example.com")!,
+            request: venmoRequest,
+            color: .blue,
+            width: 300
+        ) { nonce, error in
+            print("Button tapped")
+        }
+        
+        venmoHostingController = UIHostingController(rootView: venmoButton)
+        guard let venmoHostingController else { return }
+        
+        addChild(venmoHostingController)
+        view.addSubview(venmoHostingController.view)
+        
+        venmoHostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            venmoHostingController.view.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            venmoHostingController.view.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100),
+            venmoHostingController.view.widthAnchor.constraint(equalToConstant: 300),
+            venmoHostingController.view.heightAnchor.constraint(equalToConstant: 45)
+        ])
+        
+        venmoHostingController.didMove(toParent: self)
+    }
+    
+    private func setupPayPalButton() {
         let paypalRequest = BTPayPalCheckoutRequest(amount: "10.00")
+        
         let paypalButton = PayPalButton(
             authorization: authorization,
             request: paypalRequest,
-            color: .black,
+            color: .blue,
             width: 300
         ) { (nonce, error) in
             if let error = error {
@@ -49,23 +84,21 @@ class ViewController: UIViewController {
                 print("PayPal Account Nonce: \(nonce)")
             }
         }
-
         
-        // draw Venmo branded button
-        let venmoRequest = BTVenmoRequest(paymentMethodUsage: .singleUse)
-        let venmoButton = VenmoButton(
-            authorization: authorization,
-            universalLink: URL(string: "https://mobile-sdk-demo-site-838cead5d3ab.herokuapp.com/braintree-payments")!,
-            request: venmoRequest,
-            color: .blue,
-            width: 300
-        ) { (nonce, error) in
-            if let error = error {
-                print("Error: \(error.localizedDescription)")
-            } else if let nonce = nonce {
-                print("Venmo Account Nonce: \(nonce)")
-            }
-        }
-
+        paypalHostingController = UIHostingController(rootView: paypalButton)
+        guard let paypalHostingController else { return }
+        
+        addChild(paypalHostingController)
+        view.addSubview(paypalHostingController.view)
+        
+        paypalHostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            paypalHostingController.view.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            paypalHostingController.view.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            paypalHostingController.view.widthAnchor.constraint(equalToConstant: 300),
+            paypalHostingController.view.heightAnchor.constraint(equalToConstant: 45)
+        ])
+        
+        paypalHostingController.didMove(toParent: self)
     }
 }
