eb3f5d60048c26fa021bec157abb3a62e1dd724b
buzzamus <brbusby@paypal.com>
2025-12-16 09:40:59 -0600

    Add PayPal test files, PayPal color attributes unit tests

--- Demo/UI Tests/Payment Button UI Tests/PayPal_Button_UITests.swift
+++ Demo/UI Tests/Payment Button UI Tests/PayPal_Button_UITests.swift
@@ -10,4 +10,48 @@ import XCTest
 
 final class PayPal_Button_UITests: XCTestCase {
 
+    var app = XCUIApplication()
+    var springboard = XCUIApplication(bundleIdentifier: "com.apple.springboard")
+
+    override func setUp() {
+        continueAfterFailure = false
+        app.launchArguments.append("-EnvironmentSandbox")
+        app.launchArguments.append("-MockedPayPalTokenizationKey")
+        app.launchArguments.append("-Integration:PaymentButtonViewController")
+
+        app.launchEnvironment["UITEST_DISABLE_ANIMATIONS"] = "YES"
+        app.launch()
+
+        springboard = XCUIApplication(bundleIdentifier: "com.apple.springboard")
+    }
+
+    // loading test - starts w/ loading, then ends without
+
+    // tests that it can onyl be tapped once - disabled after first tap
+
+    func testPayPal_button_loadingState() {
+        // Verify state is not in loading, enabled
+        XCTAssertTrue(app.buttons["Pay with PayPal"].isEnabled)
+
+        app.buttons["Pay with PayPal"].tap()
+
+        XCTAssertFalse(app.buttons["Pay with PayPal"].isEnabled)
+
+        // assert in loading
+    }
+
+    func testPayPalButton_tapLaunchesPayPalFlow() {
+        app.buttons["Pay with PayPal"].tap()
+
+        _ = springboard.buttons["Continue"].waitForExistence(timeout: 20.0)
+        springboard.buttons["Continue"].tap()
+
+        let webviewElementsQuery = app.webViews.element.otherElements
+
+        self.waitForElementToAppear(webviewElementsQuery.links["Proceed with Sandbox Purchase"])
+
+        webviewElementsQuery.links["Proceed with Sandbox Purchase"].forceTapElement()
+
+        XCTAssertTrue(app.buttons["Got a nonce. Tap to make a transaction."].waitForExistence(timeout: 2))
+    }
 }
--- Demo/UI Tests/Payment Button UI Tests/Venmo_Button_UITests.swift
+++ Demo/UI Tests/Payment Button UI Tests/Venmo_Button_UITests.swift
@@ -2,7 +2,6 @@ import XCTest
 
 final class Venmo_Button_UITests: XCTestCase {
 
-
     var app = XCUIApplication()
     var springboard = XCUIApplication(bundleIdentifier: "com.apple.springboard")
 
@@ -11,19 +10,35 @@ final class Venmo_Button_UITests: XCTestCase {
         continueAfterFailure = false
         app.launchArguments.append("-EnvironmentSandbox")
         app.launchArguments.append("-ClientToken")
-        app.launchArguments.append("-Integration:PaymentButtonsViewController")
+        app.launchArguments.append("-Integration:PaymentButtonViewController")
 
         // Disable animations for more reliable tests
         app.launchEnvironment["UITEST_DISABLE_ANIMATIONS"] = "YES"
         app.launch()
 
-        app.buttons["Pay with Venmo"].tap()
-
         springboard = XCUIApplication(bundleIdentifier: "com.apple.springboard")
     }
 
-    func testVenmo_button_tapDirectsToVenmo() {
+    // loading test - starts w/ loading, then ends without
+
+    // tests that it can onyl be tapped once - disabled after first tap
+
+    func testVenmo_button_loadingState() {
+        XCTAssertTrue(app.buttons["Pay with Venmo"].isEnabled)
+
+        app.buttons["Pay with Venmo"].tap()
+
+        XCTAssertFalse(app.buttons["Pay with Venmo"].isEnabled)
+        // assert loading status
+
+    }
+
+    func testVenmo_button_tapLaunchesVenmoFlow() {
+        app.buttons["Pay with Venmo"].tap()
         _ = springboard.buttons["Return to SDK Demo"].waitForExistence(timeout: 3.0)
+
+        XCTAssertNotNil(app.webViews.element.otherElements)
+
         springboard.buttons["Return to SDK Demo"].tap()
 
         XCTAssertTrue(app.buttons["Pay with Venmo"].waitForExistence(timeout: 2))
