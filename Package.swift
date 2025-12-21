77f8b09eebb70ded47440f07bfd0dcb89abd1f26
Gheorghe Gavrisco <g.gavrisco@autodoc.eu>
2025-12-21 20:58:00 +0200

    Use paypal messages from source

--- Package.swift
+++ Package.swift
@@ -6,12 +6,6 @@ import PackageDescription
 let package = Package(
     name: "Braintree",
     platforms: [.iOS(.v14)],
-    dependencies: [
-        .package(
-            url: "https://github.com/gicugavrisco/paypal-messages-ios.git",
-            branch: "feature/source-spm"
-        )
-    ],
     products: [
         .library(
             name: "BraintreeAmericanExpress",
@@ -60,11 +54,15 @@ let package = Package(
         .library(
             name: "BraintreeVenmo",
             targets: ["BraintreeVenmo"]
-        ),
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/gicugavrisco/paypal-messages-ios.git",
+            exact: "1.2.0"
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BraintreeAmericanExpress",
             dependencies: ["BraintreeCore"],
