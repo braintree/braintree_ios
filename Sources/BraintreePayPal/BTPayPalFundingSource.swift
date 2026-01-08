778ec1443bff19e6d0e169be937c94bc9bce701b
nrodzik <nrodzik@paypal.com>
2026-01-08 12:11:24 -0600

    Add a doc-string to BTPayPalFundingSource

--- Sources/BraintreePayPal/BTPayPalFundingSource.swift
+++ Sources/BraintreePayPal/BTPayPalFundingSource.swift
@@ -1,12 +1,9 @@
-//
-//  BTPayPalFundingSource.swift
-//  Braintree
-//
-//  Created by Anastasia Rodzik on 1/8/26.
-//
-
+/// Funding sources available when creating a PayPal payment.
 public enum BTPayPalFundingSource: String {
+    /// Standard PayPal balance or linked bank account funding.
     case payPal = "paypal"
+    /// PayPal Credit revolving line of credit.
     case credit
+    /// PayPal Pay Later BNPL products for short-term installments (e.g. Pay in 4, Pay Monthly in the US).
     case payLater = "paylater"
 }
