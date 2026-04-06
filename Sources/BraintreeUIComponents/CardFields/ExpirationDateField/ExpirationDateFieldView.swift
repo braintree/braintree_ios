3430e350576257d3a6d5dce67fa66c97ac64d5dc
Alekhya Geddam <ageddam@paypal.com>
2026-04-06 14:48:36 -0500

    add logic to draw the expiration date box

--- Sources/BraintreeUIComponents/CardFields/ExpirationDateField/ExpirationDateFieldView.swift
+++ Sources/BraintreeUIComponents/CardFields/ExpirationDateField/ExpirationDateFieldView.swift
@@ -1,12 +1,47 @@
 import SwiftUI
 
-struct ExpirationDateFieldView: View {
+struct ExpirationDateFieldView<ViewModel: CardFieldsViewModelProtocol>: View {
+    
+    @ObservedObject var viewModel: ViewModel
+    var onAutoAdvance: (() -> Void)?
+    @FocusState private var isFocused: Bool
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        CardFieldsContainerView(
+            validationState: viewModel.validationState,
+            isFocused: isFocused
+        ) {
+            VStack(alignment: .leading, spacing: 2) {
+                Text("Expiration (MM/YY)")
+                    .font(.caption)
+                    .foregroundColor(Color(.secondaryLabel))
+                
+                TextField(
+                    "",
+                    text: Binding(
+                    get: { viewModel.value },
+                    set: { viewModel.updateValue($0) }
+                  )
+                )
+                .keyboardType(.numberPad)
+                .focused($isFocused)
+                .font(.body)
+                .foregroundColor(Color(.label))
+            }
+            
+            Spacer()
+        }
+        .frame(width: 179, height: 64)
+        // update `onChange(of:perform:)` signature once min deployment target is iOS 17
+        .onChange(of: isFocused) { focused in
+            viewModel.isFocused = focused
+        }
+        .onChange(of: viewModel.shouldAutoAdvance) { shouldAutoAdance in
+            if shouldAutoAdance { onAutoAdvance?() }
+        }
     }
 }
 
 #Preview {
-    ExpirationDateFieldView()
+    ExpirationDateFieldView(viewModel: ExpirationDateFieldViewModel())
 }
--- Sources/BraintreeUIComponents/CardFields/ExpirationDateField/ExpirationDateFieldViewModel.swift
+++ Sources/BraintreeUIComponents/CardFields/ExpirationDateField/ExpirationDateFieldViewModel.swift
@@ -1,7 +1,16 @@
 import Foundation
 
-// TODO: have conform to CardFieldsViewModelProtocol
-class ExpirationDateFieldViewModel {
+class ExpirationDateFieldViewModel: CardFieldsViewModelProtocol {
     
-    var state: String = ""
+    @Published private(set) var value: String = ""
+    @Published private(set) var validationState: ValidationResult = .valid
+    @Published var isFocused: Bool = false
+    
+    // TODO: Update auto-advance logic
+    var shouldAutoAdvance: Bool { false }
+    
+    // TODO: Implement expiration fiel validation and formatting checks
+    func updateValue(_ newValue: String) {
+        value = newValue
+    }
 }
