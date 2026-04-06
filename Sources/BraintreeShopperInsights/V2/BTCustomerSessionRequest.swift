41f092e34efc5a34dc3d13c4d58e88d529048b43
aaleid-paypal-public <aaleid@paypal.com>
2026-04-06 12:15:56 -0500

    Update BTCustomerSessionRequest.swift

--- Sources/BraintreeShopperInsights/V2/BTCustomerSessionRequest.swift
+++ Sources/BraintreeShopperInsights/V2/BTCustomerSessionRequest.swift
@@ -1,3 +1,6 @@
+import Foundation
+
+/// A `BTCustomerSessionRequest`for creating a customer session.
 public struct BTCustomerSessionRequest {
 
     let hashedEmail: String?
@@ -7,6 +10,15 @@ public struct BTCustomerSessionRequest {
     let purchaseUnits: [BTPurchaseUnit]?
     let payPalCampaigns: [BTPayPalCampaign]?
 
+    /// Creates a BTCustomerSessionRequest
+    /// - Parameters:
+    ///   - hashedEmail: Optional: The customer's email address hashed via SHA256 algorithm.
+    ///   - hashedPhoneNumber: Optional: The customer's phone number hased via SHA256 algorithm.
+    ///   - payPalAppInstalled: Optional: Checks whether the PayPal app is installed on the device.
+    ///   - venmoAppInstalled: Optional: Checks whether the Venmo app is installed on the device.
+    ///   - purchaseUnits: Optional: The list of purchase units containing the amount and currency code.
+    /// - Warning: This feature is in beta. It's public API may change or be removed in future releases.
+    
     public init(
         hashedEmail: String? = nil,
         hashedPhoneNumber: String? = nil,
