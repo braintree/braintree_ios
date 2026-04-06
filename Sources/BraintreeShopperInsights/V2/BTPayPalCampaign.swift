0d2bda2a78453337237e31cf61c0f3fd2f47468e
aaleid-paypal-public <aaleid@paypal.com>
2026-04-06 14:00:18 -0500

    Update BTPayPalCampaign.swift

--- Sources/BraintreeShopperInsights/V2/BTPayPalCampaign.swift
+++ Sources/BraintreeShopperInsights/V2/BTPayPalCampaign.swift
@@ -2,7 +2,7 @@ import Foundation
 
 /// Creates a BTPayPalCampaign
 /// - Parameter id: The campaign identifier associated between PayPal and the merchant/partner.
-/// - Warning: This feature is in beta. It's public API may change or be removed in future releases.
+
 public struct BTPayPalCampaign: Encodable {
     let id: String
     
