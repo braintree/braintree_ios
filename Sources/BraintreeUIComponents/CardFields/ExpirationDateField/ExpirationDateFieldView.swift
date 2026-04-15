66a32a3eb16eb608135c647705ea7f32d2e1e8f6
agedd <105314544+agedd@users.noreply.github.com>
2026-04-15 10:19:45 -0500

    Create Expiration Date Field Form (#1774)
    * Add card number field UI
    
    * Fix linting error
    
    * add logic to draw the expiration date box
    
    * cleanup
    
    * fix swiftlint warning
    
    * address pr feedback
    
    ---------
    
    Co-authored-by: buzzamus <brbusby@paypal.com>

--- Sources/BraintreeUIComponents/CardFields/ExpirationDateField/ExpirationDateFieldView.swift
+++ Sources/BraintreeUIComponents/CardFields/ExpirationDateField/ExpirationDateFieldView.swift
@@ -1,12 +1,50 @@
 import SwiftUI
 
-struct ExpirationDateFieldView: View {
+struct ExpirationDateFieldView<ViewModel: CardFieldsViewModelProtocol>: View {
+    
+    // MARK: - Internal Properties
+    
+    @ObservedObject var viewModel: ViewModel
+    var onAutoAdvance: (() -> Void)?
+    
+    // MARK: - Private Properties
+    
+    @FocusState private var isFocused: Bool
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        CardFieldsContainerView(
+            validationState: viewModel.validationState,
+            isFocused: isFocused
+        ) {
+            VStack(alignment: .leading, spacing: 2) {
+                Text("Expiration (MM/YY)")
+                    .font(.system(size: 12))
+                    .foregroundColor(Color(.secondaryLabel))
+                
+                TextField(
+                    "",
+                    text: Binding(
+                        get: { viewModel.value },
+                        set: { viewModel.updateValue($0) }
+                    )
+                )
+                .keyboardType(.numberPad)
+                .focused($isFocused)
+                .font(.body)
+                .foregroundColor(Color(.label))
+            }
+            
+            Spacer()
+        }
+        .onChange(of: isFocused) { _, focused in
+            viewModel.isFocused = focused
+        }
+        .onChange(of: viewModel.shouldAutoAdvance) { _, shouldAutoAdvance in
+            if shouldAutoAdvance { onAutoAdvance?() }
+        }
     }
 }
 
 #Preview {
-    ExpirationDateFieldView()
+    ExpirationDateFieldView(viewModel: ExpirationDateFieldViewModel())
 }
