1b2fcc235ec9ecbe34ed31ff72e4a7738201a285
Alekhya Geddam <ageddam@paypal.com>
2026-04-28 12:04:14 -0500

    test: remove returnsResult test

--- IntegrationTests/BraintreeAmexExpress_IntegrationTests.swift
+++ IntegrationTests/BraintreeAmexExpress_IntegrationTests.swift
@@ -21,29 +21,6 @@ class BTAmericanExpressClient_IntegrationTests: XCTestCase {
     
     // MARK: - getRewardsBalance
     
-    func testGetRewardsBalance_returnsResult() async {
-        let amexClient = BTAmericanExpressClient(authorization: BTIntegrationTestsConstants.sandboxClientTokenVersion3)
-        
-        let card = BTCard(
-            number: "371260714673002",
-            expirationMonth: "12",
-            expirationYear: Helpers.shared.futureYear(),
-            cvv: "1234"
-        )
-        
-        do {
-            let tokenizedCard = try await cardClient.tokenize(card)
-            let rewardsBalance = try await amexClient.getRewardsBalance(forNonce: tokenizedCard.nonce, currencyISOCode: "USD")
-            
-            XCTAssertEqual(rewardsBalance.rewardsAmount, "45256433")
-            XCTAssertEqual(rewardsBalance.rewardsUnit, "Points")
-            XCTAssertEqual(rewardsBalance.currencyAmount, "316795.03")
-            XCTAssertEqual(rewardsBalance.currencyIsoCode, "USD")
-        } catch {
-            XCTFail("Unexpected error: \(error.localizedDescription)")
-        }
-    }
-    
     func testGetRewardsBalance_withNonAmexNonce_returnsError() {
         let cardExpectation = expectation(description: "Tokenize Visa card")
         var visaNonce: String?
