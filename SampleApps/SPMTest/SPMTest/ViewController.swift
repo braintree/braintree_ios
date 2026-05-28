dee9adc830ea174b26f71ccfc76e1b4806395ae5
braintreeps <code@getbraintree.com>
2026-05-28 18:27:49 +0000

    Revert "Remove SPMTest app to avoid Carthage timeout"
    This reverts commit 1c103228b0ed370382da65e5ce2c82bf9bc3f851.

---
+++ SampleApps/SPMTest/SPMTest/ViewController.swift
@@ -0,0 +1,33 @@
+import UIKit
+import BraintreeAmericanExpress
+import BraintreeApplePay
+import BraintreeCard
+import BraintreeCore
+import BraintreeDataCollector
+import BraintreeLocalPayment
+import BraintreePayPal
+import BraintreePayPalMessaging
+import BraintreeThreeDSecure
+import BraintreeVenmo
+import BraintreeSEPADirectDebit
+
+class ViewController: UIViewController {
+    
+    let authorization: String = "sandbox_9dbg82cq_dcpspy2brwdjr3qn"
+
+    override func viewDidLoad() {
+        let amexClient = BTAmericanExpressClient(authorization: authorization)
+        let applePayClient = BTApplePayClient(authorization: authorization)
+        let cardClient = BTCardClient(authorization: authorization)
+        let dataCollector = BTDataCollector(authorization: authorization)
+        let localPaymentClient = BTLocalPaymentClient(authorization: authorization)
+        let payPalClient = BTPayPalClient(authorization: authorization)
+        let payPalMessagingView = BTPayPalMessagingView(authorization: authorization)
+        let threeDSecureClient = BTThreeDSecureClient(authorization: authorization)
+        let venmoClient = BTVenmoClient(
+            authorization: authorization,
+            universalLink: URL(string: "https://mobile-sdk-demo-site-838cead5d3ab.herokuapp.com/braintree-payments")!
+        )
+        let sepaDirectDebitClient = BTSEPADirectDebitClient(authorization: authorization)
+    }
+}
