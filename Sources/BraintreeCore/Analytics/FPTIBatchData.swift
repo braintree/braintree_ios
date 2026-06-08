1dad0c4d00118f031bb4a058cbf4971bd86dde99
buzzamus <brbusby@paypal.com>
2026-06-08 13:29:51 -0500

    fix(core): Revert component name to correct value

--- Sources/BraintreeCore/Analytics/FPTIBatchData.swift
+++ Sources/BraintreeCore/Analytics/FPTIBatchData.swift
@@ -180,7 +180,7 @@ struct FPTIBatchData: Codable {
 
         let clientOS: String = UIDevice.current.systemName + " " + UIDevice.current.systemVersion
 
-        let component = "Brent-Test-braintreeclientsdk"
+        let component = "braintreeclientsdk"
 
         let deviceManufacturer = "Apple"
 
