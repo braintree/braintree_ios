eeb1cba2c775293e242d59482e43ff81151e41f6
buzzamus <brbusby@paypal.com>
2026-06-09 09:31:57 -0500

    fix(uicomponents): Add new CardFields UI Test File

---
+++ Demo/UI Tests/CardFields UI Tests/CardFields_UITests.swift
@@ -0,0 +1,40 @@
+//
+//  CardFields_UITests.swift
+//  UITests
+//
+//  Created by Brent Busby on 6/9/26.
+//  Copyright © 2026 braintree. All rights reserved.
+//
+
+import XCTest
+
+final class CardFields_UITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() throws {
+        // This measures how long it takes to launch your application.
+        measure(metrics: [XCTApplicationLaunchMetric()]) {
+            XCUIApplication().launch()
+        }
+    }
+}
