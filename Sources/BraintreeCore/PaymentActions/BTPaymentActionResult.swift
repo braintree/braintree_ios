8f696f7585686db364337670ab4423f618e42b85
Alekhya Geddam <ageddam@paypal.com>
2026-07-22 16:49:16 -0500

    refactor: trim out types and fields

--- Sources/BraintreeCore/PaymentActions/BTPaymentActionResult.swift
+++ Sources/BraintreeCore/PaymentActions/BTPaymentActionResult.swift
@@ -2,9 +2,6 @@ import Foundation
 
 struct BTPaymentActionResult {
     
-    public let id: String
-    public let status: BTPaymentActionStatus
-    // TODO: Add the nextAction result type in a later M4-related PR.
-    public let nextAction: BTJSON?
-    public let selectedPaymentMethod: BTPaymentActionSelectedPaymentMethod?
+    let id: String
+    let status: BTPaymentActionStatus
 }
